d13afca2adb405da6de7d0557adafc9c6235af27
Jun Wang <jwang@dellnote.nju.edu.cn>
2009-10-15 01:36:18 +0800

    minor change for berendsen propagator
    Just create the file and have no contents.

---
+++ propagator-particle-Berendsen-vverlet.h
@@ -0,0 +1,18 @@
+
+#ifndef _Propagator_Particle_Berendsen_VelVerlet_H_
+#define _Propagator_Particle_Berendsen_VelVerlet_H_
+
+#include "property.h"
+#include "param-list.h"
+#include "interaction-4listset.h"
+#include "monomer-propagator-format.h"
+
+namespace std {
+
+  enum PropagatorParam_4ParticleBerendsenVelVerlet {
+  };
+
+}
+
+#endif
+
