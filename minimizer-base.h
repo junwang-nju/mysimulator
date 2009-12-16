8a47af49ef873b5bf6f75cc56f98880491a4b05e
Jun Wang <jwang@dellnote.nju.edu.cn>
2009-12-17 02:09:51 +0800

    temporary modification
    The new form of minimization is created, but is not finished.

--- minimizer-base.h
+++ minimizer-base.h
@@ -2,211 +2,112 @@
 #ifndef _Minimizer_Base_H_
 #define _Minimizer_Base_H_
 
-#include "property-list.h"
-#include "interaction-4listset.h"
-#include "ref-vector.h"
-#include <cmath>
+#include "memory.h"
+#include "var-vector.h"
 
 namespace std {
 
-  double MinimalStep4(const VectorBase<double>& X,
-                      const VectorBase<double>& Dirc,
-                      const VectorBase<uint>& Fg) {
-    uint n=X.size();
-    assert(n==Dirc.size());
-    assert(n==Fg.size());
-    double MinStep=0.,tmd,ndeg=0.;
-    for(uint i=0;i<n;++i) {
-      if(Fg[i]==0)  continue;
-      tmd=fabs(X[i]);
-      tmd=(tmd<1.0?Dirc[i]:Dirc[i]/tmd);
-      MinStep+=tmd*tmd;
-      ndeg+=1.;
-    }
-    return DRelDelta*sqrt(ndeg/MinStep); 
-  }
-
-  template <typename DistEvalObj, typename GeomType>
-  class MinimizerKern {
-
-    protected:
-      void allocateSystem4Minimize(PropertyList& PList,
-                                   const VectorBase<Property>& iPList) {
-        uint n=iPList.size();
-        varVector<uint> MType(n),MFlag(n);
-        for(uint i=0;i<n;++i) {
-          MType[i]=iPList[i].Info[MonomerTypeID];
-          MFlag[i]=GradientEnable;
-        }
-        PList.gAllocate(MType,MFlag,iPList[0].Info[MonomerDimension]);
-      }
-
-      void copySystem4Minimization(PropertyList& PList,
-                                   const PropertyList& iPList) {
-        // here assume that PList and iPList have the same structure
-        PList.gIProperty[gInfo]=iPList.gIProperty[gInfo];
-        PList.gDProperty[gCoordinate]=iPList.gDProperty[gCoordinate];
-        PList.gIProperty[gMask]=iPList.gIProperty[gMask];
-        PList.gDProperty[gDMask]=iPList.gDProperty[gDMask];
-        PList.gDProperty[gGradient]=iPList.gDProperty[gGradient];
-        if(PList.gDProperty[gInCoordinate].isAvailable())
-          PList.gDProperty[gInCoordinate]=
-              iPList.gDProperty[gInCoordinate];
-        if(PList.gDProperty[gInGradient].isAvailable())
-          PList.gDProperty[gInGradient]=
-              iPList.gDProperty[gInGradient];
-      }
+  template <typename CoordinateType, typename ParameterType>
+  class MinimizerKernBase {
 
     public:
+      typedef void (*YFuncType)(const CoordinateType*,ParameterType*,
+                                double&);
+      typedef void (*GFuncType)(const CoordinateType*,ParameterType*,
+                                CoordinateType*);
+      typedef void (*BFuncType)(const CoordinateType*,ParameterType*,
+                                double&,CoordinateType*);
+      YFuncType                 YFunc;
+      GFuncType                 GFunc;
+      BFuncType                 BFunc;
+      CoordinateType            *pMinCoor;
+      double                    MinY;
+      GradientType              *pMinGrad;
+      ParameterType             *pRunParam;
+      double                    MinPrj;
+      double                    MinMove;
+      uint                      MinGCount;
+      double                    MinScale;
 
-      PropertyList        MinSys;      
-      
-      double              MinE;
-      
-      ParamList*          runParamPtr;
+      MinimizerKernBase() : YFunc(NULL), GFunc(NULL), BFunc(NULL),
+                            pMinCoor(NULL), MinY(0.), pMinGrad(NULL),
+                            pRunParam(NULL), MinPrj(0.), MinMove(0.),
+                            MinGCount(0), MinScale(0.1) {}
+      void clear() {
+        YFunc=NULL;
+        GFunc=NULL;
+        BFunc=NULL;
+        safe_delete(pMinCoor);
+        safe_delete(pMinGrad);
+        safe_delete(pRunParam);
+      }
 
-      varVector<IDList<DistEvalObj,GeomType> >*
-                          runIDLSPtr;
-      
-      DistEvalObj*        runDEvalPtr;
-      
-      GeomType*           runGeoPtr;
-      
-      double              MinPrj;
-      
-      double              MinMove;
-      
-      uint                MinGCount;
-      
-      double              MinScale;
-      
-      MinimizerKern()
-        : MinSys(), MinE(0.), runParamPtr(NULL), runDEvalPtr(NULL),
-          runGeoPtr(NULL), MinPrj(0.), MinMove(0.), MinGCount(0),
-          MinScale(0.1) {
-      } 
-
-      void Import(const PropertyList& iPList, const DistEvalObj& DEval,
-                  const ParamList& PList,
-                  const varVector<IDList<DistEvalObj,GeomType> >& IDLS,
-                  const GeomType& Geo, const double& E) {
-        allocateSystem4Minimize(MinSys,iPList);
-        copySystem4Minimization(MinSys,iPList);
-        runDEvalPtr=const_cast<DistEvalObj*>(&DEval);
-        runParamPtr=const_cast<ParamList*>(&PList);
-        runIDLSPtr
-          =const_cast<varVector<IDList<DistEvalObj,GeomType> >*>(&IDLS);
-        runGeoPtr=const_cast<GeomType*>(&Geo);
-        MinE=E;
+      void ImportFunc(const YFuncType& YF,const GFuncType& GF,
+                      const BFuncType& BF) {
+        YFunc=const_cast<YFuncType&>(YF);
+        GFunc=const_cast<GFuncType&>(GF);
+        BFunc=const_cast<BFuncType&>(BF);
       }
 
   };
 
-  template <typename DistEvalObj, typename GeomType>
-  class CoarseMinimizerKern : public MinimizerKern<DistEvalObj,GeomType> {
+  template <typename CoordinateType, typename ParameterType>
+  class MinimizerKern : public MinimizerKernBase<CoordinateType,ParameterType>{
+    public:
+      typedef MinimizerKern<CoordinateType,ParameterType>     Type;
+      typedef MinimizerKernBase<CoordinateType,ParameterType> ParentType;
+      MinimizerKern() : ParentType() {}
+      virtual void ImportState(const CoordinateType&,const ParameterType&)=0;
+  };
+
+  template <>
+  class MinimizerKern<varVector<double>,varVector<double> >
+    : public MinimizerKernBase<varVector<double>,varVector<double> > {
+    public:
+      typedef varVector<double> DVecType;
+      typedef MinimizerKern<DVecType,DVecType>      Type;
+      typedef MinimizerKernBase<DVecType,DVecType>  ParentType;
+      MinimizerKern() : ParentType() {}
+      void ImportState(const DVecType& Coor, const DVecType& Param) {
+        assert(this->BFunc!=NULL);
+        this->pMinCoor=new DVecType;
+        this->pMinCoor->allocate(Coor);
+        *(this->pMinCoor)=Coor;
+        this->pMinGrad=new DVecType;
+        this->pMinGrad.allocate(Coor);
+        MinY=0.;
+        *(this->pMinGrad)=0.;
+        BFunc(this->pMinCoor,this->pRunParam,MinY,this->pMinGrad);
+      }
+  };
+
+  template <typename CoordinateType, typename ParameterType>
+  class LineMinimizerBase
+    : public MinimizerKern<CoordinateType,ParameterType> {
 
     public:
-    
-      typedef CoarseMinimizerKern<DistEvalObj,GeomType>   Type;
-      
-      typedef MinimizerKern<DistEvalObj,GeomType>         ParentType;
+      typedef LineMinimizerBase<CoordinateType,ParameterType> Type;
+      typedef MinimizerKern<CoordinateType,ParameterType>     ParentType;
 
-      double Step;
-      
-      static const uint DefaultMaxIter;
-      
-    private:
-    
-      void Update(const double& runStep, const VectorBase<double>& Orig,
-                  const VectorBase<double>& Dirc,
-                  PropertyList& rSys, double& E, double& Prj) {
-        rSys.gDProperty[gCoordinate]=Orig;
-        rSys.gDProperty[gCoordinate].shift(runStep,Dirc);
-        rSys.gDProperty[gGradient]=0.;
-        this->runDEvalPtr->Update();
-        E=0.;
-        EG_ListSet(rSys,*(this->runParamPtr),*(this->runIDLSPtr),
-                   *(this->runDEvalPtr),*(this->runGeoPtr),E);
-        ++(this->MinGCount);
-        Prj=dot(rSys.gDProperty[gGradient],Dirc);
-      }
+      CoordinateType  *pUppCoor,*pLowCoor,*pMidCoor;
+      CoordinateType  *pUppGrad,*pLowGrad,*pMidGrad;
+      double          UppY,   LowY,   MidY;
 
-      PropertyList  UppSys, LowSys, MidSys;
-
-      double        UppE,   LowE,   MidE;
-
-      double        UppP,   LowP,   MidP;
-
-    public:
-
-      void SetStep(const double& step) { Step=step; }
-
-      void Import(const PropertyList& iPList, const DistEvalObj& DEval,
-                  const ParamList& PList,
-                  const varVector<IDList<DistEvalObj,GeomType> >& IDLS,
-                  const GeomType& Geo, const double& E) {
-        static_cast<ParentType*>(this)->Import(iPList,DEval,PList,IDLS,Geo,E);
-        this->allocateSystem4Minimize(UppSys,iPList);
-        this->allocateSystem4Minimize(LowSys,iPList);
-        this->allocateSystem4Minimize(MidSys,iPList);
-        this->copySystem4Minimization(UppSys,iPList);
-        this->copySystem4Minimization(LowSys,iPList);
-        this->copySystem4Minimization(MidSys,iPList);
-      }
-
-      int MinimizeAlongLine(const VectorBase<double>& Dirc) {
-        double a=0., b, c=Step;
-        Update(Step,this->MinSys.gDProperty[gCoordinate],Dirc,
-               UppSys,UppE,UppP);
-        double tmd;
-        tmd=DSqrtRelDelta*fabs(this->MinE);
-        if( (UppE<this->MinE) || ((UppP<0)&&(UppE<this->MinE+tmd)) ) {
-          this->MinSys.swap(UppSys);
-          this->MinE=UppE;
-          this->MinMove=Step;
-          Step*=(UppP<0?IvGold:Gold);
-          return 1;
-        } else  Step*=Gold;
-        uint nEval=0;
-        this->copySystem4Minimization(LowSys,this->MinSys);
-        LowE=this->MinE;
-        LowP=this->MinPrj;
-        do {
-          if( (LowP<0)&&(UppP>0) )  b=a+LowP*(a-c)/(UppP-LowP);
-          else                      b=0.5*(a+c);
-          if( (b<=a)||(b>=c) )      b=0.5*(a+c);
-          Update(b,this->MinSys.gDProperty[gCoordinate],Dirc,
-                 MidSys,MidE,MidP);
-          if(MidP>0) {
-            c=b;
-            UppSys.swap(MidSys);
-            UppE=MidE;
-            UppP=MidP;
-          } else {
-            a=b;
-            LowSys.swap(MidSys);
-            LowE=MidE;
-            LowP=MidP;
-          }
-          ++nEval;
-        } while( ((MidE>UppE)||(MidE>LowE))&&(nEval<DefaultMaxIter) );
-        if( (this->MinE-MidE<DRelDelta)||(nEval>=DefaultMaxIter) ) return 2;
-        if(UppE<LowE) {
-          this->MinSys.swap(UppSys);
-          this->MinE=UppE;
-          this->MinMove=c;
-        } else {
-          this->MinSys.swap(LowSys);
-          this->MinE=LowE;
-          this->MinMove=a;
-        }
-        return 1;
-      }
+      LineMinimizerBase() : ParentType(),
+                            pUppCoor(NULL), pLowCoor(NULL), pMidCoor(NULL),
+                            pUppGrad(NULL), pLowGrad(NULL), pMidGrad(NULL),
+                            UppY(0.), LowY(0.), MidY(0.) {}
 
   };
 
-  template <typename DistEvalObj, typename GeomType>
-  const uint CoarseMinimizerKern<DistEvalObj,GeomType>::DefaultMaxIter=20;
+  template <>
+  class LineMinimizer<varVector<double>,varVector<double> >
+    : public MinimizerKern<varVector<double>,varVector<double> > {
+    public:
+      typedef varVector<double> DVecType;
+      typedef LineMinimizer<DVecType,DVecType>    Type;
+      typedef MinimizerKern<DVecType,DVecType>    ParentType;
+  };
 
 }
 
