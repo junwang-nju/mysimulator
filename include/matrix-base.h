3d6407c1ec4eb57646aba6a26b45fbe45b08e25f
Jun Wang <jwang@dellnote.nju.edu.cn>
2010-01-29 23:26:16 +0800

    finish base for matrix
    The matrix base class is finished.

---
+++ include/matrix-base.h
@@ -0,0 +1,88 @@
+
+#ifndef _Matrix_Base_H_
+#define _Matrix_Base_H_
+
+#include "data-pack.h"
+
+namespace std {
+  enum MatrixFormat {
+    UnknownFormat=-1,
+    Rectangle,
+    Square,
+    Triangle,
+    Band,
+    TriangleBand,
+    NumberMatrixFormat
+  };
+  enum MatrixDataOrder {
+    UnknownOrder=-1,
+    COrder,
+    FortranOrder,
+    DiagonalOrder,
+    NumberMatrixDataOrder
+  };
+  enum MatrixTransposeType {
+    UnknowTranspose=-1,
+    NoTranspose,
+    WithTranspose
+  };
+  enum CommonInfoItems {
+    MatrixType=0,
+    ExpectOrder,
+    TransposeState,
+    ActualOrder,
+    NumberRows,
+    NumberColumns,
+    NumberCommonItems
+  };
+  template <typename T, template<typename> class VecType>
+  class MatrixBase : DataPack<T,VecType,VecType,VecType> {
+    public:
+      typedef T   DataType;
+      typedef MatrixBase<T,VecType>   Type;
+      typedef DataPack<T,VecType,VecType,VecType> ParentType;
+      typedef T& (*GetElemFuncType)(VecType<refVector<T> >&,
+                                    unsigned int,unsigned int,T&);
+    protected:
+      T OtherElems;
+      GetElemFuncType getElem;
+    public:
+      MatrixBase() : ParentType(), OtherElems(), getElem(NULL) {}
+      MatrixBase(const Type& MB) {
+        myError("Cannot create from Matrix base");
+      }
+      virtual ~MatrixBase() {
+        getElem=NULL;
+        this->info()[NumberColumns]=uZero;
+        this->info()[NumberRows]=uZero;
+        this->info()[ActualOrder]=UnknownOrder;
+        this->info()[TransposeState]=UnknowTranspose;
+        this->info()[ExpectOrder]=UnknownOrder;
+      }
+      Type& operator=(const Type& MB) {
+        unsigned int m=(NumRow()<MB.NumRow()?NumRow():MB.NumRow());
+        unsigned int n=(NumCol()<MB.NumCol()?NumCol():MB.NumCol());
+        for(unsigned int i=0;i<m;++i)
+        for(unsigned int j=0;j<n;++j) operator()(i,j)=MB(i,j);
+        return *this;
+      }
+      template <typename inputT>
+      Type& operator=(const inputT& V) { this->data()=V; return *this; }
+      void clear();
+      const unsigned int& NumRow() const;
+      const unsigned int& NumCol() const;
+      const int& MatrixOrder() const;
+      const int& MatrixTransposeState() const;
+      const int& MatrixActualOrder() const;
+      void SetSize(const unsigned int NR, const unsigned int NC);
+      void SetOrder(const int Ord);
+      void SetTranpose(const int Trans);
+      void SetActualOrder(const int AOrd);
+      void AssignGetMethod(const GetElemFuncType& iGMethod);
+      T& operator()(const unsigned int I, const unsigned int J);
+      const T& operator()(const unsigned int I, const unsigned int J) const;
+  };
+}
+
+#endif
+
--- lib/matrix-base.cxx
+++ lib/matrix-base.cxx
@@ -3,69 +3,81 @@
 
 namespace std {
 
-  template <typename T>
-  void MatrixBase<T>::clear() {
-    getUnit=NULL;
-    NRow=NCol=uZero;
-    Order=UnknownOrder;
-    Transpose=UnknownTranspose;
-    DataOrder=UnknownOrder;
-    Data.clear();
-    Lines.clear();
+  template <typename T, template<typename> class VecType>
+  void MatrixBase<T,VecType>::clear() {
+    getElem=NULL;
+    this->info()[NumberColumns]=uZero;
+    this->info()[NumberRows]=uZero;
+    this->info()[ActualOrder]=UnknownOrder;
+    this->info()[TransposeState]=UnknowTranspose;
+    this->info()[ExpectOrder]=UnknownOrder;
+    this->data().clear();
+    this->structure.clear();
   }
 
-  template <typename T>
-  varVector<T>& MatrixBase<T>::data() { return Data; }
-
-  template <typename T>
-  const varVector<T>& MatrixBase<T>::data() const { return Data; }
-
-  template <typename T>
-  const unsigned int& MatrixBase<T>::NumberRow() const { return NRow; }
-
-  template <typename T>
-  const unsigned int& MatrixBase<T>::NumberColumn() const { return NCol; }
-
-  template <typename T>
-  const int& MatrixBase<T>::MatrixOrder() const { return Order; }
-
-  template <typename T>
-  const int& MatrixBase<T>::MatrixTransposeStatus() const { return Transpose; }
-
-  template <typename T>
-  const int& MatrixBase<T>::MatrixDataOrder() const { return DataOrder; }
-
-  template <typename T>
-  void MatrixBase<T>::SetSize(const unsigned int NR, const unsigned int NC) {
-    NRow=NR;
-    NCol=NC;
+  template <typename T, template<typename> class VecType>
+  const unsigned int& MatrixBase<T,VecType>::NumRow() const {
+    return this->info()[NumberRows];
   }
 
-  template <typename T>
-  void MatrixBase<T>::SetMatrixOrder(const int MOrd) { Order=MOrd; }
-
-  template <typename T>
-  void MatrixBase<T>::SetMatrixTransposeStatus(const int MTrans) {
-    Transpose=MTrans;
+  template <typename T, template<typename> class VecType>
+  const unsigned int& MatrixBase<T,VecType>::NumCol() const {
+    return this->info()[NumberColumns];
   }
 
-  template <typename T>
-  void MatrixBase<T>::SetDataOrder(const int DOrd) { DataOrder=DOrd; }
-
-  template <typename T>
-  void MatrixBase<T>::AssignGetMethod(
-      const MatrixBase<T>::GetUnitType& iGetUnit) {
-    getUnit=iGetUnit;
+  template <typename T, template<typename> class VecType>
+  const int& MatrixBase<T,VecType>::MatrixOrder() const {
+    return this->info()[ExpectOrder];
   }
 
-  template <typename T>
-  T& MatrixBase<T>::operator()(unsigned int I, unsigned int J) {
-    return getUnit(Lines,I,J);
+  template <typename T, template<typename> class VecType>
+  const int& MatrixBase<T,VecType>::MatrixTransposeState() const {
+    return this->info()[TransposeState];
   }
 
-  template <typename T>
-  const T& MatrixBase<T>::operator()(unsigned int I, unsigned int J) const {
-    return getUnit(Lines,I,J);
+  template <typename T, template<typename> class VecType>
+  const int& MatrixBase<T,VecType>::MatrixActualOrder() const {
+    return this->info()[ActualOrder];
+  }
+
+  template <typename T, template<typename> class VecType>
+  void MatrixBase<T,VecType>::SetSize(const unsigned int NR,
+                                      const unsigned int NC) {
+    this->info()[NumberRows]=NR;
+    this->info()[NumberColumns]=NC;
+  }
+
+  template <typename T, template<typename> class VecType>
+  void MatrixBase<T,VecType>::SetOrder(const int Ord) {
+    this->info()[ExpectOrder]=Ord;
+  }
+
+  template <typename T, template<typename> class VecType>
+  void MatrixBase<T,VecType>::SetTranpose(const int Trans) {
+    this->info()[TransposeState]=Trans;
+  }
+
+  template <typename T, template<typename> class VecType>
+  void MatrixBase<T,VecType>::SetActualOrder(const int AOrd) {
+    this->info()[ActualOrder]=AOrd;
+  }
+
+  template <typename T, template<typename> class VecType>
+  void MatrixBase<T,VecType>::AssignGetMethod(
+      const MatrixBase<T,VecType>::GetElemFuncType& iGMethod){
+    getElem=iGMethod;
+  }
+
+  template <typename T, template<typename> class VecType>
+  T& MatrixBase<T,VecType>::operator()(const unsigned int I,
+                                       const unsigned int J) {
+    return getElem(this->structure(),I,J,OtherElems);
+  }
+
+  template <typename T, template<typename> class VecType>
+  const T& MatrixBase<T,VecType>::operator()(const unsigned int I,
+                                             const unsigned int J) const {
+    return getElem(this->structure(),I,J,OtherElems);
   }
 
 }
