9ddba6bdc93b8441914d5942cc632d50b80f3c93
Jun Wang <jwang@dellnote.nju.edu.cn>
2010-02-01 23:04:24 +0800

    add hash function
    hash functions are added.

---
+++ include/hash-func.h
@@ -0,0 +1,27 @@
+
+/**
+ * @file hash-func.h
+ * @brief Functions to generate hash
+ */
+
+#ifndef _Hash_Function_H_
+#define _Hash_Function_H_
+
+#include "error-proc.h"
+
+namespace std {
+  unsigned int hash_rs(const char* str, unsigned int nchar);
+  unsigned int hash_js(const char* str, unsigned int nchar);
+  unsigned int hash_pjw(const char* str, unsigned int nchar);
+  unsigned int hash_elf(const char* str, unsigned int nchar);
+  unsigned int hash_bkdr(const char* str, unsigned int nchar);
+  unsigned int hash_sdbm(const char* str, unsigned int nchar);
+  unsigned int hash_djb(const char* str, unsigned int nchar);
+  unsigned int hash_dek(const char* str, unsigned int nchar);
+  unsigned int hash_bp(const char* str, unsigned int nchar);
+  unsigned int hash_fnv(const char* str, unsigned int nchar);
+  unsigned int hash_ap(const char* str, unsigned int nchar);
+}
+
+#endif
+
