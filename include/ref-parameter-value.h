be4c234d404c648c3f9136e1d5edb8697399b637
Jun Wang <junwang.nju@gmail.com>
2010-02-07 21:17:01 +0800

    add parameter value type
    add two kinds of parameter value.

---
+++ include/ref-parameter-value.h
@@ -0,0 +1,12 @@
+
+#ifndef _Reference_Parameter_Value_H_
+#define _Reference_Parameter_Value_H_
+
+#include "ref-vector.h"
+
+namespace std {
+  typedef refVector<double>   refParameterValue;
+}
+
+#endif
+
---
+++ include/var-parameter-value.h
@@ -0,0 +1,12 @@
+
+#ifndef _Variable_Parameter_Value_H_
+#define _Variable_Parameter_Value_H_
+
+#include "var-vector.h"
+
+namespace std {
+  typedef varVector<double>   varParameterValue;
+}
+
+#endif
+
