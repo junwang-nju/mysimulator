4ce8350f13a24d8826693f829f4585ffdab7429c
Jun Wang <jwang@debian.localdomain>
2010-02-10 18:35:51 +0800

    modify for varable vector
    the implementation for template of var-vector is modified.

--- include/var-vector.h
+++ include/var-vector.h
@@ -7,32 +7,59 @@
 #include "memory.h"
 
 namespace std {
+
   template <typename T>
   class varVector : public ObjectWStorage<VectorBase<T> > {
+
     public:
+
       typedef T DataType;
+
       typedef varVector<T>  Type;
+
       typedef ObjectWStorage<VectorBase<T> >  ParentType;
+
       varVector() : ParentType() { }
+
       varVector(const unsigned int N) : ParentType() { allocate(N); }
+
       varVector(const Type& V) {
         myError("Cannot create from variable vector");
       }
+
       virtual ~varVector() { safe_delete(this->data()); }
+
       Type& operator=(const Type& V) {
         static_cast<ParentType*>(this)->operator=(
             static_cast<const ParentType&>(V));
         return *this;
       }
+
       template <typename inputT>
       Type& operator=(const inputT& V) {
         static_cast<ParentType*>(this)->operator=(V);
         return *this;
       }
-      void clear();
-      void allocate(const unsigned int N);
-      virtual const char* type() const;
+
+      void clear() {
+        safe_delete(this->data());
+        static_cast<ParentType*>(this)->clear();
+      }
+
+      void allocate(const unsigned int N) {
+        static_cast<ParentType*>(this)->RefList().clear();
+        if(this->size()!=N) {
+          safe_delete(this->data());
+          this->data()=new T[N];
+          this->SetSize(N);
+          this->SetHeadTail();
+        }
+      }
+
+      virtual const char* type() const { return "Variable Vector"; }
+
   };
+
 }
 
 #endif
