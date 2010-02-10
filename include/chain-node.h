59b46bb8642303065e2888f590ee1edd025a6279
Jun Wang <jwang@debian.localdomain>
2010-02-10 14:25:01 +0800

    modify template for chain
    include implementation related to template for chain and chain-node.

--- include/chain-node.h
+++ include/chain-node.h
@@ -5,34 +5,70 @@
 #include "error-proc.h"
 
 namespace std {
+
   template <typename T>
   class ChainNode {
+
     public:
+
       typedef ChainNode<T>    Type;
+
     protected:
+
       Type* Parent;
+
       Type* Child;
+
       T     Content;
+
       bool  AllocFg;
+
     public:
+
       ChainNode() : Parent(NULL), Child(NULL), Content(), AllocFg(false) {}
+
       ChainNode(const Type& CN) { myError("Cannot create from chain node"); }
+
       ~ChainNode() {}
+
       Type& operator=(const Type& CN) { Content=CN.Content; return *this; }
+
       template <typename inputT>
       Type& operator=(const inputT& D) { Content=D; return *this; }
-      void clear();
-      Type*& parent();
-      const Type* parent() const;
-      Type*& child();
-      const Type* child() const;
-      T& content();
-      const T& content() const;
-      const bool IsAllocByChain() const;
-      void SetChainAllocFlag(const bool efg);
-      void add_before(Type& node);
-      void remove_self();
+
+      void clear() { Parent=NULL; Child=NULL; }
+
+      Type*& parent() { return Parent; }
+
+      const Type* parent() const { return Parent; }
+
+      Type*& child() { return Child; }
+
+      const Type* child() const { return Child; }
+
+      T& content() { return Content; }
+
+      const T& content() const { return Content; }
+
+      const bool IsAllocByChain() const { return AllocFg; }
+
+      void SetChainAllocFlag(const bool efg) { AllocFg=efg; }
+
+      void add_before(Type& node) {
+        Parent->Child=&node;
+        node.Parent=Parent;
+        node.Child=this;
+        Parent=&node;
+      }
+
+      void remove_self() {
+        Parent->Child=Child;
+        Child->Parent=Parent;
+        clear();
+      }
+
   };
+
 }
 
 #endif
--- include/chain.h
+++ include/chain.h
@@ -3,20 +3,33 @@
 #define _Chain_H_
 
 #include "chain-node.h"
+#include "memory.h"
 
 namespace std {
+
   template <typename T>
   class Chain {
+
     public:
+
       typedef Chain<T>  Type;
+
       typedef ChainNode<T> NodeType;
+
     protected:
+
       NodeType  Root;
+
       NodeType  Head;
+
     public:
+
       Chain() : Root(), Head() { Root.child()=&Head; Head.parent()=&Root; }
+
       Chain(const Type& C) { myError("Cannot create from chain"); }
+
       ~Chain() { clear(); }
+
       Type& operator=(const Type& C) {
         clear();
         NodeType *pCNode=const_cast<NodeType*>(C.Root.child());
@@ -26,11 +39,28 @@ namespace std {
         }
         return *this;
       }
-      void clear();
-      void append(const T& content);
-      void append(ChainNode<T>& node);
-      void remove(ChainNode<T>*& pnode);
+
+      void clear() {
+        NodeType* pNode;
+        while((pNode=Root.child())!=&Head) remove(pNode);
+      }
+
+      void append(const T& content) {
+        NodeType* pNode=new ChainNode<T>;
+        pNode->SetChainAllocFlag(true);
+        Head.add_before(*pNode);
+        pNode->content()=content;
+      }
+
+      void append(ChainNode<T>& node) { Head.add_before(node); }
+
+      void remove(ChainNode<T>*& pnode) {
+        pnode->remove_self();
+        if(pnode->IsAllocByChain())   safe_delete(pnode); 
+      }
+
   };
+
 }
 
 #endif
