c9dba38d44271415a98cad0b80f844a0b5ea8218
Jun Wang <junwang.nju@gmail.com>
2010-02-11 21:10:37 +0800

    finish the test module for vector-impl
    the vector-impl is tested.

--- test/test-vector-impl.cxx
+++ test/test-vector-impl.cxx
@@ -12,6 +12,7 @@ int main() {
   vector_assign(dV2,dV1,4,1,2,0,1);
   for(unsigned int i=0;i<10;++i) cout<<dV2[i]<<" ";
   cout<<endl;
+  cout<<endl;
 
   cout<<"Test -- vector_assign<int>"<<endl;
   int *iV1=new int[10],*iV2=new int[10];
@@ -20,20 +21,24 @@ int main() {
   vector_assign(iV2,iV1,4,1,2,0,1);
   for(unsigned int i=0;i<10;++i) cout<<iV2[i]<<" ";
   cout<<endl;
+  cout<<endl;
 
   cout<<"Test -- vector_scale<double> of value"<<endl;
   vector_scale(dV2,2.5,7,0,1);
   for(unsigned int i=0;i<10;++i) cout<<dV2[i]<<" ";
+  cout<<endl;
   cout<<endl;
 
   cout<<"Test -- vector_scale<double> of array"<<endl;
   vector_scale(dV2,dV1,7,0,1,0,1);
   for(unsigned int i=0;i<10;++i) cout<<dV2[i]<<" ";
   cout<<endl;
+  cout<<endl;
 
   cout<<"Test -- vector_shift<double>"<<endl;
   vector_shift(dV2,2.,dV1,5,0,1,0,1);
   for(unsigned int i=0;i<10;++i) cout<<dV2[i]<<" ";
+  cout<<endl;
   cout<<endl;
 
   cout<<"Test -- vector_scaleshift<double>"<<endl;
@@ -42,15 +47,30 @@ int main() {
   vector_scaleshift(dV2,-1.,2.,dV3,dV1,7,1,1,2,1,0,1);
   for(unsigned int i=0;i<10;++i) cout<<dV2[i]<<" ";
   cout<<endl;
+  cout<<endl;
 
   cout<<"Test -- vector_exchange<double>"<<endl;
   vector_exchange(dV1,dV3,4,2,1,1,2);
   for(unsigned int i=0;i<10;++i) cout<<dV1[i]<<" ";
   cout<<endl;
+  cout<<endl;
 
   cout<<"Test -- vector_exchange<int>"<<endl;
   vector_exchange(iV1,iV2,4,2,1,1,2);
   for(unsigned int i=0;i<10;++i) cout<<iV1[i]<<" ";
+  cout<<endl;
+  cout<<endl;
+
+  cout<<"Test -- vector_dot<double>"<<endl;
+  cout<<vector_dot(dV1,dV3,4,2,1,1,2)<<endl;
+  cout<<endl;
+
+  cout<<"Test -- vector_norm<double>"<<endl;
+  cout<<vector_norm(dV1,5,2,1)<<endl;
+  cout<<endl;
+
+  cout<<"Test -- vector_asum<double>"<<endl;
+  cout<<vector_asum(dV1,5,2,1)<<endl;
   cout<<endl;
 
   return 1;
