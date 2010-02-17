9b3b40ad1e1bfb4719ac42daa7826df5f40ea9b0
Jun Wang <junwang.nju@gmail.com>
2010-02-17 23:47:40 +0800

    finish test for variable parameter key
    finish the test program for variable parameter key.

--- test/test-var-parameter-key.cxx
+++ test/test-var-parameter-key.cxx
@@ -4,6 +4,44 @@
 using namespace std;
 
 int main() {
+  cout<<"Test -- init"<<endl;
+  varParameterKey vPK;
+  varParameterKey vPK1;
+  cout<<endl;
+
+  cout<<"Test -- Set size of index"<<endl;
+  vPK.SetIndexSize(3);
+  vPK1.SetIndexSize(3);
+  cout<<endl;
+
+  cout<<"Test -- assign through index"<<endl;
+  vPK.index()[0]=1.;
+  vPK.index()[1]=100.;
+  vPK.index()[2]=0;
+  cout<<endl;
+
+  cout<<"Test -- assign through another key"<<endl;
+  vPK1=vPK;
+  cout<<vPK1.index()<<endl;
+  cout<<endl;
+
+  cout<<"Test -- read hash"<<endl;
+  cout<<vPK1.hash()<<endl;
+  cout<<endl;
+
+  cout<<"Test -- read from cin"<<endl;
+  cin>>vPK;
+  vPK1=vPK;
+  cout<<vPK1.index()<<endl;
+  cout<<vPK1.hash()<<endl;
+  cout<<endl;
+
+  cout<<"Test -- clear"<<endl;
+  vPK1.clear();
+  cout<<vPK1.index().size()<<endl;
+  cout<<vPK1.hash().size()<<endl;
+  cout<<endl;
+
   return 1;
 }
 
