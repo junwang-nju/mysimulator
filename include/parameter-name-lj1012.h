e6f19591008ab532889ad988ffc99000ab59ada4
Jun Wang <jwang@debian.localdomain>
2010-03-18 13:36:34 +0800

    add interaction of lj-10-12 form
    The interaction with LJ1012 format is added.

---
+++ include/parameter-name-lj1012.h
@@ -0,0 +1,17 @@
+
+#ifndef _Parameter_Name_LJ1012_H_
+#define _Parameter_Name_LJ1012_H_
+
+namespace std {
+
+  enum LJ1012Parametername {
+    LJ1012_EqRadius=0,
+    LJ1012_EqStrength,
+    LJ1012_EqRadiusSQ,
+    LJ1012_SixtyEqStrength,
+    LJ1012_NumberParameter
+  };
+
+}
+
+#endif
