498791e139eb451f33f6010a9c6fbeba2fe02a95
Jun Wang <junwang.nju@gmail.com>
2010-04-11 03:12:54 +0800

    add project for wealth distribution
    It is the basic part for wealth distribution.

---
+++ prj/wealth-dist/basic.cxx
@@ -0,0 +1,32 @@
+
+#include "var-vector.h"
+#include "random-generator.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  const unsigned int NUnit=20000;
+  varVector<double> State(NUnit);
+  State=1;
+  UniformDbRNG urng(23813);
+  unsigned int u,v;
+  double d;
+  for(unsigned int i=0;i<100000000;++i) {
+    u=static_cast<unsigned int>(urng()*NUnit);
+    v=static_cast<unsigned int>(urng()*NUnit)+u+1;
+    if(v>=NUnit)  v-=NUnit;
+    d=urng()*State[u];
+    State[u]-=d;
+    State[v]+=d;
+  }
+  varVector<unsigned int> Hist(1000);
+  Hist=0;
+  for(unsigned int i=0;i<NUnit;++i)
+    if(State[i]<10)
+      ++Hist[static_cast<unsigned int>(State[i]/0.01)];
+  for(unsigned int i=0;i<Hist.size();++i)
+    cout<<i<<"\t"<<Hist[i]<<endl;
+  return 1;
+}
+
