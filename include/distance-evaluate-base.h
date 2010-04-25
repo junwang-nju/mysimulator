ec711f230498eccd6c185b28fb31de480558d9d4
Jun Wang <junwang.nju@gmail.com>
2010-04-25 19:44:00 +0800

    Update base for distance evaluation
    The base of distance evaluation is updated, and tested.

--- include/distance-evaluate-base.h
+++ include/distance-evaluate-base.h
@@ -1,72 +1,79 @@
 
-#ifndef _Distance_Evaluate_H_
-#define _Distance_Evaluate_H_
+#ifndef _Distance_Evaluate_Base_H_
+#define _Distance_Evaluate_Base_H_
 
-#include "vector-base.h"
+#include "storage-state-name.h"
+#include "error-proc.h"
+#include "vector-impl.h"
 
 namespace std {
 
-  template <template <typename> class VecType>
-  class DistanceEvalBase {
+  struct DistanceEvalBase;
 
-    public:
+  void assign(DistanceEvalBase&, const DistanceEvalBase&);
+  void release(DistanceEvalBase&);
 
-      typedef DistanceEvalBase<VecType> Type;
+  struct DistanceEvalBase {
 
-      static const bool IsDistanceEvalMethod;
+    typedef DistanceEvalBase  Type;
 
-    protected:
+    double* displacement;
+    double* distance;
+    unsigned int size;
+    unsigned int state;
 
-      VecType<double> nowDist;
+    DistanceEvalBase()
+      : displacement(NULL), distance(NULL), size(0), state(Unused) {}
+    DistanceEvalBase(const Type&) {
+      myError("Cannot create from Distance Evaluate Base");
+    }
+    Type& operator=(const Type& DEB) { assign(*this,DEB); return *this; }
+    ~DistanceEvalBase() { release(*this); }
 
-      VecType<double> nowDisplacement;
-
-    public:
-
-      DistanceEvalBase() : nowDist(), nowDisplacement() {}
-
-      DistanceEvalBase(const Type&) {
-        myError("Cannot create from Distance Evaluation Base");
-      }
-
-      ~DistanceEvalBase() { clear(); }
-
-      void clear() {
-        nowDist.clear();
-        nowDisplacement.clear();
-      }
-
-      Type& operator=(const Type& DEB) {
-        nowDist=DEB.nowDist;
-        nowDisplacement=DEB.nowDisplacement;
-        return *this;
-      }
-
-      template <template <typename> class iVecType>
-      Type& operator=(const DistanceEvalBase<iVecType>& DEB) {
-        nowDist[0]=DEB.DistanceSQ();
-        nowDisplacement=DEB.Displacement();
-        return *this;
-      }
-
-      const double& DistanceSQ() const { return nowDist[0]; }
-
-      VecType<double>& DistanceSQVec() { return nowDist; }
-
-      const VecType<double>& DistanceSQVec() const { return nowDist; }
-
-      const VecType<double>& Displacement() const { return nowDisplacement; }
-
-      VecType<double>& Displacement() { return nowDisplacement; }
-
-      const unsigned int Dimension() const { return Displacement().size(); }
-
-      virtual void update() = 0;
+    double& operator()() { return *distance; }
+    const double& operator()() const { return *distance; }
 
   };
 
-  template <template <typename> class VecType>
-  const bool DistanceEvalBase<VecType>::IsDistanceEvalMethod=true;
+  bool IsAvailable(const DistanceEvalBase& DEB) {
+    return IsAvailable(DEB.distance);
+  }
+
+  void release(DistanceEvalBase& DEB) {
+    if(DEB.state==Allocated) {
+      safe_delete_array(DEB.displacement);
+      safe_delete(DEB.distance);
+    } else {
+      DEB.displacement=NULL;
+      DEB.distance=NULL;
+    }
+    DEB.size=0;
+    DEB.state=Unused;
+  }
+
+  void assign(DistanceEvalBase& dest, const DistanceEvalBase& src) {
+    assert(IsAvailable(src));
+    assert(IsAvailable(dest));
+    dest()=src();
+    unsigned int n=(dest.size<src.size?dest.size:src.size);
+    assign(dest.displacement,src.displacement,n);
+  }
+
+  void allocate(DistanceEvalBase& DEB, const unsigned int dim) {
+    release(DEB);
+    DEB.displacement=new double[dim];
+    DEB.distance=new double;
+    DEB.size=dim;
+    DEB.state=Allocated;
+  }
+
+  void refer(DistanceEvalBase& dest, const DistanceEvalBase& src) {
+    release(dest);
+    dest.displacement=src.displacement;
+    dest.distance=src.distance;
+    dest.size=src.size;
+    dest.state=Reference;
+  }
 
 }
 
---
+++ test/test-distance-evaluate-base.cxx
@@ -0,0 +1,55 @@
+
+#include "distance-evaluate-base.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  cout<<"Test -- initialize"<<endl;
+  DistanceEvalBase DEB;
+  cout<<endl;
+
+  cout<<"Test -- allocate"<<endl;
+  allocate(DEB,5);
+  cout<<DEB.size<<endl;
+  cout<<endl;
+
+  cout<<"Test -- access (directly)"<<endl;
+  DEB.displacement[0]=12.8;
+  DEB.displacement[1]=2.9;
+  DEB.displacement[2]=1.5;
+  DEB.displacement[3]=-12.;
+  DEB.displacement[4]=23.4;
+  DEB()=normSQ(DEB.displacement,5);
+  cout<<DEB()<<endl;
+  cout<<endl;
+
+  cout<<"Test -- assign from another DistanceEvalBase"<<endl;
+  DistanceEvalBase DEB2;
+  allocate(DEB2,4);
+  assign(DEB2,DEB);
+  for(unsigned int i=0;i<DEB2.size;++i)
+    cout<<DEB2.displacement[i]<<"\t";
+  cout<<endl;
+  cout<<DEB2()<<endl;
+  cout<<endl;
+
+  cout<<"Test -- release"<<endl;
+  release(DEB2);
+  cout<<endl;
+
+  cout<<"Test -- availability check"<<endl;
+  cout<<IsAvailable(DEB)<<endl;
+  cout<<IsAvailable(DEB2)<<endl;
+  cout<<endl;
+
+  cout<<"Test -- refer operation"<<endl;
+  refer(DEB2,DEB);
+  for(unsigned int i=0;i<DEB2.size;++i)
+    cout<<DEB2.displacement[i]<<"\t";
+  cout<<endl;
+  cout<<DEB2()<<endl;
+  cout<<endl;
+
+  return 1;
+}
+
