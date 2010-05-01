f4ca6d5f4df683e57ffde2b538e3793f3135111b
Jun Wang <junwang.nju@gmail.com>
2010-05-01 23:38:40 +0800

    add BoxMuller generator
    The box-muller algorithm is added, but is not finished.

--- include/random-generator-boxmuller.h
+++ include/random-generator-boxmuller.h
@@ -2,87 +2,53 @@
 #ifndef _Random_Generator_BoxMuller_H_
 #define _Random_Generator_BoxMuller_H_
 
-#include "ref-vector.h"
-//#include <cstdlib>
-#include <cmath>
-#include <cassert>
-
 namespace std {
 
-  template <typename UniformDbRNGType>
-  class BoxMuller {
+  template <typename UniformRNG>
+  struct BoxMuller {
 
-    private:
+    typedef BoxMuller<UniformRNG>   Type;
 
-      double X,Y,R2;
+    UniformRNG *urng;
+    double *DbFac;
+    bool *isSecond;
+    double *output;
+    unsigned int state;
 
-      bool isSecond;
-
-      double od;
-
-    public:
-
-      typedef BoxMuller<UniformDbRNGType> Type;
-
-      UniformDbRNGType urng;
-
-      BoxMuller() : isSecond(true), od(0.), urng() {}
-
-      BoxMuller(const unsigned int seed) { Init(seed); }
-
-      BoxMuller(const Type& BM) { assert(false); }
-
-      void Init(const unsigned int seed) {
-        urng.SetWithSeed(seed);
-        isSecond=true;
-        od=0.;
-      }
-
-      void FillArray(double* Array, const unsigned int size) {
-        for(unsigned int i=0;i<size;++i)  Array[i]=GenRandNormal();
-      }
-
-      void FillArray(VectorBase<double>& v) { FillArray(v.data(),v.size()); }
-
-      const double& GenRandNormal() {
-        isSecond=!isSecond;
-        if(isSecond)  od=Y*R2;
-        else {
-          do {
-            X=urng.GenRand_Open0Open1();   X+=X;   X-=1.;
-            Y=urng.GenRand_Open0Open1();   Y+=Y;   Y-=1.;
-            R2=X*X+Y*Y;
-          } while( (R2>1.)||(R2==0) );
-          R2=sqrt(-2.0*log(R2)/R2);
-          od=X*R2;
-        }
-        return od;
-      }
-
-      const double& Default(const double&) { return GenRandNormal(); }
-
-      void saveStatus(ostream& os) {
-        unsigned int op=os.precision();
-        os.precision(20);
-        urng.saveStatus(os);
-        os<<"\t"<<X<<"\t"<<Y<<"\t"<<R2<<"\t"<<isSecond;
-        os.precision(op);
-      }
-
-      void loadStatus(istream& is) {
-        urng.loadStatus(is);
-        is>>X>>Y>>R2>>isSecond;
-      }
+    BoxMuller()
+      : urng(NULL), DbFac(NULL), isSecond(NULL), output(NULL), state(Unused) {}
+    BoxMuller(const Type&) { myError("Cannot create from BoxMuller"); }
+    Type& operator=(const Type& BM) { assign(*this,BM); return *this; }
+    ~BoxMuller() { release(*this); }
 
   };
 
-  template <typename UniformDbRNGType>
-  void BuildRationalVector(const BoxMuller<UniformDbRNGType>& rg,
-                           const VectorBase<double>& iV,
-                           refVector<double>& rV) {
-    rV.refer(iV);
+  template <typename UniformRNG>
+  void release(BoxMuller<UniformRNG>& BM) {
+    if(BM.state==Allocated) {
+      safe_delete(BM.urng);
+      safe_delete_array(BM.DbFac);
+      safe_delete(BM.isSecond);
+      safe_delete(BM.output);
+    } else {
+      BM.urng=NULL;
+      BM.DbFac=NULL;
+      BM.isSecond=NULL;
+      BM.output=NULL;
+    }
+    BM.state=Unused;
   }
 
+  template <typename UniformRNG>
+  void refer(BoxMuller<UniformRNG>& dest, const BoxMuller<UniformRNG>& src) {
+    assert(IsAvailable(src));
+    release(dest);
+    dest.urng=src.urng;
+    dest.DbFac=src.DbFac;
+    dest.isSecond=src.isSecond;
+    dest.output=src.output;
+    dest.state=Reference;
+  }
 
 }
 
