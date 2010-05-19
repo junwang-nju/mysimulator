3e4d395b17af7f711ab53f2f28154d0bdc8bb696
Jun Wang <junwang.nju@gmail.com>
2010-05-19 19:28:08 -0400

    update test for monomoer-propagator-op
    Update the test for operation on monomer propagator

--- test/test-monomer-propagator-op.cxx
+++ test/test-monomer-propagator-op.cxx
@@ -4,6 +4,32 @@
 using namespace std;
 
 int main() {
+  MonomerPropagator MP;
+  allocate(MP);
+  cout<<"Test -- Set as Constant-E velocity verlet"<<endl;
+  Set(MP,ParticleType,ConstantE_VelocityVerlet);
+  cout<<*(MP.UnitKind)<<"\t"<<*(MP.MoveMode)<<endl;
+  cout<<MP.Move.size<<endl;
+  cout<<MP.MSet.size<<endl;
+  cout<<MP.MParam.size<<endl;
+  cout<<endl;
+
+  cout<<"Test -- set as Berendsen velocity verlet"<<endl;
+  Set(MP,ParticleType,Berendsen_VelocityVerlet);
+  cout<<*(MP.UnitKind)<<"\t"<<*(MP.MoveMode)<<endl;
+  cout<<MP.Move.size<<endl;
+  cout<<MP.MSet.size<<endl;
+  cout<<MP.MParam.size<<endl;
+  cout<<endl;
+
+  cout<<"Test -- set as Langevin velocity verlet"<<endl;
+  Set(MP,ParticleType,Langevin_VelocityVerlet);
+  cout<<*(MP.UnitKind)<<"\t"<<*(MP.MoveMode)<<endl;
+  cout<<MP.Move.size<<endl;
+  cout<<MP.MSet.size<<endl;
+  cout<<MP.MParam.size<<endl;
+  cout<<endl;
+
   return 0;
 }
 
