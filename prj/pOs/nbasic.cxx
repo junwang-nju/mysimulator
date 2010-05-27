2accda3b1428e2f31ac5838e9464e7d071c2a8cf
Jun Wang <junwang.nju@gmail.com>
2010-05-27 11:07:26 -0400

    update pOs with propagator
    The pOs project is updated with propagator and interaction now. It is an
    extension of previous project.

---
+++ prj/pOs/nbasic.cxx
@@ -0,0 +1,168 @@
+
+#include "propagator-op.h"
+#include "distance-evaluate-direct.h"
+#include "free-space.h"
+#include <iostream>
+using namespace std;
+
+template <typename DistEvalMethod, typename GeomType>
+void OutFunc(ostream& os, const Propagator<DistEvalMethod,GeomType>& P,
+             InteractionMethod<DistEvalMethod,GeomType>* IMLst,
+             const Vector<double>* Coor, const Vector<double>* Vel,
+             const Vector<double>* Grad, const Vector<double>* Mass,
+             const Vector<double>* dMask,
+             const Vector<unsigned int>* IdxLst,
+             const Vector<UniqueParameter> *PrmLst,
+             const unsigned int nunit, const unsigned int nlst,
+             DistEvalMethod& DEval, const GeomType& Geo) {
+  double E,kE;
+  E=kE=0.;
+  EFunc(Coor,IdxLst,PrmLst,IMLst,6,DEval,Geo,E);
+  for(unsigned int i=0;i<nunit;++i) kE+=normSQ(Vel[i])*Mass[i][0]*0.5;
+  MassCenter(Coor,IdxLst[nlst-1](),nunit-1,(*(IMLst[nlst-1].tmpvec))[0]);
+  cout<<P.GParam[NowTime].d<<"\t"<<(*(IMLst[nlst-1].tmpvec))[0];
+  cout<<"\t"<<E<<"\t"<<kE;
+  cout<<"\t"<<Coor[nunit-1]<<endl;
+}
+
+int main() {
+  const unsigned int NMer=50;
+  const unsigned int NInter=NMer-1+((NMer-2)*(NMer-1))/2+NMer+1;
+
+  PropertyList<double> Coor,Vel,Grad,Mass,iMass,dMask,RandV;
+  Vector<unsigned int> sz;
+  allocate(sz,NMer+1);
+  assign(sz,3);
+  allocate(Coor,sz);
+  allocate(Vel,sz);
+  allocate(Grad,sz);
+  allocate(Mass,sz);
+  allocate(iMass,sz);
+  allocate(dMask,sz);
+  allocate(RandV,sz);
+
+  assign(Coor,0.);
+  for(unsigned int i=0;i<NMer;++i) {
+    Coor[i][0]=i-(NMer-1.)*0.5;
+    Coor[i][1]=8;
+  }
+  assign(Vel,0.);
+  assign(Mass,1.);
+  assign(iMass,1.);
+  assign(dMask,1.);
+  assign(dMask[NMer],0.);
+
+  Vector<InteractionMethod<DistanceEvalDirect,FreeSpace> > IMLst;
+  allocate(IMLst,NInter);
+  for(unsigned int i=0;i<NInter;++i)  allocate(IMLst[i]);
+  unsigned int n;
+  n=0;
+  for(unsigned int i=0;i<NMer-1;++i,++n)
+    Set(IMLst[n],PairwiseHarmonic);
+  for(unsigned int i=0;i<((NMer-2)*(NMer-1))/2;++i,++n)
+    Set(IMLst[n],PairwiseCoreLJ612);
+  for(unsigned int i=0;i<NMer;++i,++n)
+    Set(IMLst[n],PairwiseCoreExpandCoreLJ612);
+  Set(IMLst[n],CentroidHarmonic,3);
+
+  Vector<Vector<UniqueParameter> > ParamLst;
+  allocate(ParamLst,NInter);
+  n=0;
+  for(unsigned int i=0;i<NMer-1;++i,++n)
+    allocate(ParamLst[n],HarmonicNumberParameter);
+  for(unsigned int i=0;i<((NMer-1)*(NMer-2))/2;++i,++n)
+    allocate(ParamLst[n],CoreLJ612NumberParameter);
+  for(unsigned int i=0;i<NMer;++i,++n)
+    allocate(ParamLst[n],CoreExpCoreLJ612NumberParameter);
+  allocate(ParamLst[n],CentroidHarmonicNumberParameter);
+  n=0;
+  for(unsigned int i=0;i<NMer-1;++i,++n) {
+    ParamLst[n][HarmonicEqLength]=1.;
+    ParamLst[n][HarmonicEqStrength]=100.;
+    GenerateParameterHarmonic(ParamLst[n]);
+  }
+  for(unsigned int i=0;i<((NMer-1)*(NMer-2))/2;++i,++n) {
+    ParamLst[n][CoreLJ612Radius]=0.5;
+    ParamLst[n][CoreLJ612EnergyDepth]=1.;
+    GenerateParameterCoreLJ612(ParamLst[n]);
+  }
+  for(unsigned int i=0;i<NMer;++i,++n) {
+    ParamLst[n][CoreExpCoreRadius]=5;
+    //ParamLst[i][CoreExpLJ612EqRadius]=1.;
+    //ParamLst[i][CoreExpLJ612EqEnergyDepth]=1.;
+    ParamLst[n][CoreExpCoreLJ612Radius]=1.;
+    ParamLst[n][CoreExpCoreLJ612EnergyDepth]=1.;
+    GenerateParameterCoreExpandLJ612(ParamLst[n]);
+  }
+  ParamLst[n][CentroidHarmonicEqLength]=8.;
+  ParamLst[n][CentroidHarmonicEqStrength]=10.;
+  GenerateParameterCentroidHarmonic(ParamLst[n]);
+
+  PropertyList<unsigned int> IdxLst;
+  allocate(sz,NInter);
+  assign(sz,2);
+  sz[NInter-1]=NMer+1+4;
+  allocate(IdxLst,sz);
+  n=0;
+  for(unsigned int i=0;i<NMer-1;++i,++n) {
+    IdxLst[n][0]=i;
+    IdxLst[n][1]=i+1;
+  }
+  for(unsigned int i=0;i<NMer;++i)
+  for(unsigned int j=i+2;j<NMer;++j,++n) {
+    IdxLst[n][0]=i;
+    IdxLst[n][1]=j;
+  }
+  for(unsigned int i=0;i<NMer;++i,++n) {
+    IdxLst[n][0]=i;
+    IdxLst[n][1]=NMer;
+  }
+  for(unsigned int i=0;i<=NMer;++i)
+    IdxLst[n][i]=i;
+  IdxLst[n][NMer+1]=0;    IdxLst[n][NMer+2]=NMer;
+  IdxLst[n][NMer+3]=NMer; IdxLst[n][NMer+4]=1;
+
+  DistanceEvalDirect DEval;
+  FreeSpace FS;
+  allocate(DEval,3,NMer);
+  allocate(FS,3);
+
+  assign(Grad,0.);
+  GFunc(Coor.structure,IdxLst.structure,ParamLst(),IMLst(),NInter,
+        DEval,FS,Grad.structure);
+
+  GaussianRNG gng;
+  allocate(gng);
+  init(gng,1237913);
+
+  Propagator<DistanceEvalDirect,FreeSpace>  P;
+  allocate(P);
+  Set(P,Langevin_VelocityVerlet);
+  allocate(sz,NMer+1);
+  assign(sz,ParticleType);
+  buildUnit(P,sz);
+
+  setparameter(P,TimeStepCommon,0.001);
+  setparameter(P,StartTimeCommon,0.);
+  setparameter(P,TotalTimeCommon,10000.0);
+  setparameter(P,OutputTimeIntervalCommon,0.1);
+  setparameter(P,TemperatureLV,0.1);
+  setparameter(P,ViscosityLV,0.5);
+  setparameter(P,GaussianRNGPointerLV,gng);
+  for(unsigned int i=0;i<NMer+1;++i) {
+    setparameter(P.Unit[i],HydrodynamicRadiusPLV,1.0);
+    setparameter(P.Unit[i],RandomVelocityPointerPLV,RandV[i][0]);
+  }
+
+  synchronize(P,iMass.structure,dMask.structure,Vel.structure,NMer+1);
+
+  P.FOutput=OutFunc<DistanceEvalDirect,FreeSpace>;
+  P.HOutput=NULL;
+
+  Run(P,Coor.structure,Vel.structure,Grad.structure,Mass.structure,
+      dMask.structure,IMLst(),IdxLst.structure,ParamLst(),NMer+1,NInter,
+      DEval,FS);
+
+  return 0;
+}
+
