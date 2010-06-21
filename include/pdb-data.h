18a5040ae360108dcda5565d5fe6d66ec184ed6f
Jun Wang <junwang.nju@gmail.com>
2010-06-21 11:45:23 -0400

    update pdb data structure
    The import function for pdb-data is finished, but not tested.

--- include/pdb-data.h
+++ include/pdb-data.h
@@ -38,6 +38,10 @@ namespace std{
     ENtFile
   };
 
+  bool IsPDBProperty(const char* line, const char* signature) {
+    return strncmp(line,signature,6)==0;
+  }
+
   void Import(const char* pdbcode, PDBData& pdb, const char* dataRoot="./",
               const unsigned int ftype=PDBFile) {
     char runname[256];
@@ -52,8 +56,32 @@ namespace std{
     if(ifs.fail())  myError("Something Wrong with Input data)");
     char lbuff[1024];
     lbuff[1023]=0;
-    int modelCount=0;
+    unsigned int modelCount=0;
     int nowResID=-1, AACount=-1, AtomCount;
+    pdb.model.push_back(ModelType());
+    while(!ifs.eof()) {
+      ifs.getline(lbuff,1023);
+      if(IsPDBProperty(lbuff,"MODEL ")) {
+      } else if(IsPDBProperty(lbuff,"ENDMDL")) {
+        ++modelCount;
+        AACount=-1;
+      } else if(IsPDBProperty(lbuff,"ATOM  ")) {
+        if(nowResID!=atoi(lbuff+22)) {
+          nowResID=atoi(lbuff+22);
+          pdb.model[modelCount].push_back(AminoAcidType());
+          ++AACount;
+          Set(pdb.model[modelCount][AACount].name,lbuff+17);
+        }
+        pdb.model[modelCount][AACount].data.push_back(AtomType());
+        Set(pdb.model[modelCount][AACount].data[AtomCount],lbuff+12);
+        Set(pdb.model[modelCount][AACount].data[AtomCount],
+            atoi(lbuff+30),atoi(lbuff+38),atoi(lbuff+46));
+        pdb.model[modelCount][AACount].data[AtomCount].TempFactor=
+            (strncmp(lbuff+60,"      ",6)!=0?atoi(lbuff+0):0.);
+        ++AtomCount;
+      }
+    }
+    ifs.close();
   }
 
 }
