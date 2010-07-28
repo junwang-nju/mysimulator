268118a9f2a8b854fd6c1b4a1bedaadef72af3f2
JunWang <junwang.nju@gmail.com>
2010-07-29 01:09:10 +0800

    add project for partition zero
    The project to calculate partition zero for lattice models are added.

---
+++ prj/zero/2d-conf-enum.cxx
@@ -0,0 +1,22 @@
+
+#include "vector.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+  unsigned int L=9;
+  unsigned int L1=8;
+
+  Vector<int> dirc;
+  Vector<int> X,Y;
+  unsigned int YTurnHead;
+
+  allocate(dirc,L1);
+  allocate(X,L);
+  allocate(Y,L);
+  X[0]=0;     Y[0]=0;
+  X[1]=1;     Y[1]=0;
+  for(unsigned int i=0;i<L1;++i)  dirc[i]=-1;
+  return 0;
+}
+
