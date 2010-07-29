f57893492e17fc0ef1db0b7deebe5f7891df928c
JunWang <junwang.nju@gmail.com>
2010-07-29 13:16:21 +0800

    update code for conformation enumeration
    The code for conformation enumeration is updated.

--- prj/zero/2d-conf-enum.cxx
+++ prj/zero/2d-conf-enum.cxx
@@ -9,14 +9,18 @@ int main() {
 
   Vector<int> dirc;
   Vector<int> X,Y;
-  unsigned int YTurnHead;
+  unsigned int YTurnHead,runHead;
 
   allocate(dirc,L1);
   allocate(X,L);
   allocate(Y,L);
   X[0]=0;     Y[0]=0;
   X[1]=1;     Y[1]=0;
-  for(unsigned int i=0;i<L1;++i)  dirc[i]=-1;
+  for(unsigned int i=0;i<L1;++i)  dirc[i]=0;
+  YTurnHead=L1;
+  runHead=L1;
+  
+  
   return 0;
 }
 
