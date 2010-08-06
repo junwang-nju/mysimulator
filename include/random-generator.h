57cd5f640f3e777a45f3d78ee49418c0a9bc3e31
Jun Wang <junwang.nju@gmail.com>
2010-08-06 11:13:21 -0400

    add document for random generator
    The document for basic head related to random generator is added.

--- include/random-generator.h
+++ include/random-generator.h
@@ -1,3 +1,11 @@
+/**
+ * @file random-generator.h
+ * @brief random generator
+ *
+ * This file contains the head files for random generator.
+ *
+ * @author Jun Wang (junwang.nju@gmail.com)
+ */
 
 #ifndef _Random_Generator_H_
 #define _Random_Generator_H_
@@ -8,7 +16,22 @@
 
 namespace std {
 
+  /**
+   * @brief definition of random generator with uniform distribution
+   *
+   * This kind of generator may produce double-type value which is
+   * uniformly distributed in [0,1). It is implemented with dSFMT method.
+   */
   typedef dSFMT<216091> UniformDbRNG;
+
+  /**
+   * @brief definition of random generator with gaussian distribution
+   *
+   * This kind of generator may produce double-type value which takes
+   * gaussian distribution with zero average and unit deviation. It is
+   * implemented with Box-Muller method. The involved random generator
+   * with uniform distribution is UniformDbRNG. 
+   */
   typedef BoxMuller<UniformDbRNG> GaussianRNG;
 
 }
