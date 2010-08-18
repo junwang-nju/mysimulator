79f007b56e025bbfb52b37be8abc3ecfc85be04b
Jun <junwang.nju@gmail.com>
2010-08-18 19:13:31 +0800

    add doc for random generator
    The document for random generators are added, but not finished

--- include/random-generator-generic.h
+++ include/random-generator-generic.h
@@ -1,4 +1,14 @@
 
+/**
+ * @file random-generator-generic.h
+ * @brief Modules for generic random generators
+ *
+ * This file contains some procedures which are generic for
+ * all random generators.
+ *
+ * @author Jun Wang (junwang.nju@gmail.com)
+ */
+
 #ifndef _Random_Generator_Generic_H_
 #define _Random_Generator_Generic_H_
 
@@ -7,6 +17,18 @@
 
 namespace std {
 
+  /**
+   * @brief initiate a generator with present time.
+   *
+   * The present time is converted as \c unsigned \c int which records
+   * the seconds starting from a certain time to now. This \c unsigned 
+   * \c int is used as the seed to initiate the random generator
+   *
+   * RNG is the type of the random generator
+   *
+   * @param rg [out] the random generator to be initialized
+   * @return nothing
+   */
   template <typename RNG>
   void initWithTime(RNG& rg) { rg.Init(static_cast<unsigned int>(time(0))); }
 
