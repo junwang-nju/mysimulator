c0f983d9a4debc510cfdd40a8954eac90ad0bcb4
Jun Wang <junwang.nju@gmail.com>
2010-08-20 11:54:00 -0400

    add doc for functional corelj612
    The document for the functional with corelj612 type is added.

--- include/functional-corelj612.h
+++ include/functional-corelj612.h
@@ -1,4 +1,15 @@
 
+/**
+ * @file functional-corelj612.h
+ * @brief functional format for core function with Lennard-Jone 6-12 form
+ *
+ * This file gives the functions to calculate value and/or gradient of
+ * core function with Lennard-Jones 6-12 form. The distance and parameter
+ * are inputs for the corresponding functions.
+ *
+ * @author Jun Wang (junwang.nju@gmail.com)
+ */
+
 #ifndef _Functional_CoreLJ612_H_
 #define _Functional_CoreLJ612_H_
 
@@ -6,6 +17,18 @@
 
 namespace std {
 
+  /**
+   * @brief calculate value of CoreLJ612-type function
+   *
+   * Based on the input distance information, the corresponding value of
+   * CoreLJ612-type function is calculated. The corresponding parameters
+   * are also included for flexibility.
+   *
+   * @param dsq [in] the square of distance
+   * @param prm [in] the array of parameters to define CoreLJ612-type function
+   * @param func [out] the output function value
+   * @param nothing
+   */
   void FuncCoreLJ612(const double dsq, const UniqueParameter* prm,
                      double& func) {
     if(dsq>prm[CoreLJ612RadiusSQ].d)  func=0.;
@@ -16,6 +39,19 @@ namespace std {
     }
   }
 
+  /**
+   * @brief differentiation for CoreLJ612-type interaction
+   *
+   * Based on the input distance information, the differentiation of
+   * CoreLJ612-type function is calculated. The corresponding parameters
+   * are also included for flexibility.
+   *
+   * @param dsq [in] the square of distance
+   * @param prm [in] the array of parameters to define CoreLJ612-type function
+   * @param diff [out] the output differentiation value
+   * @return nothing
+   */
+   */
   void DiffCoreLJ612(const double dsq, const UniqueParameter* prm,
                      double& diff) {
     if(dsq>prm[CoreLJ612RadiusSQ].d)  diff=0.;
@@ -27,6 +63,19 @@ namespace std {
     }
   }
 
+  /**
+   * @brief function value and differentiation for CoreLJ612-type function
+   *
+   * Based on the distance information, the value and differentiation of
+   * CoreLJ612-type function are calculated at the same time. The
+   * corresponding parameters are also included for flexibility.
+   *
+   * @param dsq [in] the square of distance
+   * @param prm [in] the array to define CoreLJ612-type function
+   * @param func [out] the output function value
+   * @param diff [out] the output differentiation value
+   * @return nothing
+   */
   void BothCoreLJ612(const double dsq, const UniqueParameter* prm,
                      double& func, double& diff) {
     if(dsq>prm[CoreLJ612RadiusSQ].d)  func=diff=0.;
