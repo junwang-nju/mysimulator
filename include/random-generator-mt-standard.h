a5f00caf4734beadeaf7d26dd8833a61d8f10571
Jun Wang <junwang.nju@gmail.com>
2010-08-24 11:08:45 -0400

    doc updated
    The document for random-generator with MT-Standard Method is updated.

--- include/random-generator-mt-standard.h
+++ include/random-generator-mt-standard.h
@@ -117,15 +117,57 @@ namespace std {
      * @return the reference to the resultant generator
      */
     Type& operator=(const Type& MS) { assign(*this,MS); return *this; }
+    /**
+     * @brief destructor of MT_Standard generator
+     *
+     * it is implemented with release operation.
+     */
     ~MT_Standard() { release(*this); }
 
+    /**
+     * @brief initiate generator with a \c unsigned \c int
+     *
+     * It is implemented with the init operation in std namespace.
+     *
+     * @param seed [in] the \c unsigned \c int as the seed
+     * @return nothing.
+     */ 
     void Init(const unsigned int seed) { std::init(*this,seed); }
+    /**
+     * @brief initiate generator with \c unsigned \c int array
+     *
+     * It is implemented with the init operation using array.
+     *
+     * @param key [in] the \c unsigned \c int array containing the seeds
+     * @param len [in] the number of seeds
+     * @param off [in] the offset of the fisrt seed in array key, which takes
+     *                 the default value uZero
+     * @param step [in] the step between seeds in array key, which takes
+     *                  the default value uOne
+     * @return nothing
+     */
     void Init(const unsigned int* key, const unsigned int len,
               const unsigned int off=uZero, const unsigned int step=uOne) {
       std::init(*this,key,len,off,step);
     }
+    /**
+     * @brief initiate generator with vector
+     *
+     * it is implemented with init operation using vector
+     *
+     * @param key [in] the vector containing seeds
+     * @return nothing
+     */
     void Init(const Vector<unsigned int>& key) { std::init(*this,key); }
 
+    /**
+     * @brief generate a \c unsigned \c int random number (32-bit)
+     *
+     * This is the basic generator of MT_Standard generator. It is
+     * a complex operation for the internal storage.
+     *
+     * @return a 32-bit random number
+     */
     const unsigned int& UInt32() {
       unsigned int& ri=*mti;
       unsigned int& u=output->u[0];
@@ -151,11 +193,26 @@ namespace std {
       return u;
     }
 
+    /**
+     * @brief generate a 31-bit int
+     *
+     * Just pick the lower 31-bit part of 32-bit \c unsigned \c int random
+     * number.
+     *
+     * @return the 31-bit random number
+     */
     const int& Int31() {
       output->i[0]=static_cast<int>(UInt32()>>1);
       return output->i[0];
     }
 
+    /**
+     * @brief generate a \c double value in region [0,1]
+     *
+     * it is implemented by a proper conversion from \c int to \c double.
+     *
+     * @return the \c double value in the region [0,1] 
+     */
     const double& DoubleClose0Close1() {
       UInt32();
       output->d[0]=static_cast<double>(output->i[0])*(1.0/4294967295.0)
@@ -163,6 +220,13 @@ namespace std {
       return output->d[0];
     }
 
+    /**
+     * @brief generate a \c double value in region [0,1)
+     *
+     * it is implemented by a proper conversion from \c int to \c double.
+     *
+     * @return the \c double value in region [0,1)
+     */
     const double& DoubleClose0Open1() {
       UInt32();
       output->d[0]=static_cast<double>(output->i[0])*(1.0/4294967296.0)
@@ -170,6 +234,13 @@ namespace std {
       return output->d[0];
     }
 
+    /**
+     * @brief generate a \c double value in region (0,1)
+     *
+     * it is implemented by a proper conversion from \c int to \c double
+     *
+     * @return a \c double value in region (0,1)
+     */
     const double& DoubleOpen0Open1() {
       UInt32();
       output->d[0]=static_cast<double>(output->i[0])*(1.0/4294967296.0)
@@ -177,6 +248,16 @@ namespace std {
       return output->d[0];
     }
 
+    /**
+     * @brief generate a 53-bit \c double value in [0,1)
+     *
+     * It is a certain combination of two 32-bit \c int.
+     *
+     * @return the 53-bit \c double value
+     * @note It is assumed that the conversion from \c long to \c double is
+     *       fast. This kind of implementation is believed to be fast, but the
+     *       efficiency is not tested.
+     */
     const double& Double53Close0Open1() {
       long x,y;
       x=static_cast<long>(UInt32()>>5);
@@ -185,6 +266,15 @@ namespace std {
       return output->d[0];
     }
 
+    /**
+     * @brief generate a 53-bit \c double value in [0,1) in slow mode
+     *
+     * It is a combination of two 32-bit \c int.
+     *
+     * @return the 53-bit \c double value
+     * @note the conversion from \c unsigned \c int to \c double is believed
+     *       to be slow. The efficiency is not tested.
+     */
     const double& Double53Close0Open1Slow() {
       unsigned int x,y;
       x=UInt32()>>5;
@@ -194,6 +284,15 @@ namespace std {
       return output->d[0];
     }
 
+    /**
+     * @brief generate 63-bit \c long \c double in region [0,1)
+     *
+     * It is a combination with two \c int.
+     *
+     * @return the 63-bit \c long \c double value
+     * @note the conversion from \c long to \c long \c double is believed
+     *       to be fast. The effeciency is not tested.
+     */
     const long double& LDouble63Close0Open1() {
       long x,y;
       x=static_cast<long>(UInt32());
@@ -204,6 +303,15 @@ namespace std {
       return output->ld;
     }
 
+    /**
+     * @brief generate a 63-bit \c long \c double in region [0,1) in slow mode
+     *
+     * It is a combination with two \c int.
+     *
+     * @return the 63-bit \c long \c double value.
+     * @note the conversion from \c unsigned \c int to \c long \c double
+     *       is believed to be fast. The efficiency is not tested.
+     */
     const long double& LDouble63Close0Open1Slow() {
       unsigned int x,y;
       x=UInt32();
@@ -215,20 +323,79 @@ namespace std {
 
   };
 
+  /**
+   * @brief the size of internal storage in MT_Standard generator
+   */
   const unsigned int MT_Standard::N=624;
+  /**
+   * @brief the wrap size in MT_Standard generator
+   */
   const unsigned int MT_Standard::M=397;
+  /**
+   * @brief the difference between N and M in MT_Standard
+   */
   const unsigned int MT_Standard::dNM=MT_Standard::N-MT_Standard::M;
+  /**
+   * @brief the temporary quantity of N mius 1 in MT_Standard
+   */
   const unsigned int MT_Standard::Nm1=MT_Standard::N-1;
+  /**
+   * @brief the temporary quantity of M minus 1 in MT_Standard
+   */
   const unsigned int MT_Standard::Mm1=MT_Standard::M-1;
+  /**
+   * @brief the quantity for bit operation in MT_Standard
+   */
   const unsigned int MT_Standard::MatrixA=0x9908B0DFUL;
+  /**
+   * @brief the mask to pick the upper bit in \c unsigned \c int
+   */
   const unsigned int MT_Standard::UppMask=0x80000000UL;
+  /**
+   * @brief the mask to pick the lower 31 bits in \c unsigned \c int
+   */
   const unsigned int MT_Standard::LowMask=0x7FFFFFFFUL;
+  /**
+   * @brief the array storing the temporary quantities
+   */ 
   const unsigned int MT_Standard::Mag01[2]={0x0UL,MatrixA};
+  /**
+   * @brief the mask to pick 32-bit \c unsigned \c int
+   *
+   * @note This is used to pick 32-bit part in 64-bit system.
+   */
   const unsigned int MT_Standard::Mask32b=0xFFFFFFFFUL;
 
+  /**
+   * @brief check the availability of MT_Standard generator
+   *
+   * It is implemented by checking the availability of the internal storage.
+   *
+   * @param MS [in] the input MT_Standard generator
+   * @return the flag indicating if the input MT_Standard generator is valid
+   */
   bool IsAvailable(const MT_Standard& MS) { return IsAvailable(MS.mt); }
-  bool IsRandomGenerator(const MT_Standard&) { return true; }
+  /**
+   * @brief check if MT_Standard is a random generator
+   *
+   * The MT_Standard generator is certainly a random generator.
+   *
+   * @param MS [in] the input MT_Standard generator
+   * @return true since MT_Standard generator is a random generator
+   */
+  bool IsRandomGenerator(const MT_Standard& MS) { return true; }
 
+  /**
+   * @brief assign MT_Standard generator from another
+   *
+   * Just copy the internal storage from that in the source generator.
+   * The location indicator and output are also copied. The generators
+   * dest and src are checked for their availability before this operation.
+   *
+   * @param dest [out] the MT_Standard generator to accept the input
+   * @param src [in] the MT_Standard generator containing the information
+   * @return nothing 
+   */
   void assign(MT_Standard& dest, const MT_Standard& src) {
     assert(IsAvailable(dest));
     assert(IsAvailable(src));
@@ -237,6 +404,15 @@ namespace std {
     *(dest.output)=*(src.output);
   }
 
+  /**
+   * @brief release a MT_Standard generator
+   *
+   * The internal storage, the internal location and the output variable
+   * are released based on the state od storage.
+   *
+   * @param MS [in,out] the MT_Standard generator to be released
+   * @return nothing 
+   */
   void release(MT_Standard& MS) {
     if(MS.state==Allocated) {
       safe_delete_array(MS.mt);
@@ -250,6 +426,17 @@ namespace std {
     MS.state=Unused;
   }
 
+  /**
+   * @brief allocate a MT_Standard generator
+   *
+   * The internal storage, the location flag and the output are allocated.
+   * The corresponding state is set as Allocated. Then, the generator is
+   * initiated with a default seed of 5489. The input generator is
+   * released before this operation.
+   *
+   * @param MS [in,out] the MT_Standard generator to be allocated
+   * @return nothing
+   */ 
   void allocate(MT_Standard& MS) {
     release(MS);
     MS.mt=new unsigned int[MT_Standard::N];
@@ -259,6 +446,17 @@ namespace std {
     init(MS,5489UL);
   }
 
+  /**
+   * @brief refer to another MT_Standard generator
+   *
+   * Just assign the pointers of internal variable from source object to
+   * destinate object. The state is updated as Reference. Before this
+   * operation, the source obejct is checked for its availability.
+   *
+   * @param dest [out] the MT_Standard generator to accept the input
+   * @param src [in] the MT_Standard generator containing the information
+   * @return nothing
+   */
   void refer(MT_Standard& dest, const MT_Standard& src) {
     assert(IsAvailable(src));
     release(dest);
@@ -268,6 +466,17 @@ namespace std {
     dest.state=Reference;
   }
 
+  /**
+   * @brief initiate MT_Standard generator with a \c unsigned \c int seed
+   *
+   * The first element in the internal storage is assigned with seed.
+   * The other elements are generated sequentially. Before this operation,
+   * the MT_Standard generator is checked for its availability.
+   *
+   * @param MS [in,out] the MT_Standard generator to be initiated
+   * @param seed [in] the \c unsigned \c int seed to initiate generator
+   * @return nothing
+   */
   void init(MT_Standard& MS, const unsigned int seed) {
     assert(IsAvailable(MS));
     unsigned int& ri=*(MS.mti);
@@ -281,6 +490,26 @@ namespace std {
     }
   }
 
+  /**
+   * @brief initiate MT_Standard generator with an array
+   *
+   * The internal storage are filled with the results calculated based on
+   * elementes in arrays. The generator is first initiated with a default
+   * value of 19650218 before the operation on array. The array and generator
+   * are checked for their availability before this operation.
+   *
+   * @param MS [out] the MT_Standard genrator to be initiated
+   * @param key [in] the array containing the seeds
+   * @param len [in] the number of elements for initiation
+   * @param off [in] the offset for the first element in array key. It takes
+   *                 a default value of uZero.
+   * @param step [in] the step between elements in array key. It takes
+   *                  a default value of uOne.
+   * @return nothing
+   * @note it is proved that the initiation with array may produce better
+   *       uniform distribution. basically, the array may provide more
+   *       control (more bits of information) in input.
+   */
   void init(MT_Standard& MS, const unsigned int *key, const unsigned int len,
                              const unsigned int off=uZero,
                              const unsigned int step=uOne) {
@@ -309,6 +538,15 @@ namespace std {
     MS.mt[0]=MT_Standard::UppMask;
   }
 
+  /**
+   * @brief initiate MT_Standard generator with a vector
+   *
+   * It is implemented by init operation with array.
+   *
+   * @param MS [out] the MT_Standard generator to be initiated
+   * @param key [in] the vector containing seeds
+   * @return nothing
+   */
   void init(MT_Standard& MS, const Vector<unsigned int>& key) {
     init(MS,key(),key.size);
   }
