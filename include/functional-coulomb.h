433033281569de0eec78c8a5cd3e1ccbbe8c3a12
Jun Wang <junwang.nju@gmail.com>
2010-09-21 11:00:49 -0400

    add 3d-conf diffuse
    The 3d-conf diffusion is added but not tested.

--- include/functional-coulomb.h
+++ include/functional-coulomb.h
@@ -35,12 +35,37 @@ namespace std {
     func=prm[CoulombStrength].d/sqrt(dsq);
   }
 
+  /**
+   * @brief differentiation for Coulomb interaction
+   *
+   * Based on the input distance information, the differentiation of Coulomb
+   * function is calculated. The corresponding parameters are also included
+   * for flexibility.
+   *
+   * @param dsq [in] the square of distance
+   * @param prm [in] the array of parameters for concerned function
+   * @param diff [out] the output differentiation value
+   * @return nothing
+   */
   void DiffCoulomb(const double dsq, const UniqueParameter* prm,
                    double& diff) {
     double d=sqrt(dsq)*dsq;
     diff=-prm[CoulombStrength].d/d;
   }
 
+  /**
+   * @brief function value and differentiation for Coulomb function
+   *
+   * Based on the input distance information, the value and differentiation
+   * of Coulomb function are calculated at the same time. The corresponding
+   * parameters are also included for flexibility.
+   *
+   * @param dsq [in] the square of distance
+   * @param prm [in] the array of parameters for concerned function
+   * @param func [out] the output function value
+   * @param diff [out] the output differentiation value
+   * @return nothing
+   */
   void BothCoulomb(const double dsq, const UniqueParameter* prm,
                    double& func, double& diff) {
     double d=sqrt(dsq);
