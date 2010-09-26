6525da0d555c9603db97fa904217c64cbc81128c
Jun Wang <jwang@debian.localdomain>
2010-09-26 03:59:54 +0000

    3d-conf minor modification
    3d-conf diffusion is modified for efficiency.

--- prj/cg/3d-conf-diffuse.cxx
+++ prj/cg/3d-conf-diffuse.cxx
@@ -120,7 +120,7 @@ int main(int argc, char** argv) {
     M[nbID[u][v]]=-1;
 
     S=1;
-    for(rt=0;rt<5000;++rt) {
+    for(rt=0;rt<1000;++rt) {
       assign(dM,0.);
       for(unsigned int i=0;i<885641;++i)
         for(unsigned int k=0;k<nbID[i].size;++k) {
@@ -130,11 +130,9 @@ int main(int argc, char** argv) {
         }
       shift(M,dM);
       rM=asum(M);
-      if(rM<1e-3) {
-        S+=0.5*rM;  break;
-      } else
-        S+=rM;
+      S+=rM;
     }
+    S-=0.5*rM;
     cout<<u<<"\t"<<nbID[u][v]<<"\t"<<S<<"\t"<<rt<<endl;
   }
 
