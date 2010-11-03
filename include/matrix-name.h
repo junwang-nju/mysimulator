a16387b234cf7d2064c24c5cc345a48c2ff5f090
Jun Wang <junwang.nju@gmail.com>
2010-11-03 11:05:38 -0400

    update codes for matrix
    I plan to update codes for matrix with property-list as base. This is
    not finished.

---
+++ include/matrix-name.h
@@ -0,0 +1,39 @@
+
+#ifndef _Matrix_Name_H_
+#define _Matrix_Name_H_
+
+#include "vector.h"
+
+namespace std {
+
+  enum MatrixBasePropertyName {
+    MatrixType=0,
+    MatrixNumberElement,
+    MatrixNumberRow,
+    MatrixNumberColumn,
+    MatrixDataOrder,
+    MatrixDataState,
+    MatrixTransposeForm,
+    MatrixActualOrder,
+    MatrixCommonNumberProperty
+  };
+
+  enum MatrixTypeName {
+    RectangleMatrix=0,
+    TriangleMatrix
+  };
+
+  enum MatrixDataOrderName {
+    COrder=0,
+    FortranOrder,
+    DiagonalOrder
+  };
+
+  enum MatrixTransposeFormName {
+    NoTranspose=0,
+    Transposed
+  };
+
+}
+
+#endif
--- include/property-list.h
+++ include/property-list.h
@@ -3,103 +3,59 @@
 #define _Property_List_H_
 
 #include "vector.h"
-#include <cassert>
 
 namespace std {
 
   template <typename T>
   struct PropertyList : public Vector<T> {
-
+    typedef PropertyList<T>   Type;
+    typedef Vector<T> ParentType;
     Vector<T>* structure;
     unsigned int nunit;
-
-    typedef PropertyList<T> Type;
-    typedef Vector<T>   ParentType;
-
-    PropertyList() : ParentType(), structure(NULL), nunit(0) {}
-    PropertyList(const Type&) { myError("Cannor create from PropertyList"); }
-    Type& operator=(const Type& PL) { assign(*this,PL); return *this; }
-    ~PropertyList() { release(*this); }
-
-    Vector<T>& operator[](const unsigned int I) { return structure[I]; }
-    const Vector<T>& operator[](const unsigned int I) const {
-      return structure[I];
+    
+    PropertyList() : ParentType(), structure(), nunit(0) {}
+    PropertyList(const Type& L) { myError("Cannot create from PropertyList"); }
+    Type& operator=(const Type& L) {
+      MyError("Cannot copy PropertyList");
+      return *this;
     }
-
+    ~PropertyList() { release(*this); }
   };
 
   template <typename T>
-  bool IsAvailable(const PropertyList<T>& PL) {
-    return IsAvailable(static_cast<const Vector<T>&>(PL));
+  bool IsAvailable(const PropertyList<T>& L) {
+    return IsAvailable(static_cast<const Vector<T>&>(L));
   }
 
   template <typename T>
-  void release(PropertyList<T>& PL) {
-    if(PL.state==Allocated) {
-      //for(unsigned int i=0;i<PL.size;++i) release(PL.structure[i]);
-      safe_delete_array(PL.structure);
-    } else  PL.structure=NULL;
-    PL.nunit=0;
-    release(static_cast<Vector<T>&>(PL));
+  void release(PropertyList<T>& L) {
+    if(L.state==Allocated) safe_delete_array(L.structure);
+    else L.structure=NULL;
+    L.nunit=0;
+    release(static_cast<Vector<T>&>(L));
   }
 
   template <typename T>
-  void assign(PropertyList<T>& destPL, const PropertyList<T>& srcPL) {
-    assert(IsAvailable(destPL));
-    assert(IsAvailable(srcPL));
-    unsigned int n=(destPL.nunit<srcPL.nunit?destPL.nunit:srcPL.nunit);
-    for(unsigned int i=0;i<n;++i)
-      assign(destPL.structure[i],srcPL.structure[i]);
+  void copy(PropertyList<T>& L, const PropertyList<T>& sL) {
+    assert(IsAvailable(L));
+    assert(IsAvailable(sL));
+    unsigned int n=(L.nunit<sL.nunit?L.nunit:sL.nunit);
+    for(unsigned int i=0;i<n;++i) copy(L.structure[i],sL.structure[i]);
   }
 
   template <typename T>
-  void allocate(PropertyList<T>& PL, const unsigned int size) {
-    myError("allocate for vector is prohibited for proerpty list");
-  }
-
-  template <typename T>
-  void allocate(PropertyList<T>& PL,
-                const unsigned int *vsize, const unsigned int size) {
-    release(PL);
-    PL.nunit=size;
-    unsigned int dsize=0;;
+  void allocate(PropertyList<T>& L,
+                const unsigned int* vsize, const unsigned int size) {
+    release(L);
+    L.nunit=size;
+    unsigned int dsize=0;
     for(unsigned int i=0;i<size;++i)  dsize+=vsize[i];
-    allocate(static_cast<Vector<T>&>(PL),dsize);
-    PL.structure=new Vector<T>[size];
-    T* dPtr=PL.data;
-    for(unsigned int i=0;i<size;dPtr+=vsize[i],++i) refer(PL[i],dPtr,vsize[i]);
-  }
-
-  template <typename T>
-  void allocate(PropertyList<T>& PL, const Vector<unsigned int>& sz) {
-    allocate(PL,sz(),sz.size);
-  }
-
-  template <typename T>
-  void refer(PropertyList<T>& destPL, const PropertyList<T>& srcPL) {
-    assert(IsAvailable(srcPL));
-    release(destPL);
-    destPL.structure=srcPL.structure;
-    destPL.nunit=srcPL.nunit;
-    refer(static_cast<Vector<T>&>(destPL),static_cast<const Vector<T>&>(srcPL));
-  }
-
-  template <typename T>
-  void swap(PropertyList<T>& PLA, PropertyList<T>& PLB) {
-    swap(PLA.structure,PLB.structure);
-    swap(PLA.nunit,PLB.nunit);
-    swap(static_cast<Vector<T>&>(PLA),static_cast<Vector<T>&>(PLB));
-  }
-
-  template <typename T>
-  ostream& operator<<(ostream& os, const PropertyList<T>& PL) {
-    assert(IsAvailable(PL));
-    os<<PL[0];
-    for(unsigned int k=1;k<PL.nunit;++k) os<<endl<<PL[k];
-    return os;
+    allocate(static_cast<Vector<T>&>(L),dsize);
+    L.structure=new Vector<T>[size];
+    T* dPtr=L.data;
+    for(unsigned int i=0;i<size;dPtr+=vsize[i],++i)  refer(L[i],dPtr,vsize[i]);
   }
 
 }
 
 #endif
-
