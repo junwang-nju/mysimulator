c4e6451459fba121d2e8492e2977bf64171a73f3
Jun Wang <junwang.nju@gmail.com>
2010-11-07 14:00:42 +0800

    update parameter list
    The parameter list is updated and tested.

--- test/test-parameter-list.cxx
+++ test/test-parameter-list.cxx
@@ -10,29 +10,29 @@ int main() {
 
   cout<<"Test -- allocate"<<endl;
   allocate(PL,3,2,7);
-  unsigned int *ksize, *vsize;
-  ksize=new unsigned int[9];
-  vsize=new unsigned int[8];
-  assign(ksize,3,9);
-  assign(vsize,2,8);
-  allocate(PL,ksize,vsize,8);
-  allocate(PL,ksize,5,6);
-  allocate(PL,4,vsize,5);
+  Vector<unsigned int> ksize, vsize;
+  allocate(ksize,9);
+  allocate(vsize,8);
+  copy(ksize,3);
+  copy(vsize,2);
+  allocate(PL,ksize.data,vsize.data,8);
+  allocate(PL,ksize.data,5,6);
+  allocate(PL,4,vsize.data,5);
   release(ksize);
   release(vsize);
   cout<<endl;
 
   cout<<"Test -- input data by direct access"<<endl;
   for(unsigned int i=0;i<5;++i) {
-    PL.key[i].index[0]=0;
-    PL.key[i].index[1]=i;
-    PL.key[i].index[2]=i+1;
-    PL.key[i].index[3]=i+1;
-    buildhash(PL.key[i]);
+    PL.key[i][0]=0;
+    PL.key[i][1]=i;
+    PL.key[i][2]=i+1;
+    PL.key[i][3]=i+1;
+    PL.key[i].update();
     PL.value[i][0].d=123.45*i+34.789;
     PL.value[i][1].u=12345+i*87;
   }
-  updatetree(PL);
+  PL.update();
   cout<<endl;
 
   cout<<"Test -- access tree through []"<<endl;
@@ -40,13 +40,13 @@ int main() {
   cout<<endl;
 
   cout<<"Test -- get value"<<endl;
-  unsigned int *idx;
-  idx=new unsigned int[4];
+  Vector<unsigned int> idx;
+  allocate(idx,4);
   idx[0]=0;
   idx[1]=1;
   idx[2]=2;
   idx[3]=2;
-  const Vector<UniqueParameter> *pVU=get(PL,idx,4);
+  const Vector<UniqueParameter> *pVU=get(PL,idx.data,4);
   cout<<(*pVU)[0].d<<endl;
   cout<<(*pVU)[1].u<<endl;
   release(idx);
@@ -60,7 +60,7 @@ int main() {
   cout<<"Test -- assign from another parameter list"<<endl;
   ParameterList PL2;
   allocate(PL2,5,3,4);
-  PL2=PL;
+  copy(PL2,PL);
   for(unsigned int i=0;i<0xFFFFU;++i) if(IsAvailable(PL2[i])) cout<<i<<endl;
   cout<<endl;
 
