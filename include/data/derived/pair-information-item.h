b8361bf541fe327393bdbe411aa1869f30156dea
Jun Wang <junwang.nju@gmail.com>
2010-11-08 10:08:35 +0000

    update pair item
    The item for pair information is updated. The test is also updated.

--- include/data/derived/pair-information-item.h
+++ include/data/derived/pair-information-item.h
@@ -14,61 +14,30 @@ namespace std {
   struct PairItem {
     typedef PairItem<T>   Type;
     
-    T* content;
-    unsigned int *status;
-    unsigned int state;
+    T content;
+    unsigned int status;
     
-    PairItem() : content(NULL), status(NULL), state(Unused) {}
+    PairItem() : content(), status(0) {}
     PairItem(const Type& P) { myError("Cannot create Pair Item"); }
     Type& operator=(const Type& P) {
       myError("Cannot copy Pair Item");
       return *this;
     }
-    ~PairItem() { release(*this); }
 
-    T& operator()() { return *content; }
-    const T& operator()() const { return *content; }
+    T& operator()() { return content; }
+    const T& operator()() const { return content; }
 
   };
 
   template <typename T>
-  bool IsAvailable(const PairItem<T>& P) { return IsAvailable(P.content); }
-
-  template <typename T>
-  void release(PairItem<T>& P) {
-    if(P.state==Allocated) { safe_delete(P.content); safe_delete(P.status); }
-    else { P.content=NULL; P.status=NULL; }
-    P.state=Unused;
-  }
-
-  template <typename T>
   void copy(PairItem<T>& P, const PairItem<T>& cP) {
-    assert(IsAvailable(P));
-    assert(IsAvailable(cP));
-    *(P.content)=*(cP.content);
-    *(P.status)=*(cP.status);
-  }
-
-  template <typename T>
-  void allocate(PairItem<T>& P) {
-    release(P);
-    P.content=new T;
-    P.status=new unsigned int;
-    P.state=Allocated;
-  }
-
-  template <typename T>
-  void refer(PairItem<T>& P, const PairItem<T>& cP) {
-    assert(IsAvailable(cP));
-    release(P);
     P.content=cP.content;
     P.status=cP.status;
-    P.state=Reference;
   }
 
   template <typename T>
   ostream& operator<<(ostream& os, const PairItem<T>& P) {
-    os<<"[ "<<P()<<"\t("<<*(P.status)<<" )]";
+    os<<"[ "<<P()<<"\t("<<P.status<<" )]";
     return os;
   }
 
--- test/test-pair-item.cxx
+++ test/test-pair-item.cxx
@@ -9,34 +9,16 @@ int main() {
   PairItem<double> MSI;
   cout<<endl;
 
-  cout<<"Test -- allocate"<<endl;
-  allocate(MSI);
-  cout<<endl;
-
   cout<<"Test -- access"<<endl;
   MSI()=23.4567;
-  *(MSI.status)=12;
+  MSI.status=12;
   cout<<MSI<<endl;
   cout<<endl;
 
   cout<<"Test -- assign from another Item"<<endl;
   PairItem<double> MSI2;
-  allocate(MSI2);
   copy(MSI2,MSI);
   cout<<MSI2<<endl;
-  cout<<endl;
-
-  cout<<"Test -- release"<<endl;
-  release(MSI2);
-  cout<<endl;
-
-  cout<<"Test -availability check"<<endl;
-  cout<<IsAvailable(MSI)<<endl;
-  cout<<IsAvailable(MSI2)<<endl;
-  cout<<endl;
-
-  cout<<"Test -- refer operation"<<endl;
-  refer(MSI2,MSI);
   cout<<endl;
 
   return 1;
