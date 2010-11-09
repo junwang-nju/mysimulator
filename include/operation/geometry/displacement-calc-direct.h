16d067c89ef92382f35134e8defe52877820e16f
Jun Wang <junwang.nju@gmail.com>
2010-11-09 08:47:15 +0000

    update displacement calc
    The displacement operation is updated, not finished.

---
+++ include/operation/geometry/displacement-calc-direct.h
@@ -0,0 +1,18 @@
+
+#ifndef _Displacement_Calculate_Direct_H_
+#define _Displacement_Calculate_Direct_H_
+
+namespace std {
+
+  template <typename TA, typename TB, typename TD>
+  void Displacement(const TA* va, const TB* vb, TC* vc,
+                    const unsigned int dim,
+                    const int aoff=iZero, const long astep=lOne,
+                    const int boff=iZero, const long bstep=lOne,
+                    const int doff=iZero, const long dstep=lOne) {
+  }
+
+}
+
+#endif
+
