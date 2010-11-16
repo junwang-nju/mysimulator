03e83e92cf66da492ca2f75e65fe934dae3b94fb
JunWang <junwang.nju@gmail.com>
2010-11-16 13:06:52 +0800

    update interaction
    the interaction update is just started.

---
+++ include/operation/interaction/pairwise-base.h
@@ -0,0 +1,18 @@
+
+#ifndef _Interaction_Pairwise_Base_H_
+#define _Interaction_Pairwise_Base_H_
+
+#include "data/basic/vector.h"
+#include "data/basic/unique-parameter.h"
+
+namespace std {
+
+  template <typename T>
+  void EFuncPairwiseBase(
+      const Vector<T>* X, const unsigned int* idx, const UniqueParameter* P) {
+  }
+
+}
+
+#endif
+
