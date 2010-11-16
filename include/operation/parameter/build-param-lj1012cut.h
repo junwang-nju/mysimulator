551328e1905fdd6ff0d9f2da0d7192f9a1f1ca0b
Jun Wang <junwang.nju@gmail.com>
2010-11-17 01:26:34 +0800

    update parameter for LJ1012
    The build procedure for parameter of LJ1012cut is updated.

--- include/operation/parameter/build-param-lj1012cut.h
+++ include/operation/parameter/build-param-lj1012cut.h
@@ -6,6 +6,20 @@
 #include "data/basic/unique-parameter.h"
 
 namespace std {
+
+  void BuildParameterLJ1012Cut(UniqueParameter* prm) {
+    double r0rc,r0rc2,r0rcTEN,r0rcTLF,rc2;
+    rc2=prm[LJ1012CutCutR].d;
+    rc2*=rc2;
+    r0rc=prm[LJ1012CutCutR].d/prm[LJ1012CutEqRadius].d;
+    r0rc2=r0rc*r0rc;
+    r0rcTEN=r0rc2*r0rc2;
+    r0rcTEN*=r0rcTEN;
+    r0rcTEN*=r0rc2;
+    r0rcTLF=r0rcTEN*r0rc2;
+    copy(prm[LJ1012CutRealRadiusSQ],rc2*(r0rcTEN*r0rc-1.)/(r0rcTLF*r0rc-1.));
+  }
+
 }
 
 #include "data/basic/vector.h"
