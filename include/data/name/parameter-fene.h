aa504193aca84fabe5edc41ef6f501006364a062
JunWang <junwang.nju@gmail.com>
2010-12-01 00:12:10 +0800

    update fene parameter
    The parameter for fene interaction is updated (not finished)

---
+++ include/data/name/parameter-fene.h
@@ -0,0 +1,19 @@
+
+#ifndef _Parameter_FENE_H_
+#define _Parameter_FENE_H_
+
+namespace std {
+
+  enum FENEParameterName {
+    FENEStrength=0,
+    FENEDeltaRadisuMax,
+    FENEHalfStrength,
+    FENEDeltaRadisuMaxSQ,
+    FENEIvDeltaRadiusMax,
+    FENEHalfStrengthDRadiusSQ,
+    FENENumberParameter
+  };
+
+}
+
+#endif
