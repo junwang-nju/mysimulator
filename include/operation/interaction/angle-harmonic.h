6fd07fd9a0b3fc6139e199e935ef776cc09d93b9
Jun Wang <junwang.nju@gmail.com>
2010-11-30 03:19:11 +0000

    update test for angle-harmonic interaction
    The test for angle-harmonic interaction is updated.

--- include/operation/interaction/angle-harmonic.h
+++ include/operation/interaction/angle-harmonic.h
@@ -2,8 +2,8 @@
 #ifndef _Interaction_Angle_Harmonic_H_
 #define _Interaction_Angle_Harmonic_H_
 
-#include "operation/interaction/interaction-angle-base.h"
-#include "operation/functional/functional-angle-harmonic.h"
+#include "operation/interaction/angle-base.h"
+#include "operation/functional/angle-harmonic.h"
 
 namespace std {
 
@@ -26,9 +26,9 @@ namespace std {
   template <typename T,template <typename> class DistBuffer,typename GeomType>
   void BFuncAngleHarmonic(
       const Vector<T>* X, const unsigned int* idx, const UniqueParameter* P,
-      DistBuffer<T>& B, const GeomType& Geo, Vector<T>* Gradient,
+      DistBuffer<T>& B, const GeomType& Geo, T& Energy, Vector<T>* Gradient,
       Vector<T>* tmvec, const unsigned int idxsize=0) {
-    BFuncAngleBase(X,idx,P,B,Geo,Energy,Gradient,tmvec,DiffAngleHarmonic);
+    BFuncAngleBase(X,idx,P,B,Geo,Energy,Gradient,tmvec,BothAngleHarmonic);
   }
 
 }
--- test/test-interaction-angle-harmonic.cxx
+++ test/test-interaction-angle-harmonic.cxx
@@ -1,14 +1,15 @@
 
-#include "interaction-angle-harmonic.h"
-#include "distance-evaluate-direct.h"
-#include "free-space.h"
-#include "property-list.h"
+#include "operation/interaction/angle-harmonic.h"
+#include "operation/geometry/distance-calc-simplebuffer.h"
+#include "operation/geometry/displacement-calc-freespace.h"
+#include "operation/parameter/build-param-angle-harmonic.h"
+#include "data/basic/property-list.h"
 #include <iostream>
 using namespace std;
 
 int main() {
 
-  DistanceEvalDirect DED;
+  DistanceBufferSimple<double> DED;
   FreeSpace FS;
   allocate(DED,3,3);
   allocate(FS,3);
@@ -16,16 +17,16 @@ int main() {
   PropertyList<double> v,g,tmv;
   Vector<unsigned int> sz;
   allocate(sz,4);
-  assign(sz,3);
-  allocate(v,sz(),3);
-  allocate(g,sz(),3);
+  copy(sz,3);
+  allocate(v,sz);
+  allocate(g,sz);
   allocate(tmv,sz);
   
-  assign(v,0.);
+  copy(v,0.);
   v[1][0]=1.2;
   v[2][0]=1.5;  v[2][1]=1.; v[2][2]=0.5;
-  assign(g,0.);
-  assign(tmv,0.);
+  copy(g,0.);
+  copy(tmv,0.);
   
   Vector<unsigned int> idx;
   allocate(idx,3);
@@ -33,29 +34,28 @@ int main() {
   
   Vector<UniqueParameter> prm;
   allocate(prm,AngleHarmonicNumberParameter);
-  prm[AngleHarmonicEqAngle]=2./3.*M_PI;
-  prm[AngleHarmonicEqStrength]=20.;
-  GenerateParameterAngleHarmonic(prm);
+  prm[AngleHarmonicEqAngle].d=2./3.*M_PI;
+  prm[AngleHarmonicEqStrength].d=20.;
+  BuildParameterAngleHarmonic<double>(prm);
   
   double E;
   cout<<"Test -- calculate Energy of angle"<<endl;
   E=0.;
-  EFuncAngleHarmonic(v.structure,idx(),prm(),3,NULL,0,DED,FS,E);
+  EFuncAngleHarmonic(v.structure,idx.data,prm.data,DED,FS,E);
   cout<<E<<endl;
   cout<<endl;
 
   cout<<"Test -- calculate Gradient of angle"<<endl;
-  assign(g,0.);
-  GFuncAngleHarmonic(v.structure,idx(),prm(),3,tmv.structure,4,
-                     DED,FS,g.structure);
+  copy(g,0.);
+  GFuncAngleHarmonic(v.structure,idx(),prm(),DED,FS,g.structure,tmv.structure);
   cout<<g<<endl;
   cout<<endl;
 
   cout<<"Test -- calculate Energy and Gradient of angle"<<endl;
   E=0.;
-  assign(g,0.);
-  BFuncAngleHarmonic(v.structure,idx(),prm(),3,tmv.structure,4,
-                     DED,FS,E,g.structure);
+  copy(g,0.);
+  BFuncAngleHarmonic(v.structure,idx(),prm(),DED,FS,E,g.structure,
+                     tmv.structure);
   cout<<E<<endl;
   cout<<g<<endl;
   cout<<endl;
