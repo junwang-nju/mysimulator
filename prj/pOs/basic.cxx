1bc92a11f832fe74ce9a9cfba7448f7c972354bf
Jun Wang <junwang.nju@gmail.com>
2011-01-27 23:39:24 +0800

    temporary test for pOs-basic
    It seems that the FENE is not stable. It would be checked tomorrow.

--- prj/pOs/basic.cxx
+++ prj/pOs/basic.cxx
@@ -5,14 +5,14 @@
 #include "operation/parameter/interaction-parameter-regular-op.h"
 #include "operation/interaction/calc.h"
 #include "operation/parameter/build-param-propagator-vverlet.h"
-#include "data/random/regular.h"
-#include "operation/random/random-generator-boxmuller-op.h"
-#include "operation/random/random-generator-op.h"
 #include "operation/parameter/build-param-ext-object-lj612cut.h"
 #include "operation/parameter/build-param-fene.h"
 #include "operation/parameter/build-param-lj612cut.h"
 #include "operation/parameter/build-param-spheric-shell-property.h"
 #include "operation/propagate/run.h"
+#include "data/random/regular.h"
+#include "operation/random/random-generator-boxmuller-op.h"
+#include "operation/random/random-generator-op.h"
 using namespace std;
 
 template <typename T, typename ParameterType,
@@ -20,12 +20,25 @@ template <typename T, typename ParameterType,
           template <typename> class DBuffer, typename GeomType>
 void OutFunc(Propagator<T>& P, IType<T,DBuffer,GeomType>& F,
              const ParameterType& Pm, ostream& os) {
+  T E,kE;
+  static Vector<T> V(3);
+  E=kE=0.;
+  CalcInteraction(F,P.X,Pm,E);
+  for(unsigned int i=0;i<P.V.nunit;++i)
+    kE+=
+    0.5*static_cast<UniqueParameter&>(P.sysPg[0].merPg[i][MassData]).value<T>()
+       *normSQ(P.V[i]);
+  os<<static_cast<UniqueParameter&>(P[NowTime]).value<T>();
+  os<<"\t"<<E<<"\t"<<kE<<"\t"<<norm(P.X[0])<<"\t"<<norm(P.X[1])<<"\t";
+  copy(V,P.X[0]);
+  shift(V,-1.,P.X[1]);
+  os<<norm(V)<<endl;
 }
 
 
 int main() {
 
-  const unsigned int nunit=100;
+  const unsigned int nunit=2;
 
   Propagator<double> P;
   Vector<unsigned int> enstype;
@@ -33,7 +46,7 @@ int main() {
   allocate(enstype,1);
   allocate(mtype,1);
   allocate(mtype[0],nunit);
-  enstype[0]=Langevin;
+  enstype[0]=ConstantE;
   copy(mtype,ParticleType);
   allocate<Vector<InteractionParameterUnit>,ListInteraction,
            DistanceBufferSimple,FreeSpace>(P,VelocityVerlet,enstype,mtype);
@@ -42,7 +55,7 @@ int main() {
   allocate(grng);
   init(grng,12721849);
 
-  PropertyList<double> X,V,G,dMsk,RandV;
+  PropertyList<double> X,V,G,dMsk;
   Vector<unsigned int> sz;
   allocate(sz,nunit);
   copy(sz,3);
@@ -50,14 +63,13 @@ int main() {
   allocate(V,sz);
   allocate(G,sz);
   allocate(dMsk,sz);
-  allocate(RandV,sz);
   copy(X,0.);
   for(unsigned int i=0;i<nunit;++i) {
     X[i][0]=i;
     X[i][1]=6.;
   }
   fillarray(grng,V);
-  scale(V,0.01);
+  scale(V,1.);
   copy(G,0.);
   copy(dMsk,1.);
   refer(P.X,X);
@@ -73,18 +85,10 @@ int main() {
   mapData(P,id);
 
   P[StartTime].d=0.;
-  P[TotalTime].d=100000.;
-  P[OutputInterval].d=0.1;
-  P.sysPg[0][TimeStep].d=0.001;
-  P.sysPg[0][LV_Temperature].d=0.1;
-  P.sysPg[0][LV_Viscosity].d=0.5;
-  P.sysPg[0][LV_GaussianRNG].ptr=reinterpret_cast<void*>(&grng);
-  for(unsigned int i=0;i<nunit;++i) {
-    P.sysPg[0].merPg[i][MassData].d=1.;
-    P.sysPg[0].merPg[i][PLV_HydrodynamicRadius].d=1.;
-    P.sysPg[0].merPg[i][PLV_RandomVelocity].ptr=
-        reinterpret_cast<void*>(RandV.structure+i);
-  }
+  P[TotalTime].d=10;
+  P[OutputInterval].d=0.001;
+  P.sysPg[0][TimeStep].d=0.0001;
+  for(unsigned int i=0;i<nunit;++i) P.sysPg[0].merPg[i][MassData].d=1.;
   BuildParameterPropagatorVVerlet(P);
 
   ListInteraction<double,DistanceBufferSimple,FreeSpace> F;
@@ -114,7 +118,7 @@ int main() {
     Prm[i].prm[ExtObjectLocationPointer].ptr=reinterpret_cast<void*>(&wv);
     Prm[i].prm[ExtObjectPropertyPointer].ptr=reinterpret_cast<void*>(&pprop);
     Prm[i].prm[ExtObjLJ612CutEqRadius].d=1.;
-    Prm[i].prm[ExtObjLJ612CutEqEnergyDepth].d=4.;
+    Prm[i].prm[ExtObjLJ612CutEqEnergyDepth].d=0.;
     Prm[i].prm[ExtObjLJ612CutCutR].d=5.;
     BuildParameterExtObjLJ612Cut<double>(Prm[i].prm);
   }
@@ -127,14 +131,23 @@ int main() {
   for(unsigned int i=0,n=nunit+nunit-1;i<nunit;++i)
   for(unsigned int j=i+2;j<nunit;++j,++n) {
     Prm[n].prm[LJ612CutEqRadius].d=1.;
-    Prm[n].prm[LJ612CutEqEnergyDepth].d=0.1;
+    Prm[n].prm[LJ612CutEqEnergyDepth].d=0.;
     Prm[n].prm[LJ612CutCutR].d=5.0;
     BuildParameterLJ612Cut<double>(Prm[n].prm);
+  }
+  for(unsigned int i=0;i<nunit;++i) Prm[i].idx[0]=i;
+  for(unsigned int i=0,n=nunit;i<nunit-1;++i,++n) {
+    Prm[n].idx[0]=i;
+    Prm[n].idx[1]=i+1;
+  }
+  for(unsigned int i=0,n=nunit+nunit-1;i<nunit;++i)
+  for(unsigned int j=i+2;j<nunit;++j,++n) {
+    Prm[n].idx[0]=i;
+    Prm[n].idx[1]=j;
   }
 
   copy(P.G,0.);
   CalcInteraction(F,P.X,Prm,P.G);
-  cout<<P.G<<endl;
 
   assignOutput(P,OutFunc<double,Vector<InteractionParameterUnit>,
                          ListInteraction,DistanceBufferSimple,FreeSpace>);
