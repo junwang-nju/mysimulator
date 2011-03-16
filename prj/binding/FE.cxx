a26d88e78048fdaebc2068067a34bd4e42ae0262
Jun Wang <junwang.nju@gmail.com>
2011-03-16 11:09:34 +0000

    finish the coulomb attract case
    the case with coulomb attractive is finished (almost)

--- prj/binding/FE.cxx
+++ prj/binding/FE.cxx
@@ -1,5 +1,7 @@
 
 #include "data/basic/console-output.h"
+#include "data/basic/vector.h"
+#include "data/basic/file-input.h"
 #include  <cmath>
 using namespace std;
 
@@ -7,16 +9,25 @@ double f1(double d) {
   return -pow(d,-2.);
 }
 
-double IntF1(double d) {
-  return pow(d,-1.);
+double IntFunc(double d, double d0, double delta, const Vector<double>& mesh,
+               const double slope) {
+  if(d<d0) Error("Small");
+  unsigned int n=static_cast<unsigned int>((d-d0)/delta);
+  double shift=d-d0-n*delta;
+  if(n+1>=mesh.size)
+    return mesh[mesh.size-1]+slope*(d-(d0+(mesh.size-1)*delta));
+  return mesh[n]+shift/delta*(mesh[n+1]-mesh[n]);
 }
 
 double f(double d) {
-  return -pow(d,-6.);
+  //return -pow(d/8.,-6);
+  return -exp(-d/0.5)*10;
 }
 
 double S(double n, double d) {
-  return 2*log(d)-d*d/n;
+  double tm=2*log(d)-d*d/n;
+  if(d<n) return tm;
+  else return tm-100*(d-n)*(d-n);
 }
 
 int main() {
@@ -27,39 +38,69 @@ int main() {
 
   const double e1=0;
   const double e2=0;
+  const double epsilon=100;
 
-  double theta,cthNear,cthFar;
+  const unsigned int NData=99651;
+  Vector<double> MeshG, MeshFG;
+  const double d0=0.35;
+  const double delta=0.001;
+  allocate(MeshG,NData);
+  allocate(MeshFG,NData);
+  FileInput FI;
+  allocate(FI,"integral.coulomb-attract");
+  for(unsigned int i=0;i<NData;++i) {
+    double tmd;
+    FI>>tmd>>MeshG[i]>>MeshFG[i];
+  }
+  release(FI);
+
+  double cthNear,cthFar;
   double d,d1,d2;
-  double w,w1,w2;
-  unsigned int r,r1,r2;
+  unsigned int r,r1,r2,rMax;
   double R,RA,RB;
-  double F,T;
+  double FU,FL,T,SU,SL;
   unsigned int NA,NB,Nd;
-  double c1,c2,c3;
+  double C,CFG,CG;
 
-  T=2;
-  R=20;
-  d=0.1;
+  T=1.48;
+  R=4;
+  d=0.12;
 
-  r=0;
-  r1=0;
-  r2=r-r1;
-  NA=N1+r1;
-  NB=N2+r2;
-  Nd=Nm-r;
-  RA=pow(NA,-1./3.);
-  RB=pow(NB,-1./3.);
-  d1=(-NA*RA+NB*(RB+d)+Nd*d/2.)/N;
-  d2=d-d1;
-  cthNear=(R-RA)/(RA+d1);
-  if(cthNear>1.)    cthNear=1.;
-  cthFar=-(R-RB)/(RB+d2);
-  if(cthFar<-1.)    cthFar=-1.;
-
-  F=(IntF1(R-cthFar*(RA+d1))-IntF1(R-cthNear*(RA+d1)))/(RA+d1)+
-    (f(RA+RB+d)-T*S(Nd,d))*(cthNear-cthFar);
-
-  COut<<F<<Endl;
+  //for(R=3.8;R<40;R+=0.1) {
+  for(d=0.001;d<Nm;d+=0.0002) {
+    //rMax=Nm-(static_cast<unsigned int>(d-d*DRelDelta)+1);
+    rMax=Nm-1;
+    SU=SL=0;
+    r=0;
+    //for(r=0;r<=rMax;++r)
+    for(r1=0;r1<=r;++r1) {
+      r2=r-r1;
+      NA=N1+r1;
+      NB=N2+r2;
+      Nd=Nm-r;
+      RA=pow(NA,1./3.);
+      RB=pow(NB,1./3.);
+      d1=(-RA*NA+(RB+d)*NB+d/2.*Nd)/(N+0.);
+      d2=d-d1;
+      cthNear=(R-RA)/(RA+d1);
+      if(cthNear>1.)    cthNear=1.;
+      cthFar=-(R-RB)/(RB+d2);
+      if(cthFar<-1.)    cthFar=-1.;
+    
+      C=(f(d)-T*S(Nd,d))-e1*r1-e2*r2;
+      CG=IntFunc((R-cthFar*(RA+d1))*sqrt(T/epsilon),d0,delta,MeshG,1)
+        -IntFunc((R-cthNear*(RA+d1))*sqrt(T/epsilon),d0,delta,MeshG,1);
+      CFG=IntFunc((R-cthFar*(RA+d1))*sqrt(T/epsilon),d0,delta,MeshFG,0)
+        -IntFunc((R-cthNear*(RA+d1))*sqrt(T/epsilon),d0,delta,MeshFG,0);
+      FU=exp(-C/T)*sqrt(epsilon/T)/(RA+d1)*(T*CFG+C*CG);
+      FL=exp(-C/T)*sqrt(epsilon/T)/(RA+d1)*CG;
+    
+      SU+=FU;
+      SL+=FL;
+    }
+    //COut<<R<<"\t"<<log(SL)<<"\t"<<SU/SL<<Endl;
+    COut<<d<<"\t"<<-T*log(SL)<<Endl;
+  }
 
 
   return 0;
--- prj/binding/Integral.cxx
+++ prj/binding/Integral.cxx
@@ -8,25 +8,32 @@ long double f(long double x) {
 }
 
 long double g(long double x) {
-  return expl(f(x));
+  return expl(-f(x));
 }
 
 long double gg(long double x) {
-  return f(x)*expl(f(x));
+  return f(x)*expl(-f(x));
 }
 
 int main() {
   long double sG,sGG;
   const long double delta=0.001;
-  long double x=delta;
+  const double x0=0.35;
+  long double x=x0;
   sG=sGG=0.;
-  for(;x<0.5+delta*0.5;x+=delta) {
+  printf("%20.16Lf\t\t%20.16Lf\t\t%20.16Lf\n",x,sG*delta,sGG*delta);
+  sG=g(x)*0.5;
+  sGG=gg(x)*0.5;
+  x+=delta;
+  for(;x<1.5+delta*0.5;x+=delta) {
     sG+=0.5*g(x);
     sGG+=0.5*gg(x);
+    if(x>0.35-delta*0.5)
+      printf("%20.16Lf\t\t%20.16Lf\t\t%20.16Lf\n",x,sG*delta,sGG*delta);
     sG+=0.5*g(x);
     sGG+=0.5*gg(x);
   }
-  for(;x<50+delta*0.5;x+=delta) {
+  for(;x<100+delta*0.5;x+=delta) {
     sG+=0.5*g(x);
     sGG+=0.5*gg(x);
     printf("%20.16Lf\t\t%20.16Lf\t\t%20.16Lf\n",x,sG*delta,sGG*delta);
