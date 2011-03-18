b014aae215573d4b774dd3b12d4a1ecde6dda212
Jun Wang <junwang.nju@gmail.com>
2011-03-18 23:31:02 +0800

    add connectivity for amino acid
    The connectivity for amino acid is added, but not finishied

--- include/data/unit/connectivity.h
+++ include/data/unit/connectivity.h
@@ -7,10 +7,17 @@
 namespace std {
 
   Map<Vector<unsigned int>,char>  ConnectedID;
-  Vector<Vector<unsigned int> > ConnectNeighborID;
 
   void loadAminoAcidConnectivity(const char* ROOT=".") {
     if(!IsAvailable(ConnectedID)) allocate(ConnectedID);
+    char *buff=new char[1024];
+    strncpy(buff,ROOT,strlen(ROOT));
+    strcpy(buff+strlen(ROOT),"/connect.amino-acid");
+    FileInput FI;
+    allocate(FI,buff);
+    Vector<char> name;
+    release(FI);
+    delete[] buff;
   }
 
 }
