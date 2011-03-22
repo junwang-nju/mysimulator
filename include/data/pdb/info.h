75592071a3aceda706afbef3c10ea15a373aba83
Jun Wang <junwang.nju@gmail.com>
2011-03-23 00:28:27 +0800

    add new pdn information
    The PDB information structure is renewed, but is not finished

---
+++ include/data/pdb/info.h
@@ -0,0 +1,81 @@
+
+#ifndef _PDB_Information_H_
+#define _PDB_Information_H_
+
+#include "operation/unit/composition-op.h"
+
+namespace std {
+
+  struct PDBInfo;
+  void release(PDBInfo&);
+
+  struct PDBInfo {
+
+    Vector<unsigned int> AtomID;
+    Vector<unsigned int> UnitID;
+    Vector<Vector<unsigned int> > UnitAtom;
+    Vector<PropertyList<double> > AtomCoordinate;
+    PropertyList<unsigned int>    UnitConnect;
+
+    PDBInfo() : AtomID(),UnitID(),UnitAtom(),AtomCoordinate(),UnitConnect() {}
+    PDBInfo(const PDBInfo&) { Error("Cannot create PDB Information"); }
+    PDBInfo& operator=(const PDBInfo&) {
+      Error("Cannot copy PDB Information");
+      return *this;
+    }
+    ~PDBInfo() { release(*this); }
+
+  };
+
+  bool IsAvailable(const PDBInfo& PI) {
+    return IsAvailable(PI.AtomID)&&IsAvailable(PI.UnitID)&&
+           IsAvailable(PI.UnitAtom)&&IsAvailable(PI.AtomCoordinate)&&
+           IsAvailable(UnitConnect);
+  }
+  void release(PDBInfo& PI) {
+    release(PI.AtomID);
+    release(PI.UnitID);
+    release(PI.UnitAtom);
+    release(PI.AtomCoordinate);
+    release(PI.UnitConnect);
+  }
+  void imprint(PDBInfo& PI, const PDBInfo& cPI) {
+    assert(IsAvailable(cPI));
+    release(PI);
+    imprint(PI.AtomID,cPI.AtomID);
+    imprint(PI.UnitID,cPI.UnitID);
+    imprint(PI.UnitAtom,cPI.UnitAtom);
+    for(unsigned int i=0;i<cPI.UnitAtom.size;++i)
+      imprint(PI.UnitAtom[i],cPI.UnitAtom[i]);
+    imprint(PI.AtomCoordinate,cPI.AtomCoordinate);
+    for(unsigned int i=0;i<cPI.AtomCoordinate.size;++i)
+      imprint(PI.AtomCoordinate[i],cPI.AtomCoordinate[i]);
+    imprint(PI.UnitConnect,cPI.UnitConnect);
+  }
+  void copy(PDBInfo& PI, const PDBInfo& cPI) {
+    imprint(PI,cPI);
+    copy(PI.AtomID,cPI.AtomID);
+    copy(PI.UnitID,cPI.UnitID);
+    copy(PI.UnitAtom,cPI,UnitAtom);
+    copy(PI.AtomCoordinate,cPI.AtomCoordinate);
+    copy(PI.UnitConnect,cPI.UnitConnect);
+  }
+  void refer(PDBInfo& PI, const PDBInfo& cPI) {
+    assert(IsAvailable(cPI));
+    release(PI);
+    refer(PI.AtomID,cPI,AtomID);
+    refer(PI.UnitID,cPI.UnitID);
+    refer(PI.UnitAtom,cPI.UnitAtom);
+    refer(PI.AtomCoordinate,cPI.AtomCoordinate);
+    refer(PI.UnitConnect,cPI.UnitConnect);
+  }
+
+  void allocate(
+      PDBInfo& PI, const unsigned int nmodel, const unsigned int natom,
+      const Vector<unsigned int>& UnitSeq, const unsigned int nbond) {
+  }
+
+}
+
+#endif
+
