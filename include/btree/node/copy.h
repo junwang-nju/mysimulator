e13cd4c1acff1bf10b55ce105d57cbd6b0de7413
JunWang <junwang.nju@gmail.com>
2011-04-05 13:41:21 +0800

    add binary tree
    The node of new binary tree is added.

---
+++ include/btree/node/copy.h
@@ -0,0 +1,20 @@
+
+#ifndef _Binary_Tree_Node_Copy_H_
+#define _Binary_Tree_Node_Copy_H_
+
+#include "btree/node/interface.h"
+
+namespace mysimulator {
+
+  template <typename KeyType, typename ValueType>
+  void copy(BTreeNode<KeyType,ValueType>& N,
+            const BTreeNode<KeyType,ValueType>& cN) {
+    assert(IsValid(N)&&IsValid(cN));
+    copy(N.key,cN.key);
+    copy(N.value,cN.value);
+  }
+
+}
+
+#endif
+
---
+++ include/btree/node/interface.h
@@ -0,0 +1,65 @@
+
+#ifndef _Binary_Tree_Node_Interface_H_
+#define _Binary_Tree_Node_Interface_H_
+
+#include "referable-object/interface.h"
+#include "btree/branch-name.h"
+
+namespace mysimulator {
+
+  template <typename KeyType, typename ValueType>
+  struct BTreeNode {
+
+    typedef BTreeNode<KeyType,ValueType>  Type;
+
+    Object<KeyType> key;
+    Object<ValueType> value;
+    BranchName workBranch;
+    Type *left;
+    Type *right;
+    Type *parent;
+
+    BTreeNode() : key(), value(), workBranch(Unassigned), left(NULL),
+                  right(NULL), parent(NULL) {}
+    BTreeNode(const Type&) { Error("Copier of BTreeNode Disabled!"); }
+    Type& operator=(const Type&) {
+      Error("Operator= for BTreeNode Disabled!");
+      return *this;
+    }
+    ~BTreeNode() { clearData(); }
+
+    void clearData() {
+      release(key);
+      release(value);
+      workBranch=Unassigned;
+      parent=NULL;
+      release(left);
+      release(right);
+    }
+
+  };
+
+  template <typename KeyType, typename ValueType>
+  bool IsValid(const BTreeNode<KeyType,ValueType>& N) {
+    return IsValid(N.key)&&IsValid(N.value);
+  }
+  template <typename KeyType, typename ValueType>
+  void release(BTreeNode<KeyType,ValueType>& N) { N.clearData(); }
+
+  template <typename KeyType, typename ValueType>
+  void allocate(BTreeNode<KeyType,ValueType>& N) {
+    release(N);
+    allocate(N.key);
+    allocate(N.value);
+  }
+
+  template <typename KeyType, typename ValueType>
+  void imprint(BTreeNode<KeyType,ValueType>& N,
+               const BTreeNode<KeyType,ValueType>&) {
+    allocate(N);
+  }
+
+}
+
+#endif
+
