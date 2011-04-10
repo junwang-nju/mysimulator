ac2b93ea64485eda705beb64ed45f2979a61f83e
Jun Wang <junwang.nju@gmail.com>
2011-04-10 21:08:18 +0800

    add random mt-standard
    The random generator with mt-standard method is rewritten.

---
+++ include/random/mt-standard/allocate.h
@@ -0,0 +1,16 @@
+
+#ifndef _Random_MT_Standard_Allocate_H_
+#define _Random_MT_Standard_Allocate_H_
+
+#include "random/mt-standard/interface.h"
+
+namespace mysimulator {
+
+  void alloacate(MT_Standard& G) { G.init(5489UL); }
+
+  void imprint(MT_Standard& G, const MT_Standard& cG) { allocate(G);  }
+
+}
+
+#endif
+
---
+++ include/random/mt-standard/copy.h
@@ -0,0 +1,22 @@
+
+#ifndef _Random_MT_Standard_Copy_H_
+#define _Random_MT_Standard_Copy_H_
+
+#include "random/mt-standard/interface.h"
+#include "unique-parameter/128bit/copy.h"
+
+namespace mysimulator {
+
+  void copy(MT_Standard& G, const MT_Standard& cG) {
+    assert(IsValid(G)&&IsValid(cG));
+    unsigned int *p=G.s;
+    unsigned int *cp=const_cast<unsigned int*>(cG.s);
+    unsigned int *pend=p+MT_Standard::N;
+    for(;p!=pend;)  copy(*(p++),*(cp++));
+    copy(G.sl,cG.sl);
+  }
+
+}
+
+#endif
+
