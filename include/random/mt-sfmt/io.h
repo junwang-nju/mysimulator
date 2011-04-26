3c6cb6c72c9195e9a7ae2a90c5cc284fc94d2a2c
Jun Wang <junwang.nju@gmail.com>
2011-04-26 23:16:06 +0800

    finish io for mt-sfmt
    The IO module for MT-sfmt is done.

--- include/random/mt-sfmt/io.h
+++ include/random/mt-sfmt/io.h
@@ -10,6 +10,19 @@ namespace mysimulator {
 
   template <unsigned int LFac>
   OutputBase& operator<<(OutputBase& os, const MT_SFMT<LFac>& G) {
+    assert(IsValid(G));
+    os<<LFac<<"\t"<<G.s<<"\t"<<G.idx;
+    return os;
+  }
+
+  template <unsigned int LFac>
+  InputBase& operator>>(InputBase& is, MT_SFMT<LFac>& G) {
+    assert(IsValid(G));
+    unsigned int rFac;
+    is>>rFac;
+    if(rFac!=LFac)  Error("Incompatible Factor for MT_dSFMT!");
+    is>>G.s>>G.idx;
+    return is;
   }
 
 }
