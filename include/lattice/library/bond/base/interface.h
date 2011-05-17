02e2e5725306464a5ac0ef2bda659b9d90b42044
Jun Wang <junwang.nju@gmail.com>
2011-05-17 23:26:52 +0800

    update the basic library for bond
    The library for bonds is updated.

--- include/lattice/library/bond/base/interface.h
+++ include/lattice/library/bond/base/interface.h
@@ -1,6 +1,6 @@
 
-#ifndef _Lattice_Library_Bond_H_
-#define _Lattice_Library_Bond_H_
+#ifndef _Lattice_Library_Bond_Base_Interface_H_
+#define _Lattice_Library_Bond_Base_Interface_H_
 
 #include "list/allocate.h"
 #include "vector/scale.h"
@@ -8,10 +8,11 @@
 #include "list/io.h"
 #include "vector/allocate.h"
 #include "io/input/file/interface.h"
+#include "lattice/name.h"
 
 namespace mysimulator {
 
-  template <unsigned int LatticeType, unsigned int Dimension>
+  template <LatticeTypeName LatticeType, unsigned int Dimension>
   struct LatticeBondLib {
 
     typedef LatticeBondLib<LatticeType,Dimension>   Type;
@@ -43,7 +44,7 @@ namespace mysimulator {
       release(map);
       release(coordinateMap);
     }
-    void read(const char* fTemplate) {
+    void _read(const char* fTemplate) {
       Vector<unsigned int> sz;
       static char nmbuffer[1024];
       allocate(sz,MaxBonds);
@@ -54,7 +55,7 @@ namespace mysimulator {
       FileInput ifs;
       unsigned int nmotif;
       for(unsigned int i=1;i<=MaxBonds;++i) {
-        sprintf(nmbuffer,"%s.%d-bond",fTemplate,i);
+        sprintf(nmbuffer,"%s%d",fTemplate,i);
         ifs.open(nmbuffer);
         ifs>>shiftLoc[i];
         nmotif=shiftLoc[i][NumberShifts-1];
@@ -70,11 +71,38 @@ namespace mysimulator {
 
   };
 
-  template <unsigned int LatticeType, unsigned int Dimension>
+  template <LatticeTypeName LT, unsigned int Dim>
+  const unsigned int LatticeBondLib<LT,Dim>::MaxBonds=0;
+
+  template <LatticeTypeName LT, unsigned int Dim>
+  const unsigned int LatticeBondLib<LT,Dim>::NumNeighbors=0;
+
+  template <LatticeTypeName LT, unsigned int Dim>
+  const unsigned int LatticeBondLib<LT,Dim>::NumberShifts=0;
+
+  template <LatticeTypeName LT, unsigned int Dim>
+  List<unsigned int> LatticeBondLib<LT,Dim>::shiftLoc;
+
+  template <LatticeTypeName LT, unsigned int Dim>
+  Vector<List<unsigned int> > LatticeBondLib<LT,Dim>::map;
+
+  template <LatticeTypeName LT, unsigned int Dim>
+  Vector<List<int> > LatticeBondLib<LT,Dim>::coordinateMap;
+
+  template <LatticeTypeName LatticeType, unsigned int Dimension>
   bool IsValid(const LatticeBondLib<LatticeType,Dimension>& L) {
     typedef LatticeBondLib<LatticeType,Dimension> Type;
     return IsValid(Type::shiftLoc)&&IsValid(Type::map)&&
            IsValid(Type::coordinateMap);
+  }
+
+  template <LatticeTypeName LatticeType, unsigned int Dimension>
+  void release(LatticeBondLib<LatticeType,Dimension>& L) { L.clearData(); }
+
+  template <LatticeTypeName LT, unsigned int Dim>
+  const LatticeBondLib<LT,Dim>& RunBondLibrary() {
+    Error();
+    return LatticeBondLib<LT,Dim>();
   }
 
 }
---
+++ include/lattice/type-name.h
@@ -0,0 +1,14 @@
+
+#ifndef _Lattice_Type_Name_H_
+#define _Lattice_Type_Name_H_
+
+namespace mysimulator {
+
+  enum LatticeTypeName {
+    SquareLattice
+  };
+
+}
+
+#endif
+
