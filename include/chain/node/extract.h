a05f4726f8376fb914b13b7588c44704f2473141
Jun Wang <junwang.nju@gmail.com>
2011-06-07 09:08:42 +0000

    finish test for new chain
    The test for new chain is finished.

--- include/chain/node/extract.h
+++ include/chain/node/extract.h
@@ -9,13 +9,21 @@ namespace mysimulator {
 
   template <typename T>
   ObjectStateName extract(ChainNode<T>& N) {
-    swap(N.parent().child,N.child);
-    swap(N.parent().child().parent,N.parent);
-    ObjectStateName NState;
-    NState=((N.parent.flag==Allocated)||(N.child.flag==Allocated)?
-            Allocated:Referred);
-    N.parent.flag=Referred;
-    N.child.flag=Referred;
+    ObjectStateName NState=Referred;
+    if(IsValid(N.parent)&&IsValid(N.child)) {
+      swap(N.parent().child,N.child);
+      swap(N.parent().child().parent,N.parent);
+      NState=((N.parent.flag==Allocated)||(N.child.flag==Allocated)?
+               Allocated:Referred);
+      N.parent.flag=Referred;
+      N.child.flag=Referred;
+    } else if(IsValid(N.parent)) {
+      swap(N.parent().child,N.child);
+      NState=(N.child.flag==Allocated?Allocated:Referred);
+    } else if(IsValid(N.child)) {
+      swap(N.child().parent,N.parent);
+      NState=(N.parent.flag==Allocated?Allocated:Referred);
+    }
     return NState;
   }
 
--- test/test-chain.cxx
+++ test/test-chain.cxx
@@ -1,7 +1,8 @@
 
-#include "chain/node/allocate.h"
 #include "chain/node/copy.h"
 #include "chain/node/refer.h"
+#include "chain/allocate.h"
+#include "chain/copy.h"
 using namespace mysimulator;
 
 #include <iostream>
@@ -37,8 +38,51 @@ int main() {
   refer(Nd3,Nd);
   cout<<"content of referred Chain Node: "<<Nd3()<<endl;
 
-  cout<<Nd.parent.flag<<endl;
+  cout<<endl;
+  cout<<"Extract Node and Check whether it is allocated by Chain: "<<extract(Nd)<<endl;
 
+  cout<<endl;
+  Chain<int> C,C2;
+  cout<<"Before Allocation of Chain: root is "<<IsValid(C.root)<<endl;
+  allocate(C);
+  cout<<"After Allocation of Chain: root is "<<IsValid(C.root)<<endl;
+  imprint(C2,C);
+  cout<<"Aftter Imprint of Chain: root is "<<IsValid(C2.root)<<endl;
+  release(C2);
+  cout<<"After release of Chain: head is "<<IsValid(C2.head)<<endl;
+
+  cout<<endl;
+  C.append(Nd);
+  cout<<"The append content is: "<<C.head.parent().content()<<endl;
+  cout<<"Check If A Node is Contained: "<<C.IsContained(Nd)<<endl;
+  cout<<"Check If A Node is Contained: "<<C.IsContained(Nd2)<<endl;
+  cout<<"Check If A Value is Contained: "<<C.IsContained(9)<<endl;
+  C.append(8);
+  cout<<"The append content is: "<<C.head.parent().content()<<endl;
+  C.append(9);
+  cout<<"Check If A Value is Contained: "<<C.IsContained(9)<<endl;
+  int iValue=6;
+  C.append(iValue,Referred);
+  cout<<"append with Referred value:"<<endl;
+  cout<<"\t"<<C.head.parent().content();
+  cout<<"\t"<<C.head.parent().content.flag<<endl;
+  cout<<"the content of present Chain are:"<<endl;
+  cout<<"\t"<<C.head.parent().content()<<endl;
+  cout<<"\t"<<C.head.parent().parent().content()<<endl;
+  cout<<"\t"<<C.head.parent().parent().parent().content()<<endl;
+  cout<<"\t"<<C.head.parent().parent().parent().parent().content()<<endl;
+  C.remove(9);
+  cout<<"the content of present Chain After Remove are:"<<endl;
+  cout<<"\t"<<C.head.parent().content()<<endl;
+  cout<<"\t"<<C.head.parent().parent().content()<<endl;
+  cout<<"\t"<<C.head.parent().parent().parent().content()<<endl;
+
+  cout<<endl;
+  copy(C2,C);
+  cout<<"the content of copied Chain are:"<<endl;
+  cout<<"\t"<<C2.head.parent().content()<<endl;
+  cout<<"\t"<<C2.head.parent().parent().content()<<endl;
+  cout<<"\t"<<C2.head.parent().parent().parent().content()<<endl;
   return 0;
 }
 
