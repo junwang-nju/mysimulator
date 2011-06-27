43f3e0131a209ff48a20df9cfa0ed8047e49eda1
Jun Wang <junwang.nju@gmail.com>
2011-06-28 01:48:22 +0800

    update test for propagator
    The test for propagator is updated.

--- test/test-propagator.cxx
+++ test/test-propagator.cxx
@@ -4,12 +4,28 @@
 #include "propagator/base/allocate.h"
 #include "propagator/md/allocate.h"
 #include "propagator/run.h"
+
+#include "interaction/kernel/allocate.h"
+#include "interaction/kernel/copy.h"
+#include "interaction/func/generic/allocate.h"
+#include "interaction/func/generic/copy.h"
+#include "buffer/distance/simple/allocate.h"
+#include "buffer/distance/simple/copy.h"
+#include "boundary/free/allocate.h"
+#include "boundary/free/copy.h"
+#include "interaction/parameter/unit/allocate.h"
+#include "interaction/parameter/unit/copy.h"
+#include "geometry/distance/dbuffer-simple/calc.h"
+#include "geometry/displacement/free/calc.h"
 using namespace mysimulator;
 
 #include <iostream>
 using namespace std;
 
 int main() {
+  typedef InteractionFuncGeneric<SimpleDistanceBuffer,FreeSpace,double> IFType;
+  MDPropagator<double,SimpleDistanceBuffer,FreeSpace,IFType,
+               Vector<InteractionParameterUnit> > P;
   return 0;
 }
 
