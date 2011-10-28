17b090eae95e0b66dafd6360aab4f79c2de20883
Jun Wang <jwang@debian.localdomain>
2011-10-28 03:12:32 -0400

    add new motif chain
    new motif chain is added, but not finished or tested.

---
+++ include/new/lattice/motif-chain/interface.h
@@ -0,0 +1,52 @@
+
+#ifndef _Lattice_MotifChain_Interface_H_
+#define _Lattice_MotifChain_Interface_H_
+
+#include "lattice/motif-library/interface.h"
+
+namespace mysimulator {
+
+  template <LatticeShapeName LSN, unsigned int Dim, unsigned int Len>
+  struct LatticeMotifChain {
+
+    public:
+
+      typedef LatticeMotifChain<LSN,Dim,Len>  Type;
+
+      static const unsigned int NumMotifs;
+      static const unsigned int NumBondsLastMotif;
+
+      Array1D<unsigned short> SegMotif;
+
+      LatticeMotifChain() : SegMotif() {}
+      ~LatticeMotifChain() { clearData(); }
+
+      void clearData() { release(SegMotif); }
+      bool isvalid() const { return IsValid(SegMotif); }
+
+      unsigned char bondID(const unsigned int& n) {
+        unsigned int u=LatticeMotifLibrary<LSN,Dim>::MaxBondOfMotif;
+        unsigned int I=n/u;
+        unsigned int R=n%u;
+        unsigned int NB=(I==NumMotifs-1?R:u);
+        assert(IsValid(LatticeMotifLibrary<LSN,Dim>::map));
+        return LatticeMotifLibrary<LSN,Dim>::map[NB-1][SegMotif[I]].bond[R];
+      }
+
+    private:
+
+      LatticeMotifChain(const Type&) {}
+      Type& operator=(const Type&) { return *this; }
+
+  };
+
+  template <LatticeShapeName LSN, unsigned int Dim, unsigned int L>
+  bool IsValid(const LatticeMotifChain<LSN,Dim,L>& C) { return C.isvalid(); }
+
+  template <LatticeShapeName LSN, unsigned int Dim, unsigned int L>
+  void release(LatticeMotifChain<LSN,Dim,L>& C) { C.clearData(); }
+
+}
+
+#endif
+
