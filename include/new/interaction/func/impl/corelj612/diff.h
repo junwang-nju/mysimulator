6ee9a93e83458b6259c56713e36e1ef4c135e4e6
Jun Wang <junwang.nju@gmail.com>
2011-12-10 06:16:12 -0500

    add core-LJ612 interaction
    The interaction of LJ612cut as core is added.

---
+++ include/new/interaction/func/impl/corelj612/diff.h
@@ -0,0 +1,19 @@
+
+#ifndef _Interaction_Func_Implement_CoreLJ612_Diff_H_
+#define _Interaction_Func_Implement_CoreLJ612_Diff_H_
+
+#include "interaction/func/impl/corelj612/parameter/name.h"
+#include "interaction/func/impl/lj612/diff.h"
+
+namespace mysimulator {
+
+  template <typename T>
+  void DiffCoreLJ612(const T& dsq, const Unique64Bit* P, T& diff) {
+    if(dsq>P[CoreLJ612RadiusSQ].value<T>()) diff=0.;
+    else DiffLJ612(dsq,P,diff);
+  }
+
+}
+
+#endif
+
---
+++ include/new/interaction/func/impl/corelj612/parameter/name.h
@@ -0,0 +1,22 @@
+
+#ifndef _Interaction_Func_Implement_CoreLJ612_Parameter_Name_H_
+#define _Interaction_Func_Implement_CoreLJ612_Parameter_Name_H_
+
+#include "interaction/func/impl/lj612/parameter/name.h"
+
+namespace mysimulator {
+
+  enum CoreLJ612ParameterName {
+    CoreLJ612Radius           =LJ612EqRadius,
+    CoreLJ612EnergyDepth      =LJ612EqEnergyDepth,
+    CoreLJ612RadiusSQ         =LJ612EqRadiusSQ,
+    CoreLJ612TwlfEnergyDepth  =LJ612TwlfEnergyDepth,
+    CoreLJ612NumberParameters =LJ612NumberParameters
+  };
+
+}
+
+#include "interaction/func/impl/corelj612/parameter/build.h"
+
+#endif
+
