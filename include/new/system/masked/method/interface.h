1f0b4e84a667f216f027b6158279158dd263f7b1
Jun Wang <jwang@debian.localdomain>
2011-12-14 23:51:23 -0500

    modify mask method
    new implementations may be better for speed.

--- include/new/system/masked/method/interface.h
+++ include/new/system/masked/method/interface.h
@@ -4,43 +4,21 @@
 
 #include "system/masked/method/name.h"
 #include "system/masked/component/interface.h"
-#include "system/masked/method/no-operation/do.h"
-#include "system/masked/method/do-mask/do.h"
 
 namespace mysimulator {
 
-  template <SystemMaskMethodName MN,
-            typename T, template<typename> class SpaceType>
+  template <SystemMaskMethodName MN>
   struct SystemMaskApply {
 
     public:
 
-      typedef SystemMaskApply<MN,T,SpaceType>   Type;
-      typedef void (*_ApplyType)(SpaceType<T>&,const Mask4System<T,SpaceType>&);
+      typedef SystemMaskApply<MN>   Type;
 
-      static const _ApplyType _Apply=
-        (MN==SystemNoOperation?_SystemNoOperationApply<T,SpaceType>::Do:
-         MN==SystemDoMask?_SystemDoMaskApply<T,SpaceType>::Do:
-         NULL);
-
-      SystemMaskApply() {}
+      SystemMaskApply() { Error("Unknown Mask Method!"); }
       ~SystemMaskApply() { clearData(); }
 
       void clearData() {}
-      bool isvalid() const { return IsValid(_Apply); }
-
-      void Do(SpaceType<T>& V, const Mask4System<T,SpaceType>& M) {
-        assert(isvalid());
-        _Apply(V,M);
-      }
-      void Do(Object<SpaceType<T> >& V, const Mask4System<T,SpaceType>& M) {
-        Do(V(),M);
-      }
-      void Do(SpaceType<T>& V, const Object<Mask4System<T,SpaceType> >& M) {
-        Do(V,M());
-      }
-      void Do(Object<SpaceType<T> >& V,
-              const Object<Mask4System<T,SpaceType> >& M) { Do(V(),M()); }
+      bool isvalid() const { return false; }
 
     private:
 
@@ -49,22 +27,16 @@ namespace mysimulator {
 
   };
 
-  template <SystemMaskMethodName MN,
-            typename T, template<typename> class SpaceType>
-  const typename SystemMaskApply<MN,T,SpaceType>::_ApplyType
-  SystemMaskApply<MN,T,SpaceType>::_Apply;
+  template <SystemMaskMethodName MN>
+  void release(SystemMaskApply<MN>& A) { A.clearData(); }
 
-  template <SystemMaskMethodName MN,
-            typename T, template<typename> class SpaceType>
-  void release(SystemMaskApply<MN,T,SpaceType>& A) { A.clearData(); }
-
-  template <SystemMaskMethodName MN,
-            typename T, template<typename> class SpaceType>
-  bool IsValid(const SystemMaskApply<MN,T,SpaceType>& A) { return A.isvalid(); }
+  template <SystemMaskMethodName MN>
+  bool IsValid(const SystemMaskApply<MN>& A) { return A.isvalid(); }
 
 }
 
-//#include "system/masked/method/fixed/specification.h"
+#include "system/masked/method/no-operation/specification.h"
+#include "system/masked/method/do-mask/specification.h"
 
 #endif
 
