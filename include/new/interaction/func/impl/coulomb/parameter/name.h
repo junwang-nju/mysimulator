0c8058c1728e846bb2dd10365331bddc4c82094a
Jun Wang <jwang@debian.localdomain>
2011-12-18 12:01:07 -0500

    add implementation of coulomb interaction
    The coulomb interaction is added, but not registered into
    InteractionFunc object.

---
+++ include/new/interaction/func/impl/coulomb/parameter/name.h
@@ -0,0 +1,20 @@
+
+#ifndef _Interaction_Func_Implement_Coulomb_Parameter_Name_H_
+#define _Interaction_Func_Implement_Coulomb_Parameter_Name_H_
+
+namespace mysimulator {
+
+  enum CoulombParameterName {
+    CoulombChargeA,
+    CoulombChargeB,
+    CoulombDielectric,
+    CoulombStrength,
+    CoulombNumberParameters
+  };
+
+}
+
+#include "interaction/func/impl/coulomb/parameter/build.h"
+
+#endif
+
