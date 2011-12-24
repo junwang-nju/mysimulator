1da1bcd44fec2714780038860d99d824eae19007
Jun Wang <junwang.nju@gmail.com>
2011-12-24 05:43:53 -0500

    initialize neighbor-list
    Details to be continued. BTW, evolute/dynamics object for the whole
    system needs to be considered in future.

---
+++ include/new/accelerator/neighbor-list/interface.h
@@ -0,0 +1,40 @@
+
+#ifndef _Accelerator_NeighborList_Interface_H_
+#define _Accelerator_NeighborList_Interface_H_
+
+namespace mysimulator {
+
+  template <typename T>
+  struct NeighborList {
+
+    public:
+
+      typedef NeighborList<T>   Type;
+
+      T CutRadius;
+      T InteractionCutRadius;
+      T BufferRadius;
+
+      NeighborList() : CutRadius(0), InteractionCutRadius(0), BufferRadius(0) {}
+      ~NeighborList() { clearData(); }
+
+      void clearData() { CutRadius=0; InteractionCutRadius=0; BufferRadius=0; }
+      bool isvalid() const { return true; }
+
+    private:
+
+      NeighborList(const Type&) {}
+      Type& operator=(const Type&) { return *this; }
+
+  };
+
+  template <typename T>
+  void release(NeighborList<T>& L) { L.clearData(); }
+
+  template <typename T>
+  bool IsValid(const NeighborList<T>& L) { return L.isvalid(); }
+
+}
+
+#endif
+
