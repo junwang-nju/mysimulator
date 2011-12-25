7f5514058b62b97db34e82765d1c8eba647a7330
Jun Wang <junwang.nju@gmail.com>
2011-12-24 22:00:44 -0500

    add a method for neighbor-list
    neighbor-list is updated, but not finished.

--- include/new/accelerator/neighbor-list/interface.h
+++ include/new/accelerator/neighbor-list/interface.h
@@ -2,6 +2,10 @@
 #ifndef _Accelerator_NeighborList_Interface_H_
 #define _Accelerator_NeighborList_Interface_H_
 
+#include "array/1d/interface.h"
+#include "array/2d/content/interface.h"
+#include "distance/calc.h"
+
 namespace mysimulator {
 
   template <typename T>
@@ -15,6 +19,7 @@ namespace mysimulator {
       T InteractionCutRadius;
       T BufferRadius;
       T UpdateTime;
+      T CutRadiusSQ;
 
       NeighborList()
         : CutRadius(0), InteractionCutRadius(0), BufferRadius(0),
@@ -25,6 +30,23 @@ namespace mysimulator {
         CutRadius=0; InteractionCutRadius=0; BufferRadius=0; UpdateTime=0;
       }
       bool isvalid() const { return true; }
+
+      template <typename GeomType>
+      unsigned int renew(Array1DContent<Array1D<unsigned int> >& id,
+                         Array2DContent<T>& X, const GeomType& Geo) {
+        Array1D<T> tV;
+        imprint(tV,X[0]);
+        T dsq;
+        unsigned int n=0;
+        for(unsigned int i=id.last;i>=id.first;--i) {
+          dsq=DistanceSQ(tV,X[id[i][0]],X[id[i][1]],Geo);
+          if(dsq<CutRadiusSQ) {
+            if(n!=i)  exchange(id[n],id[i]);
+            ++n;
+          }
+          
+        }
+      }
 
     private:
 
