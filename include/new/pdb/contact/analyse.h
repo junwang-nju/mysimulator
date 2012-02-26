299daa934e827e0a9d0a11bea188dbbba00cafd0
Jun Wang <junwang.nju@gmai.com>
2012-02-26 14:01:42 -0500

    add c-alpha mode for contact
    THe contacts defined based on c-alpha are implemented.

--- include/new/pdb/contact/analyse.h
+++ include/new/pdb/contact/analyse.h
@@ -11,10 +11,47 @@ namespace mysimulator {
   template <PDBContactAnalyseModeName AM, PDBContactStructureModeName SM,
             typename AType>
   struct _ContactFunctor {
+
     public:
       static unsigned int Analyse(const PDBObject&, AType&, const double&) {
         Error("Not Implemented");
         return 0;
+      }
+      static unsigned int Analyse(const PDBObject& PO, AType& id) {
+        return Analyse(PO,id,0.);
+      }
+
+  };
+
+  template <typename AType>
+  struct _ContactFunctor<CAlpha,FirstModel,AType> {
+    public:
+      static unsigned int Analyse(
+          const PDBObject& PO, AType& id, const double& th) {
+        assert(IsValid(PO));
+        PDBMolecule* Mol=PO.Model[0].Molecule.start;
+        unsigned int MolSize=PO.Model[0].Molecule.size;
+        unsigned int w;
+        const double thsq=th*th;
+        w=0;
+        for(unsigned int i=0;i<MolSize;++i)
+        for(unsigned int k=0;k<Mol[i].Residue.size;++k) {
+          for(unsigned int j=0;j<MolSize;++j)
+          for(unsigned int l=0;l<Mol[j].Residue.size;++l) {
+            if((k>=l)&&(i==j))  continue;
+            if(i>j) continue;
+            if(DistanceSQ(Mol[i].Residue[k].Atom[AtomCA]->Loc,
+                          Mol[j].Residue[l].Atom[AtomCA]->Loc)<thsq) {
+              id[w][0]=i; id[w][1]=k; id[w][2]=j; id[w][3]=l;
+              ++w;
+              if(w>id.NumLines()) Error("Storage for ID Used Up!");
+            }
+          }
+        }
+        return w;
+      }
+      static unsigned int Analyse(const PDBObject& PO, AType& id) {
+        return Analyse(PO,id,7.5);
       }
   };
 
@@ -50,6 +87,7 @@ namespace mysimulator {
                               Mol[j].Residue[l].Atom[AN2]->Loc)<thsq) {
                   id[w][0]=i; id[w][1]=k; id[w][2]=j; id[w][3]=l;
                   ++w;
+                  if(w>id.NumLines()) Error("Storage for ID Used Up!");
                   fbreak=true;
                 }
               }
@@ -59,7 +97,9 @@ namespace mysimulator {
         return w;
       }
 
-      static void Analyse(const PDBObject& PO, AType& id) { Analyse(PO,id,5.); }
+      static unsigned int Analyse(const PDBObject& PO, AType& id) {
+        return Analyse(PO,id,5.0);
+      }
 
   };
 
@@ -77,9 +117,20 @@ namespace mysimulator {
   }
 
   template <PDBContactAnalyseModeName AM, PDBContactStructureModeName SM>
+  unsigned int PDB2Contact(
+      const PDBObject& PO, Array2DContent<unsigned int>& id){
+    return _ContactFunctor<AM,SM,Array2DContent<unsigned int> >::Analyse(PO,id);
+  }
+
+  template <PDBContactAnalyseModeName AM, PDBContactStructureModeName SM>
   unsigned int PDB2Contact(const PDBObject& PO, Array2DContent<int>& id,
                            const double& th){
     return _ContactFunctor<AM,SM,Array2DContent<int> >::Analyse(PO,id,th);
+  }
+
+  template <PDBContactAnalyseModeName AM, PDBContactStructureModeName SM>
+  unsigned int PDB2Contact(const PDBObject& PO, Array2DContent<int>& id) {
+    return _ContactFunctor<AM,SM,Array2DContent<int> >::Analyse(PO,id);
   }
 
 }
