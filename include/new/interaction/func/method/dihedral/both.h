f1af4ec7ae6273c64a744acf571b989bb1d2c928
Jun Wang <junwang.nju@gmai.com>
2012-04-16 19:38:20 -0400

    update interaction method
    The interaction method is updated.

--- include/new/interaction/func/method/dihedral/both.h
+++ include/new/interaction/func/method/dihedral/both.h
@@ -2,10 +2,9 @@
 #ifndef _Interaction_Func_Method_Dihedral_Both_H_
 #define _Interaction_Func_Method_Dihedral_Both_H_
 
-#include "unique/64bit/interface.h"
 #include "distance/calc.h"
 #include "array/1d/cross.h"
-#include "interaction/func/impl/common/parameter/name.h"
+#include "interaction/buffer/interface.h"
 #include "interaction/func/impl/dihedral/common/buffer/name.h"
 
 namespace mysimulator {
@@ -14,44 +13,43 @@ namespace mysimulator {
   void BFuncMethodDihedral(
       const Array1DContent<T>* X, const int* idx, const Unique64Bit* P,
       const GeomType& Geo, T& Energy, Array1DContent<T>* Grad,
-      void (*ufunc)(const T*,const Unique64Bit*,T*),
+      InteractionBuffer<T>& Buf,
       void (*bfunc)(const T*,const Unique64Bit*,T*,T*)) {
-    assert(IsValid(tmvec));
-    T* buffer=reinterpret_cast<T*>(P[InteractionBuffer].ptr[0]);
-    Array1D<T>* tmvec=
-      reinterpret_cast<Array1D<T>*>(P[InteractionArrayBuffer].ptr[0]);
     unsigned int I=idx[0], J=idx[1], K=idx[2], L=idx[3];
-    if(P[InteractionBufferFlag].u[0]==0) {
-      T dt[7];
-      DisplacementCalc(tmvec[0],X[J],X[I],Geo);
-      dt[4]=DistanceSQ(tmvec[1],X[K],X[J],Geo);
-      DisplacementCalc(tmvec[2],X[L],X[K],Geo);
-      cross(tmvec[3],tmvec[0],tmvec[1]);
-      cross(tmvec[4],tmvec[1],tmvec[2]);
-      dt[0]=dot(tmvec[3],tmvec[3]);
-      dt[1]=dot(tmvec[4],tmvec[4]);
-      dt[2]=dot(tmvec[3],tmvec[4]);
-      cross(tmvec[5],tmvec[3],tmvec[4]);
-      dt[3]=dot(tmvec[1],tmvec[5]);
-      dt[5]=dot(tmvec[0],tmvec[1]);
-      dt[6]=dot(tmvec[1],tmvec[2]);
-      ufunc(dt,P,buffer);
+    if(Buf.postUpdate) {
+      DisplacementCalc(Buf.tmvec[0],X[J],X[I],Geo);
+      DisplacementCalc(Buf.tmvec[1],X[K],X[J],Geo);
+      DisplacementCalc(Buf.tmvec[2],X[L],X[K],Geo);
+      cross(Buf.tmvec[3],Buf.tmvec[0],Buf.tmvec[1]);
+      cross(Buf.tmvec[4],Buf.tmvec[1],Buf.tmvec[2]);
+      cross(Buf.tmvec[5],Buf.tmvec[3],Buf.tmvec[4]);
+      if(IsValid(Buf.inf)) Buf.GetPreBoth(&Buf,Buf.inf.start,Buf.pre.start);
+      else {
+        Buf.pre[0]=1./normSQ(Buf.tmvec[3]);
+        Buf.pre[1]=1./normSQ(Buf.tmvec[4]);
+        Buf.pre[4]=normSQ(Buf.tmvec[1]);
+        Buf.pre[5]=dot(Buf.tmvec[0],Buf.tmvec[1]);
+        Buf.pre[6]=dot(Buf.tmvec[1],Buf.tmvec[2]);
+      }
+      Buf.pre[2]=dot(Buf.tmvec[3],Buf.tmvec[4]);
+      Buf.pre[3]=dot(Buf.tmvec[1],Buf.tmvec[5]);
+      Buf.P2PBoth(Buf.pre.start,P,Buf.post.start,Buf.postUpdate);
     }
     T ee,ef;
-    bfunc(buffer,P,&ee,&ef);
+    bfunc(Buf.post.start,P,&ee,&ef);
     Energy+=ee;
 
-    T efi=ef*buffer[DihedralFactorI];
-    T efj=ef*buffer[DihedralFactorJ];
-    T efk=ef*buffer[DihedralFactorK];
-    T efl=ef*buffer[DihedralFactorL];
+    T efi=ef*Buf.post[DihedralFactorI];
+    T efj=ef*Buf.post[DihedralFactorJ];
+    T efk=ef*Buf.post[DihedralFactorK];
+    T efl=ef*Buf.post[DihedralFactorL];
 
-    shift(Grad[I],efi,tmvec[3]);
-    shift(Grad[L],efl,tmvec[4]);
-    shift(Grad[J],-efi-efj,tmvec[3]);
-    shift(Grad[J],+efk,tmvec[4]);
-    shift(Grad[K],+efj,tmvec[3]);
-    shift(Grad[K],-efl-efk,tmvec[4]);
+    shift(Grad[I],efi,Buf.tmvec[3]);
+    shift(Grad[L],efl,Buf.tmvec[4]);
+    shift(Grad[J],-efi-efj,Buf.tmvec[3]);
+    shift(Grad[J],+efk,Buf.tmvec[4]);
+    shift(Grad[K],+efj,Buf.tmvec[3]);
+    shift(Grad[K],-efl-efk,Buf.tmvec[4]);
   }
 
 }
