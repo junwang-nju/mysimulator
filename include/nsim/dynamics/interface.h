8f15ff243d0536eac42de6deafa4eca2ada72adf
Jun Wang <junwang.nju@gmail.com>
2012-04-22 22:34:29 -0400

    start dynamics port
    start to port dynamics

---
+++ include/nsim/dynamics/interface.h
@@ -0,0 +1,17 @@
+
+#ifndef _Dynamics_Interface_H_
+#define _Dynamics_Interface_H_
+
+#include "dynamics/mode-name.h"
+#include "system/interface.h"
+
+namespace mysimulator {
+
+  template <DynamicsModeName DN,typename T,typename GRNGType>
+  struct Dynamics {
+  };
+
+}
+
+#endif
+
---
+++ include/nsim/dynamics/mode-name.h
@@ -0,0 +1,16 @@
+
+#ifndef _Dynamics_Mode_Name_H_
+#define _Dynamics_Mode_Name_H_
+
+namespace mysimulator {
+
+  enum DynamicsModeName {
+    MicroCanonicalVelVerlet,
+    LangevinVelVerlet,
+    BerendsenVelVerlet
+  };
+
+}
+
+#endif
+
