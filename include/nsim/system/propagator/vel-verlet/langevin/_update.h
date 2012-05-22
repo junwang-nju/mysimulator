acaad093965b7a371d640053d7b5dad95dc5b1e2
Jun Wang <junwang.nju@gmai.com>
2012-05-22 06:18:41 -0400

    add update func for langevin
    update func for langevin propagator is added.

--- include/nsim/system/propagator/vel-verlet/langevin/_update.h
+++ include/nsim/system/propagator/vel-verlet/langevin/_update.h
@@ -6,13 +6,14 @@
 #include "array/2d/copy.h"
 #include "array/2d/inverse.h"
 #include "array/2d/scale.h"
+#include "array/2d/shift.h"
 #include "array/2d/square-root.h"
 
-#if !(defined(_VALUE)||defined(_ARRAY)||defined(_CARRAY))
+#if !(defined(_VALUE)||defined(_ARRAY))
 #define _VALUE(U) (*Pointer<T>(U))
 #define _ARRAY(U) (*Pointer<Array1D<Array2D<T> > >(U))
 #else
-#error "Duplicate Definition for Macro _VALUE,_ARRAY,_CARRAY"
+#error "Duplicate Definition for Macro _VALUE,_ARRAY"
 #endif
 
 namespace mysimulator {
@@ -29,10 +30,10 @@ namespace mysimulator {
       const T& dt,const T& Tempe,const Unique64Bit& Mass,
       const Unique64Bit& Fric,Unique64Bit& RSize) {
     for(unsigned int i=0;i<_ARRAY(Mass).Size();++i) {
-      BlasCopy(_Array(RSize)[i],_Array(Mass)[i]);
-      Inverse(_Array(RSize)[i]);
-      BlasScale(_Array(RSize)[i],dt*Tempe*_VALUE(Fric));
-      Sqroot(_Array(RSize)[i]);
+      BlasCopy(_ARRAY(RSize)[i],_ARRAY(Mass)[i]);
+      Inverse(_ARRAY(RSize)[i]);
+      BlasScale(_ARRAY(RSize)[i],dt*Tempe*_VALUE(Fric));
+      Sqroot(_ARRAY(RSize)[i]);
     }
   }
 
@@ -40,10 +41,10 @@ namespace mysimulator {
   void _UpdateVelVerletLangevinRandSize_UMassAFric(
       const T& dt,const T& Tempe,const Unique64Bit& Mass,
       const Unique64Bit& Fric,Unique64Bit& RSize) {
-    for(unsigned int i=0;i<_Array(Fric).Size();++i) {
-      BlasCopy(_Array(RSize)[i],_Array(Fric)[i]);
-      BlasScale(_Array(RSize)[i],dt*Tempe/_VALUE(Mass));
-      Sqroot(_Array(RSize)[i]);
+    for(unsigned int i=0;i<_ARRAY(Fric).Size();++i) {
+      BlasCopy(_ARRAY(RSize)[i],_ARRAY(Fric)[i]);
+      BlasScale(_ARRAY(RSize)[i],dt*Tempe/_VALUE(Mass));
+      Sqroot(_ARRAY(RSize)[i]);
     }
   }
 
@@ -51,12 +52,13 @@ namespace mysimulator {
   void _UpdateVelVerletLangevinRandSize_AMassAFric(
       const T& dt,const T& Tempe,const Unique64Bit& Mass,
       const Unique64Bit& Fric,Unique64Bit& RSize) {
+    assert(_ARRAY(Mass).Size()==_ARRAY(Fric).Size());
     for(unsigned int i=0;i<_ARRAY(Mass).Size();++i) {
-      BlasCopy(_Array(RSize)[i],_Array(Mass)[i]);
-      Inverse(_Array(RSize)[i]);
-      BlasScale(_Array(RSize)[i],dt*Tempe);
-      BlasScale(_Array(RSize)[i],_Array(Fric)[i]);
-      Sqroot(_Array(RSize)[i]);
+      BlasCopy(_ARRAY(RSize)[i],_ARRAY(Mass)[i]);
+      Inverse(_ARRAY(RSize)[i]);
+      BlasScale(_ARRAY(RSize)[i],dt*Tempe);
+      BlasScale(_ARRAY(RSize)[i],_ARRAY(Fric)[i]);
+      Sqroot(_ARRAY(RSize)[i]);
     }
   }
 
@@ -73,6 +75,96 @@ namespace mysimulator {
   void _UpdateVelVerletFac_AMassUFric(
       const T& dt,const Unique64Bit& Mass,const Unique64Bit& Fric,
       Unique64Bit& FacBf, Unique64Bit& FacAf) {
+    for(unsigned int i=0;i<_ARRAY(Mass).Size();++i) {
+      BlasCopy(_ARRAY(FacBf)[i],_ARRAY(Mass)[i]);
+      Inverse(_ARRAY(FacBf)[i]);
+      BlasScale(_ARRAY(FacBf)[i],-0.5*dt*_VALUE(Fric));
+      BlasCopy(_ARRAY(FacAf)[i],_ARRAY(FacBf)[i]);
+      BlasShift(_ARRAY(FacBf)[i],1.);
+      BlasScale(_ARRAY(FacAf)[i],-1.);
+      BlasShift(_ARRAY(FacAf)[i],1.);
+      Inverse(_ARRAY(FacAf)[i]);
+    }
+  }
+
+  template <typename T>
+  void _UpdateVelVerletFac_UMassAFric(
+      const T& dt,const Unique64Bit& Mass,const Unique64Bit& Fric,
+      Unique64Bit& FacBf, Unique64Bit& FacAf) {
+    for(unsigned int i=0;i<_ARRAY(Fric).Size();++i) {
+      BlasCopy(_ARRAY(FacBf)[i],_ARRAY(Fric)[i]);
+      BlasScale(_ARRAY(FacBf)[i],-0.5*dt/_VALUE(Mass));
+      BlasCopy(_ARRAY(FacAf)[i],_ARRAY(FacBf)[i]);
+      BlasShift(_ARRAY(FacBf)[i],1.);
+      BlasScale(_ARRAY(FacAf)[i],-1.);
+      BlasShift(_ARRAY(FacAf)[i],1.);
+      Inverse(_ARRAY(FacAf)[i]);
+    }
+  }
+
+  template <typename T>
+  void _UpdateVelVerletFac_AMassAFric(
+      const T& dt,const Unique64Bit& Mass,const Unique64Bit& Fric,
+      Unique64Bit& FacBf, Unique64Bit& FacAf) {
+    assert(_ARRAY(Mass).Size()==_ARRAY(Fric).Size());
+    for(unsigned int i=0;i<_ARRAY(Mass).Size();++i) {
+      BlasCopy(_ARRAY(FacBf)[i],_ARRAY(Mass)[i]);
+      Inverse(_ARRAY(FacBf)[i]);
+      BlasScale(_ARRAY(FacBf)[i],-0.5*dt);
+      BlasScale(_ARRAY(FacBf)[i],_ARRAY(Fric)[i]);
+      BlasCopy(_ARRAY(FacAf)[i],_ARRAY(FacBf)[i]);
+      BlasShift(_ARRAY(FacBf)[i],1.);
+      BlasScale(_ARRAY(FacAf)[i],-1.);
+      BlasShift(_ARRAY(FacAf)[i],1.);
+      Inverse(_ARRAY(FacAf)[i]);
+    }
+  }
+
+}
+
+#if defined(_VALUE)||defined(_ARRAY)
+#undef _ARRAY
+#undef _VALUE
+#endif
+
+#include "system/propagator/vel-verlet/const-e/_update.h"
+
+namespace mysimulator {
+
+  template <typename T>
+  void _UpdateVelVerletLangevinHTIM_UMass(
+      const T& dt,const Unique64Bit& Mass,Unique64Bit& NegHTIM) {
+    _UpdateVelVerletConstEHTIM_UMass(dt,Mass,NegHTIM);
+  }
+
+  template <typename T>
+  void _UpdatevelVerletLangevinHTIM_AMass(
+      const T& dt,const Unique64Bit& Mass,Unique64Bit& NegHTIM) {
+    _UpdateVelVerletConstEHTIM_AMass(dt,Mass,NegHTIM);
+  }
+
+  template <typename T, template<typename> class CT>
+  void _UpdateVelVerletLangevinVSQ_UMass(
+      Unique64Bit& VSQ, const Array1D<CT<T> >& gC) {
+    _UpdateVelVerletConstEVSQ_UMass(VSQ,gC);
+  }
+
+  template <typename T, template<typename> class CT>
+  void _UpdateVelVerletLangevinVSQ_AMass(
+      Unique64Bit& VSQ, const Array1D<CT<T> >& gC) {
+    _UpdateVelVerletConstEVSQ_AMass(VSQ,gC);
+  }
+
+  template <typename T>
+  void _UpdateVelVerletLangevinKEnergySimple_UMass(
+      T& KE,const Unique64Bit& Mass,const Unique64Bit& VelSQ) {
+    _UpdateVelVerletConstEKEnergySimple_UMass(KE,Mass,VelSQ);
+  }
+
+  template <typename T>
+  void _UpdateVelVerletLangevinKEnergySimple_AMass(
+      T& KE,const Unique64Bit& Mass,const Unique64Bit& VelSQ) {
+    _UpdateVelVerletConstEKEnergySimple_AMass(KE,Mass,VelSQ);
   }
 
 }
