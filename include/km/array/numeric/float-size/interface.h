8d3fb64b8c0f467349f436b8b47375813a19c89e
Jun Wang <junwang.nju@gmail.com>
2012-05-22 15:48:49 -0400

    add float-size array
    the float-size array is added, not finished.

---
+++ include/km/array/numeric/float-size/interface.h
@@ -0,0 +1,15 @@
+
+#ifndef _Array_Numeric_FloatSize_H_
+#define _Array_Numeric_FloatSize_H_
+
+#include "array/numeric/base/interface.h"
+
+namespace mysimulator {
+
+  template <typename T>
+  class ArrayNumericFloatSizeFlag { static const bool Flag; };
+
+}
+
+#endif
+
