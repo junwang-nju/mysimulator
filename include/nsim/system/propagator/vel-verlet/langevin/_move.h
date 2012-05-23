3808cd3fe3d09d73f0a2bd1a9ebd3317d2d70acd
Jun Wang <junwang.nju@gmai.com>
2012-05-23 09:45:42 -0400

    add move func for langevin
    the move func for langevin is added.

---
+++ include/nsim/system/propagator/vel-verlet/langevin/_move.h
@@ -0,0 +1,19 @@
+
+#ifndef _System_Propagator_VelVerlet_Langevin_Move_Func_H_
+#define _System_Propagator_VelVerlet_Langevin_Move_Func_H_
+
+#include "unique/64bit/interface.h"
+#include "array/2d/scale.h"
+#include "array/2d/shift.h"
+
+#if !(defined(_VALUE)||defined(_ARRAY))
+#define _VALUE(U) (*Pointer<T>(U))
+#define _ARRAY(U) (*Pointer<Array1D<Array2D<T> > >(U))
+#else
+#error "Duplicate Definition for Macro _VALUE,_ARRAY"
+#endif
+
+namespace
+
+#endif
+
