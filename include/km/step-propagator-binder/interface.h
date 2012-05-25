842ac67f3f4402aa7e8365e5ddc734bc4646499f
Jun Wang <junwang.nju@gmail.com>
2012-05-25 15:58:45 -0400

    add binder for step-propagator
    for system, some step-propgators may work at the same time. However the
    force evaluation is a global operation. The binder connects the
    propagators and force-calc together.

---
+++ include/km/step-propagator-binder/interface.h
@@ -0,0 +1,48 @@
+
+#ifndef _Step_Propagator_Binder_Interface_H_
+#define _Step_Propagator_Binder_Interface_H_
+
+#include "step-propagator/interface.h"
+#include "system-interaction/interface.h"
+
+namespace mysimulator {
+
+  template <typename T,typename GeomType>
+  class StepPropagatorBinder {
+
+    public:
+
+      typedef StepPropagatorBinder<T,GeomType>  Type;
+
+      StepPropagatorBinder() {}
+      ~StepPropagatorBinder() { Clear(*this); }
+
+      bool IsWorkable(const Array<StepPropagator<T>*>& Props) {
+        assert(Props.IsValid());
+        for(unsigned int i=0;i<Props.Size();++i) {
+          assert(Props[i]!=NULL);
+          if(!IsWorkable(Props[i]->_tag))   return false;
+        }
+        return true;
+      }
+
+      virtual void Evolute(ArrayNumeric<ArrayNumeric<T> >& X,
+                           SystemInteraction<T,GeomType>& I,
+                           Array<StepPropagator<T>*>& Props)=0;
+
+    private:
+
+      virtual bool IsWorkable(const StepPropagatorName)=0;
+
+      StepPropagatorBinder(const Type&) {}
+      Type& operator=(const Type&) { return *this; }
+
+  };
+
+  template <typename T,typename GT>
+  void Clear(StepPropagatorBinder<T,GT>&) {}
+
+}
+
+#endif
+
---
+++ include/km/step-propagator-binder/vel-verlet/interface.h
@@ -0,0 +1,63 @@
+
+#ifndef _Step_Propagator_Binder_VelVerlet_Interface_H_
+#define _Step_Propagator_Binder_VelVerlet_Interface_H_
+
+#include "step-propagator-binder/interface.h"
+
+namespace mysimulator {
+
+  template <typename T,typename GT>
+  class StepPropagatorVelVerletBinder : public StepPropagatorBinder<T,GT> {
+
+    public:
+
+      typedef StepPropagatorVelVerletBinder<T,GT>   Type;
+      typedef StepPropagatorBinder<T,GT>  ParentType;
+
+      StepPropagatorVelVerletBinder() : ParentType() {}
+      ~StepPropagatorVelVerletBinder() { Clear(*this); }
+
+      virtual void Evolute(ArrayNumeric<ArrayNumeric<T> >& X,
+                           SystemInteraction<T,GT>& I,
+                           Array<StepPropagator<T>*>& Props) {
+        // assuming X,G have been connected with Props
+        assert(X.IsValid());
+        assert(I.IsValid());
+        assert(Props.IsValid());
+        for(unsigned int i=0;i<Props.Size();++i) {
+          assert(Props[i]!=NULL);
+          Props[i]->Evolute1();
+        }
+        I.CalcG(X);
+        for(unsigned int i=0;i<Props.Size();++i) {
+          assert(Props[i]!=NULL);
+          Props[i]->Evolute2();
+        }
+      }
+
+    private:
+
+      virtual bool IsWorkable(const StepPropagatorName SPN) {
+        switch(SPN) {
+          case VelVerletConstE:
+          case VelVerletLangevin:
+            return true;
+        }
+        return false;
+      }
+
+      StepPropagatorVelVerletBinder(const Type&) {}
+      Type& operator=(const Type&) { return *this; }
+
+  };
+
+  template <typename T,typename GT>
+  void Clear(StepPropagatorVelVerletBinder<T,GT>& B) {
+    typedef typename StepPropagatorVelVerletBinder<T,GT>::ParentType PType;
+    Clear(static_cast<PType&>(B));
+  }
+
+}
+
+#endif
+
