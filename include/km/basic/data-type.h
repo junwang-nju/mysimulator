d9217e0509580d7b8e2d2fa4e315e47dd0a7787a
Jun Wang <junwang.nju@gmai.com>
2012-05-28 23:34:27 -0400

    finish sum
    the data structure for sum is finished.

--- include/km/basic/data-type.h
+++ include/km/basic/data-type.h
@@ -9,16 +9,35 @@ namespace mysimulator {
 
 }
 
+#ifndef _DataDEF_
 #define _DataDEF_(T) \
   template <> class DataType<T> { public: typedef T Type; };
+#else
+#error "Duplicate _DataDEF_"
+#endif
 
 namespace mysimulator {
 
+  _DataDEF_(long double)
   _DataDEF_(double)
+  _DataDEF_(float)
+  _DataDEF_(long long)
+  _DataDEF_(unsigned long long)
+  _DataDEF_(int)
+  _DataDEF_(unsigned int)
+  _DataDEF_(long)
+  _DataDEF_(unsigned long)
+  _DataDEF_(short)
+  _DataDEF_(unsigned short)
+  _DataDEF_(char)
+  _DataDEF_(unsigned char)
+  _DataDEF_(bool)
 
 }
 
+#ifdef _DataDEF_
 #undef _DataDEF_
+#endif
 
 namespace mysimulator {
 
--- include/km/basic/sum.h
+++ include/km/basic/sum.h
@@ -2,12 +2,13 @@
 #ifndef _Basic_Sum_H_
 #define _Basic_Sum_H_
 
+#include "basic/data-type.h"
 #include "type-check/is-sumable.h"
 
 namespace mysimulator {
 
   template <typename T>
-  const T& _Sum(const T& v) {
+  const typename DataType<T>::Type& _Sum(const T& v) {
     typedef typename IsSumable<T>::Type   SumCheck;
     return v;
   }
