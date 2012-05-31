6ba681a00646bd09540b59f14554464f0796df81
Jun Wang <junwang.nju@gmai.com>
2012-05-31 06:49:21 -0400

    correct partial specialization problem
    partial specialization problem is corrected.

--- include/km/type/is-copyable.h
+++ include/km/type/is-copyable.h
@@ -2,6 +2,9 @@
 #ifndef _Type_IsCopyAble_H_
 #define _Type_IsCopyAble_H_
 
+#include "type/is-included.h"
+#include "type/is-numeric.h"
+
 namespace mysimulator {
 
   template <typename T1, typename T2>
@@ -9,31 +12,44 @@ namespace mysimulator {
     public: static const bool Flag;
   };
 
+  template <typename T1, typename T2>
+  const bool IsCopyableFlag<T1,T2>::Flag=
+    IsIncluded<T1,T2>::Flag&&IsNumeric<T1>::Flag&&IsNumeric<T2>::Flag;
+
+  template <typename T>
+  class IsCopyableFlag<T*,T*> {
+    public: static const bool Flag;
+  };
+
   template <typename T>
   const bool IsCopyableFlag<T*,T*>::Flag=true;
 
   template <typename T>
+  class IsCopyableFlag<void*,T*> {
+    public: static const bool Flag;
+  };
+
+  template <typename T>
   const bool IsCopyableFlag<void*,T*>::Flag=true;
+
+  template <typename T>
+  class IsCopyableFlag<T*,void*> {
+    public: static const bool Flag;
+  };
 
   template <typename T>
   const bool IsCopyableFlag<T*,void*>::Flag=true;
 
 }
 
-#include "type/is-included.h"
-#include "type/is-numeric.h"
-
-namespace mysimulator {
-
-  template <typename T1, typename T2>
-  const bool IsCopyableFlag<T1,T2>::Flag=
-    IsIncluded<T1,T2>::Flag&&IsNumeric<T1>::Flag&&IsNumeric<T2>::Flag;
-
-}
-
 #include "type/is-integer.h"
 
 namespace mysimulator {
+
+  template <typename T>
+  class IsCopyableFlag<bool,T> {
+    public: static const bool Flag;
+  };
 
   template <typename T>
   const bool IsCopyableFlag<bool,T>::Flag=IsInteger<T>::Flag;
