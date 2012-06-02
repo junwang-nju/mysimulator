8202ab9cdd8816788107e926f636fbc4c6be8f83
Jun Wang <junwang.nju@gmail.com>
2012-06-02 00:31:42 -0400

    modify the neighbor-list object
    the neighbor-list object is modified. The checking for update is removed
    outside to match the condition with multiple lists.

--- include/km/neighbor-list/interface.h
+++ include/km/neighbor-list/interface.h
@@ -17,40 +17,47 @@ namespace mysimulator {
       typedef NeighborList<T,GT>    Type;
       friend void Clear<T,GT>(NeighborList<T,GT>&);
 
-      NeighborList() : _IDPool(), _ParamPool(), _Used(), _Tag(), _CutR(0),
-                       _SkinR(0), _BoundRSQ(0) {}
+      NeighborList() : _FuncPool(),_IDPool(),_ParamPool(),_Used(),_NUsed(0U),
+                       _CutR(0),_SkinR(0),_BoundRSQ(0),_HfSkinRSQ(0),Dsp() {}
       ~NeighborList() { Clear(*this); }
 
       bool IsValid() const {
-        return _IDPool.IsValid()&&_ParamPool.IsValid()&&_Used.IsValid()&&
-               (_IDPool.Size()==_ParamPool.Size())&&
-               (_IDPool.Size()==_Used.Size())&&(_CutR>0)&&(_SkinR>0)&&
-               _CheckRadius();
+        return
+          _FuncPool.IsValid() && _IDPool.IsValid() && _ParamPool.IsValid() &&
+          _Used.IsValid() && (_NUsed>0) && (_CutR>0) && (_SkinR>0) &&
+          (_FuncPool.Size()==_IDPool.Size()) &&
+          (_FuncPool.Size()==_ParamPool.Size()) &&
+          (_FuncPool.Size()==_Used.Size()) && _RadiusCheck();
       }
-      void Allocate(const Array<InteractionFuncName>& PIFN) {
-        assert(_CheckType(PIFN));
+
+      void Allocate(const Array<InteractionFuncName>& IFN, unsigned int dim) {
+        assert(_TagCheck(IFN));
         Clear(*this);
-        _Tag.Allocate(PIFN.Size());
-        _Tag.Copy(PIFN);
-        _IDPool.Allocate(PIFN.Size());
-        _ParamPool.Allocate(PIFN.Size());
-        for(unsigned int i=0;i<PIFN.Size();++i) {
-          Introduce(_ParamPool[i],PIFN[i]);
-          _ID[i].Allocate(InteractionFuncIDSize[PIFN[i]]);
+        const unsigned int n=IFN.Size();
+        _FuncPool.Allocate(n);
+        _IDPool.Allocate(n);
+        _ParamPool.Allocate(n);
+        for(unsigned int i=0;i<n;++i) {
+          Introduce(_FuncPool[i],IFN[i],dim);
+          Introduce(_ParamPool[i],IFN[i]);
+          _ID[i].Allocate(InteractionFuncIDSize[IFN[i]]);
         }
-        _Used.Allocate(PIFN.Size());
+        _Used.Allocate(n);
+        _NUsed=0;
+        Dsp.Allocate(dim);
       }
 
       Array<unsigned int>& Index(unsigned int i) {
-        assert(_IDPool.IsValid()); return _IDPool[i];
+        assert(_IDPool.IsValid());  return _IDPool[i];
       }
+      const Array<unsigned int>& Index(unsigned int i) {
+        assert(_IDPool.IsValid());  return _IDPool[i];
+      }
+
       Unique64Bit& Parameter(unsigned int n,unsigned int l) {
         assert(_ParamPool.IsValid());
         assert(_ParamPool[n]!=NULL);
         return (*_ParamPool[n])[l];
-      }
-      const Array<unsigned int>& Index(unsigned int i) const {
-        assert(_IDPool.IsValid()); return _IDPool[i];
       }
       const Unique64Bit& Parameter(unsigned int n,unsigned int l) const {
         assert(_ParamPool.IsValid());
@@ -61,85 +68,102 @@ namespace mysimulator {
       void SetRadius(const T& CR, const T& SR) {
         _CutR=CR;
         _SkinR=SR;
+        _HfSkinRSQ=_SkinR*0.5;
+        _HfSkinRSQ*=_HfSkinRSQ;
         _BoundRSQ=_CutR+_SkinR;
         _BoundRSQ*=_BoundRSQ;
       }
-      void Update(SystemInteraction<T,GT>& SI) {
+
+      const T& ThresholdDistance() const { return _HfSkinRSQ; }
+
+      void Update(const Array2DNumeric<T>& X, SystemInteraction<T,GT>& SI) {
+        const unsigned int n=_FuncPool.Size();
+        unsigned int I,J;
+        T dist;
+        _NUsed=0;
+        for(unsigned int i=0;i<n;++i) {
+          I=_IDPool[i][0];
+          J=_IDPool[i][1];
+          dist=DistanceSQ(Dsp,X[I],X[J],SI.Geometry());
+          if(dist<_BoundRSQ) { _Used[_NUsed]=i; ++_NUsed; }
+        }
+        Clear(SI._Func);
+        Clear(SI._ID);
+        Clear(SI._Param);
+        SI._Func.Allocate(_NUsed);
+        SI._ID.Allocate(_NUsed);
+        SI._Param.Allocate(_NUsed);
+        unsigned int m;
+        for(unsigned int i=0;i<_NUsed;++i) {
+          m=_Used[i];
+          SI._Func[i]=_FuncPool[m];
+          SI._ID[i].Refer(_IDPool[m]);
+          SI._Param[i]=_ParamPool[m];
+        }
       }
 
     protected:
 
-      Array<Array<unsigned int> >     _IDPool;
+      Array<InteractionFunc<T,GT>*>   _FuncPool;
+      Array<Array<unsigned int>* >    _IDPool;
       Array<InteractionParameter<T>*> _ParamPool;
-      Array<bool>                     _Used;
-      Arrag<InteractionFuncName>      _Tag;
+      Array<unsigned int> _Used;
+      unsigned int _NUsed;
       T _CutR;
       T _SkinR;
       T _BoundRSQ;
+      T _HfSkinRSQ;
 
     private:
 
       NeighborList(const Type&) {}
       Type& operator=(const Type&) { return *this; }
 
-      bool _CheckRadius() const {
-        bool _check=true;
-        for(unsigned int i=0;_check&&(i<_ParamPool.Size());++i)
-          switch(_Tag[i]) {
-            case LJ612Cut:
-              if(Parameter(i,LJ612CutCutR)>_CutR) _check=false;
-              break;
-            case LJ1012Cut:
-              if(Parameter(i,LJ1012CutCutR)>_CutR)  _check=false;
-              break;
-            case CoreLJ612:
-              if(Parameter(i,CoreLJ612Radius)>_CutR)  _check=false;
-              break;
-            default:
-              fprintf(stderr,"Improper Interaction Func Name!\n");
-          }
-        return _check;
+      const T& _CutRadius(unsigned int i) const {
+        assert(_FuncPool.IsValid());
+        assert(_ParamPool.IsValid());
+        unsigned int n=0;
+        switch(_FuncPool[i]->Name()) {
+          case LJ612Cut: n=LJ612CutCutR;  break;
+          case LJ1012Cut: n=LJ1012CutCutR; break;
+          case CoreLJ612: n=CoreLJ612Radius; break;
+          default: fprintf(stderr,"Improper Interaction Func Name!\n");
+        }
+        assert(n!=0);
+        return Value<T>(Parameter(i,n));
       }
-      bool _CheckType(const SystemInteraction<T,GT>& SI) const {
-        assert(IsValid());
-        assert(SI._Func.Size()==_Tag.Size());
-        bool _check=true;
-        for(unsigned int i=0;i<SI._Func.Size();++i)
-          if(SI._Func[i]->Name()!=_Tag[i]) { _check=false; break; }
-        return _check;
+
+      bool _RadiusCheck() const {
+        bool check=true;
+        for(unsigned int i=0;i<_FuncPool.Size();++i)
+          if(_CutRadius(i)>_CutR) { check=false; break; }
+        return check;
       }
-      bool _CheckType(const Array<InteractionFuncName>& IFN) {
-        bool _check=true;
-        for(unsigned int i=0;_check&&(i<IFN.Size());++i)
+      bool _TagCheck(const Array<InteractionFuncName>& IFN) const {
+        bool check=true;
+        for(unsigned int i=0;check && (i<IFN.Size()) ;++i)
           switch(IFN[i]) {
             case LJ612Cut:
             case LJ1012Cut:
             case CoreLJ612: break;
-            default:  _check=false;
+            default: check=false;
           }
-        return _check;
+        return check;
       }
-
-      bool _Check(const SystemInteraction<T,GT>& S) const {
-        assert(_CheckType(S));
-        bool _check=false;
-        for(unsigned int i=0;i<S._Func.Size();++i) {
-          if(S._Func[i]->_pre[PairwiseDistanceSQ]>_BoundRSQ) {
-            _check=true; break;
-          }
-          return _check;
-      }
-
+      
   };
 
   template <typename T,typename GT>
   void Clear(NeighborList<T,GT>& L) {
-    L._BoundRSQ=0;
+    L._HfSkinRSQ=0;
     L._SkinR=0;
     L._CutR=0;
+    L._NUsed=0;
     Clear(L._Used);
     Clear(L._ParamPool);
     Clear(L._IDPool);
+    Clear(L._FuncPool);
+    Clear(L.Dsp);
   }
 
 }
