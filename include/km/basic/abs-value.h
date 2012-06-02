43f65045a0a8cfc88bb354f120d5a09189c0a521
Jun Wang <junwang.nju@gmail.com>
2012-06-02 11:38:10 -0400

    add document
    add document for some modules

--- include/km/basic/abs-value.h
+++ include/km/basic/abs-value.h
@@ -3,6 +3,10 @@
  * @file basic/abs-value.h
  * @brief calculate absolute value for data of basic type
  *
+ * this module calculates the absolute value of data of intrinsic type.
+ * this is a basic mathematical operation for various kinds of sign numbers.
+ * These functions unify the interface for vatious types of numbers.
+ *
  * @author Jun | junwang.nju@gmail.com
  */
 
@@ -13,6 +17,16 @@
 #include <cstdlib>
 
 #ifndef _AbsValDEF_
+/** @brief calculate absolute value with pre-defined method
+ *
+ * For some data types (including float-pointer data, and long long, long,
+ * and int), there are some pre-defined methods. This macro implements
+ * a unique interface to calculate absolute value of the input data with
+ * these pre-defined data.
+ *
+ * @param T the type of input data
+ * @param Func the function to carry out the calculation
+ */
 #define _AbsValDEF_(T,Func)  T AbsVal(const T& d) { return Func(d); }
 #else
 #error "Duplicate _AbsValDEF_"
@@ -34,6 +48,14 @@ namespace mysimulator {
 #endif
 
 #ifndef _AbsValDEF_
+/** @brief calculate absolute value with comparison
+ *
+ * For some data types (such as char and short), there are no pre-defined
+ * methods to calculate absolute values. The calculation is implemented
+ * by comparing the value with zero.
+ *
+ * @param T the type of input data
+ */
 #define _AbsValDEF_(T) T AbsVal(const T& d) { return d>0?d:-d; }
 #else
 #error "Duplicate _AbsValDEF_"
@@ -51,6 +73,13 @@ namespace mysimulator {
 #endif
 
 #ifndef _AbsValDEF_
+/**
+ * @brief calculate absolute value for unsigned value
+ *
+ * the absolute value of unsigned number is itself.
+ *
+ * @param T the type of input data
+ */
 #define _AbsValDEF_(T) T AbsVal(const T& d) { return d; }
 #else
 #error "Duplicate _AbsValDEF_"
