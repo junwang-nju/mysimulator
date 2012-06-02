a511d15366b8b28287620a4c5d083e421a6a9c31
Jun Wang <junwang.nju@gmail.com>
2012-06-02 15:34:41 -0400

    add more documents
    mysimulator.h is introduced to include main page for doxygen.

---
+++ include/km/mysimulator.h
@@ -0,0 +1,24 @@
+
+/** @mainpage MySimulator Package
+ *
+ * This is the readme file for mysimulator package.
+ *
+ * The mysimulator package is used to define some basic API to develop
+ * simulation codes. It is implemented with C++. Generic programming is
+ * also largely used.
+ *
+ * Present finished Document:
+ *
+ * \arg type/blas-size.h
+ */
+
+/**
+ * @namespace mysimulator
+ * @brief the namespace for MySimulator
+ *
+ * This namespace includes all the objects and functions to build
+ * simulation codes. This namespace is used isolate my codes with std
+ * names.
+ */
+namespace mysimulator {}
+
