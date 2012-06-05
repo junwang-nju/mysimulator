9bff720b65b22acdd9f44192df4cdd22fe8a0aa0
Jun Wang <junwang.nju@gmail.com>
2012-06-05 02:05:29 -0400

    add update name
    To include hessian calculation, the update flag should be expanded
    beyond the bool flag.

---
+++ include/km/interaction-func/update-name.h
@@ -0,0 +1,20 @@
+
+#ifndef _Interaction_Func_Update_Name_H_
+#define _Interaction_Func_Update_Name_H_
+
+namespace mysimulator {
+
+  enum InteractionFuncUpdateName {
+    EFuncUpdateOK,
+    GFuncUpdateOK,
+    HFuncUpdateOK,
+    EGFuncUpdateOK,
+    EHFuncUpdateOK,
+    GHFuncUpdateOK,
+    EGHFuncUpdateOK
+  };
+
+}
+
+#endif
+
