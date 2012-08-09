da9f26f9b304fad9cf5cfbe3e7d0a24eab911933
Jun Wang <junwang.nju@gmai.com>
2012-08-09 06:55:04 -0400

    IsIntrinsic Document update
    the document for IsIntrinsic is updated, not finished.

--- include/km/type/is-intrinsic.h
+++ include/km/type/is-intrinsic.h
@@ -3,6 +3,11 @@
  * @file type/is-intrinsic.h
  * @brief check if a type is an intrinsic type
  *
+ * Some operations only works for intrinsic types (such as the operations
+ * in \a basic directory. This is because that the intrinsic type in c++ is
+ * not same as an object. Some checks are necessary to check if the data
+ * is of intrinsic type. This file implements the related objects.
+ *
  * @author Jun | junwang.nju@gmail.com
  */
 
@@ -13,16 +18,39 @@
 
 namespace mysimulator {
 
+  /** @class IsIntrinsicFlag
+   * @brief the class to produce flag indicating if a type is intrinsic
+   *
+   * This class could produce a flag based on the input type. This is
+   * the basic component to determine if a type is an intrinsic type
+   *
+   * @tparam T the input type
+   */
   template <typename T>
-  class IsIntrinsicFlag { public: static const bool Flag; };
+  class IsIntrinsicFlag {
+    public:
+      /** @var Flag
+       * @brief the static flag indicating if the type is intrinsic
+       */
+      static const bool Flag;
+  };
 
   template <typename T>
   const bool IsIntrinsicFlag<T>::Flag=IsNumericFlag<T>::Flag;
 
-  template <> const bool IsIntrinsicFlag<bool>::Flag=true;
+  template <>
+  class IsIntrinsicFlag<bool> {
+    public:
+      static const bool Flag;
+  };
+
+  const bool IsIntrinsicFlag<bool>::Flag=true;
 
   template <typename T>
-  class IsIntrinsicFlag<T*> { public: static const bool Flag; };
+  class IsIntrinsicFlag<T*> {
+    public:
+      static const bool Flag;
+  };
 
   template <typename T>
   const bool IsIntrinsicFlag<T*>::Flag=true;
