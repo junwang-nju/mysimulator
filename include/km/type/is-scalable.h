1f9d9cceea280da61becd864f9458a9ba3c8d1e5
Jun Wang <junwang.nju@gmai.com>
2012-08-10 06:57:12 -0400

    document for IsShiftable and IsScalable update
    Document for IsShiftable and IsScalable are updated, but not finished.

--- include/km/type/is-scalable.h
+++ include/km/type/is-scalable.h
@@ -1,4 +1,10 @@
 
+/** @file type/is-scalable.h
+ * @brief check if the types are compatible with scale operation
+ *
+ * @author Jun | junwang.nju@gmail.com
+ */
+
 #ifndef _Type_IsScalable_H_
 #define _Type_IsScalable_H_
 
@@ -7,9 +13,18 @@
 
 namespace mysimulator {
 
+  /** @class IsScalableFlag
+   */
   template <typename T1, typename T2>
-  class IsScalableFlag {  public: static const bool Flag; };
+  class IsScalableFlag {
+    public:
+      /** @var Flag
+       */
+      static const bool Flag;
+  };
 
+  /** @var IsScalableFlag::Flag
+   */
   template <typename T1, typename T2>
   const bool IsScalableFlag<T1,T2>::Flag=
     IsIncludedFlag<T1,T2>::Flag&&IsNumericFlag<T1>::Flag&&
@@ -21,9 +36,13 @@ namespace mysimulator {
 
 namespace mysimulator {
 
+  /** @class IsScalable
+   */
   template <typename T1, typename T2>
   class IsScalable {
     public:
+      /** @typedef Type
+       */
       typedef typename Check<IsScalableFlag<T1,T2>::Flag>::Type   Type;
   };
 
--- include/km/type/is-shiftable.h
+++ include/km/type/is-shiftable.h
@@ -1,4 +1,10 @@
 
+/** @file type/is-shiftable.h
+ * @brief check if the types are compatible with shift operations
+ *
+ * @author Jun | junwang.nju@gmail.com
+ */
+
 #ifndef _Type_Is_Shiftable_H_
 #define _Type_Is_Shiftable_H_
 
@@ -7,14 +13,20 @@
 namespace mysimulator {
 
   template <typename T1, typename T2>
-  class IsShiftableFlag { public: static const bool Flag; };
+  class IsShiftableFlag {
+    public:
+      static const bool Flag;
+  };
 
   template <typename T1, typename T2>
   const bool IsShiftableFlag<T1,T2>::Flag=
     IsIncludedFlag<T1,T2>::Flag;
 
   template <typename T1, typename T2>
-  class IsNegShiftableFlag { public: static const bool Flag; };
+  class IsNegShiftableFlag {
+    public:
+      static const bool Flag;
+  };
 
 }
 
@@ -28,14 +40,20 @@ namespace mysimulator {
     IsShiftableFlag<T1,T2>::Flag&&(!IsUnsignedFlag<T1>::Flag);
 
   template <typename T1,typename T2,typename T3>
-  class IsShiftable3Flag { public: static const bool Flag; };
+  class IsShiftable3Flag {
+    public:
+      static const bool Flag;
+  };
 
   template <typename T1,typename T2,typename T3>
   const bool IsShiftable3Flag<T1,T2,T3>::Flag=
     IsShiftableFlag<T1,T2>::Flag&&IsShiftableFlag<T1,T3>::Flag;
 
   template <typename T1,typename T2,typename T3,typename T4>
-  class IsShiftable4Flag { public: static const bool Flag;  };
+  class IsShiftable4Flag {
+    public:
+      static const bool Flag;
+  };
 
   template <typename T1,typename T2,typename T3,typename T4>
   const bool IsShiftable4Flag<T1,T2,T3,T4>::Flag=
