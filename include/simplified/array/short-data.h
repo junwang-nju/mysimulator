ce969947c3256cbafa16705231fbc7989ca98943
Jun Wang <junwang.nju@gmail.com>
2012-10-11 09:15:47 -0400

    modify the warning in short-data array
    using intrinsic define to check if the sse4.1 option is opened. use
    -Wfatal-errors to stop the compilation at the first errors.

--- include/simplified/array/short-data.h
+++ include/simplified/array/short-data.h
@@ -5,14 +5,13 @@
 #include "array/interface.h"
 #include "array/expression.h"
 #include "basic/type/same.h"
-#include "basic/type/selector.h"
-#include "basic/type/sum.h"
-#include "basic/type/substract.h"
-#include "basic/type/multiple.h"
-#include "basic/type/divide.h"
 
-#ifndef _HAVE_SSE4
-#error "\nSSE4 is required for ShortData Array! Try -msse4 -D_HAVE_SSE4\n"
+//#ifndef _HAVE_SSE4
+//#error "\nSSE4 is required for ShortData Array! Try -msse4 -D_HAVE_SSE4\n"
+//#endif
+
+#ifndef __SSE4_1__
+#error "\nSSE4.1 is required for ShortData Array! Try -msse4.1 !\n"
 #endif
 
 #include <emmintrin.h>
