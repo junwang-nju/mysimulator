d90c068f7e1306daf4774815c8e0a8205f998abc
Jun Wang <junwang.nju@gmail.com>
2012-12-08 00:06:24 -0500

    test codes for molecule with ang-dih
    the codes to test for angle and dihedral are added.

--- include/simplified/r.cxx
+++ include/simplified/r.cxx
@@ -1,5 +1,6 @@
 
 #include "system/interface.h"
+#include "basic/util/point-2-dihedral.h"
 using namespace mysimulator;
 
 #include <iostream>
@@ -24,9 +25,11 @@ int main() {
   S._Interaction.allocate(NC);
   Array2D<InteractionName> FN;
   FN.allocate(NC,1);
-  for(unsigned int i=0;i<64+63;++i)  FN[i][0]=InteractionName::PairHarmonic;
-  for(unsigned int i=0;i<153;++i) FN[i+64+63][0]=InteractionName::PairLJ1012;
-  for(unsigned int i=153+64+63;i<NC;++i) FN[i]=InteractionName::PairCore12;
+  for(unsigned int i=0;i<64;++i)  FN[i][0]=InteractionName::PairHarmonic;
+  for(unsigned int i=0;i<63;++i)  FN[i+64][0]=InteractionName::AngleHarmonic;
+  for(unsigned int i=0;i<62;++i)  FN[i+64+63][0]=InteractionName::DihedralDualPeriodicCommonPhase;
+  for(unsigned int i=0;i<153;++i) FN[i+64+63+62][0]=InteractionName::PairLJ612Cut;
+  for(unsigned int i=153+64+63+62;i<NC;++i) FN[i]=InteractionName::PairCore12;
   for(unsigned int i=0;i<NC;++i) {
     S._Interaction[i].allocate(FN[i]);
     S._Interaction[i].imprint_gradient_energy(S._X);
@@ -42,14 +45,31 @@ int main() {
     S._Interaction[i].Parameter(0)[PairHarmonicParameterName::EqStrength]=100.;
     S._Interaction[i].Parameter(0).build();
   }
+  double td1,td2,td3;
   for(unsigned int i=0;i<63;++i) {
     S._Interaction[i+64].ID(0)[0]=i;
-    S._Interaction[i+64].ID(0)[1]=i+2;
-    S._Interaction[i+64].Parameter(0)[PairHarmonicParameterName::EqLength]=
-      Distance<SystemKindName::Particle,SystemKindName::Particle>(
+    S._Interaction[i+64].ID(0)[1]=i+1;
+    S._Interaction[i+64].ID(0)[2]=i+2;
+    td1=DistanceSQ<SystemKindName::Particle,SystemKindName::Particle>(
           tdsp,S._X[i],S._X[i+2],S._Interaction[i+64].Geometry());
-    S._Interaction[i+64].Parameter(0)[PairHarmonicParameterName::EqStrength]=100.;
+    td2=Distance<SystemKindName::Particle,SystemKindName::Particle>(
+          tdsp,S._X[i],S._X[i+1],S._Interaction[i+64].Geometry());
+    td3=Distance<SystemKindName::Particle,SystemKindName::Particle>(
+          tdsp,S._X[i+1],S._X[i+2],S._Interaction[i+64].Geometry());
+    S._Interaction[i+64].Parameter(0)[AngleHarmonicParameterName::EqAngle]=
+      acos((-td1+td2*td2+td3*td3)/(2*td2*td3));
+    S._Interaction[i+64].Parameter(0)[AngleHarmonicParameterName::EqStrength]=20.;
     S._Interaction[i+64].Parameter(0).build();
+  }
+  for(unsigned int i=0;i<62;++i) {
+    for(unsigned int k=0;k<4;++k)
+      S._Interaction[i+64+63].ID(0)[k]=i+k;
+    S._Interaction[i+64+63].Parameter(0)[DihedralDualPeriodicCommonPhaseParameterName::Double::Phase]=Dihedral(S._X[i],S._X[i+1],S._X[i+2],S._X[i+3]);
+    S._Interaction[i+64+63].Parameter(0).child(DihedralDualPeriodicCommonPhaseParameterName::Child::Period1)[DihedralPeriodicParameterName::Double::Strength]=1.;
+    S._Interaction[i+64+63].Parameter(0).child(DihedralDualPeriodicCommonPhaseParameterName::Child::Period1)(DihedralPeriodicParameterName::Int::Frequency)=1;
+    S._Interaction[i+64+63].Parameter(0).child(DihedralDualPeriodicCommonPhaseParameterName::Child::Period2)[DihedralPeriodicParameterName::Double::Strength]=0.5;
+    S._Interaction[i+64+63].Parameter(0).child(DihedralDualPeriodicCommonPhaseParameterName::Child::Period2)(DihedralPeriodicParameterName::Int::Frequency)=3;
+    S._Interaction[i+64+63].Parameter(0).build();
   }
   ifs.open("2ci2.cnt");
   unsigned int z;
@@ -60,16 +80,18 @@ int main() {
   FG.allocate(65,65);
   FG=0;
   for(unsigned int i=0;i<153;++i) {
-    ifs>>S._Interaction[i+64+63].ID(0)[0];
-    ifs>>S._Interaction[i+64+63].ID(0)[1];
-    FG[S._Interaction[i+64+63].ID(0)[0]][S._Interaction[i+64+63].ID(0)[1]]=1;
-    ifs>>S._Interaction[i+64+63].Parameter(0)[PairLJ1012ParameterName::EqRadius];
-    S._Interaction[i+64+63].Parameter(0)[PairLJ1012ParameterName::EqEnergyDepth]=
+    ifs>>S._Interaction[i+64+63+62].ID(0)[0];
+    ifs>>S._Interaction[i+64+63+62].ID(0)[1];
+    FG[S._Interaction[i+64+63+62].ID(0)[0]][S._Interaction[i+64+63+62].ID(0)[1]]=1;
+    ifs>>S._Interaction[i+64+63+62].Parameter(0)[PairLJ612CutParameterName::EqRadius];
+    S._Interaction[i+64+63+62].Parameter(0)[PairLJ612CutParameterName::EqEnergyDepth]=
       1.;
-    S._Interaction[i+64+63].Parameter(0).build();
+    S._Interaction[i+64+63+62].Parameter(0)[PairLJ612CutParameterName::CutR]=
+      20.;
+    S._Interaction[i+64+63+62].Parameter(0).build();
   }
-  for(unsigned int i=0,n=153+64+63;i<65;++i)
-  for(unsigned int j=i+3;j<65;++j) {
+  for(unsigned int i=0,n=153+64+63+62;i<65;++i)
+  for(unsigned int j=i+4;j<65;++j) {
     if(FG[i][j]==1) continue;
     S._Interaction[n].ID(0)[0]=i;
     S._Interaction[n].ID(0)[1]=j;
@@ -101,10 +123,10 @@ int main() {
 
   cout.precision(12);
   for(unsigned int rt=0;rt<100000000;++rt) {
-    //S._V-=Double(0.5*dt)*S._G;
-    S._V=Double(1-gamma*0.5*dt)*S._V-Double(0.5*dt)*S._G;
-    for(unsigned int i=0;i<S._V.Data().size();++i)
-      S._V.Data()[i]+=rsize*generator();
+    S._V-=Double(0.5*dt)*S._G;
+    //S._V=Double(1-gamma*0.5*dt)*S._V-Double(0.5*dt)*S._G;
+    //for(unsigned int i=0;i<S._V.Data().size();++i)
+      //S._V.Data()[i]+=rsize*generator();
     S._X+=Double(dt)*S._V;
     S._G=0;
     *S._E=0;
@@ -115,10 +137,10 @@ int main() {
       *(S._E)+=S._Interaction[i].Energy();
     }
     S._V-=Double(0.5*dt)*S._G;
-    for(unsigned int i=0;i<S._V.Data().size();++i)
-      S._V.Data()[i]+=rsize*generator();
-    S._V*=Double(1./(1+gamma*dt*0.5));
-    if(rt%1000==0)
+    //for(unsigned int i=0;i<S._V.Data().size();++i)
+      //S._V.Data()[i]+=rsize*generator();
+    //S._V*=Double(1./(1+gamma*dt*0.5));
+    if(rt%10==0)
       cout<<rt<<"\t"<<*S._E<<"\t"<<0.5*NormSQ(S._V.Data())<<endl;
   }
 
